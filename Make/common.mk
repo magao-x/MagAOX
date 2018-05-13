b776b84b65f4a0b5983991b30163fc27b1f3fa2f
Jared Males <jaredmales@gmail.com>
2018-05-13 11:21:29 -0700

    updated application build system to use library PCH as dependency, removed currently unused libraries inherited from mxlib

--- Make/common.mk
+++ Make/common.mk
@@ -21,3 +21,47 @@ LIB_SOFA ?= $(LIB_PATH)/libsofa_c.a
 ARFLAGS ?= rvs
 
 INCLUDES += -I$(INCLUDE_PATH)
+
+
+########################################
+## Optimize Flags
+#######################################
+OPTIMIZE ?= -O3 -fopenmp -ffast-math
+
+########################################
+## Libraries
+#######################################
+
+#location of mxlib and sofa:
+EXTRA_LDFLAGS ?= -L$(PREFIX)/lib    
+
+#the required librarires
+EXTRA_LDLIBS ?= -lmxlib -lsofa_c -lboost_system -lboost_filesystem -ludev
+
+#Add rt on Darwin:
+ifneq ($(UNAME),Darwin)
+    EXTRA_LDLIBS += -lrt
+endif
+
+LDLIBS += $(EXTRA_LDLIBS) 
+LDFLAGS += $(EXTRA_LDFLAGS)
+
+#Hard-code the paths to system libraries so setuid works
+LDLIBRPATH := $(shell echo $$LD_LIBRARY_PATH | sed 's/::/:/g' |  sed 's/:/ -Wl,-rpath,/g')
+LDLIBS += -Wl,-rpath,$(LDLIBRPATH)
+
+########################################
+## Compilation and linking
+#######################################
+
+CFLAGS += -std=c99 -fPIC $(INCLUDES) $(OPTIMIZE)
+CXXFLAGS += -std=c++14 -fPIC $(INCLUDES) $(OPTIMIZE) 
+
+#This is needed to force use of g++ for linking
+LINK.o = $(LINK.cc)
+
+#Create an implicit rule for pre-compiled headers
+%.hpp.gch: %.hpp
+	$(CXX) $(CXXFLAGS) -c $<
+
+
--- Make/magAOXApp.mk
+++ Make/magAOXApp.mk
@@ -1,80 +1,64 @@
-# Definitions for building MagAOX applications
+####################################################
+# Makefile for building MagAOX applications
 #
+# Manages dependance on the libMagAOX pre-compiled header
+# as well as local files (such as .hpp)
+#
+# Manages creation of the git repo status header.
+#
+# This works standalone for a single-header application:
+#    -- only an <app-name>.hpp and <app-name>.cpp are needed
+#    -- in the directory containing those files, invoke make with
+#         make -f ../../Make/magAOXApp.mk t=<app-name>
+#
+# More complicated builds are also supported:
+#    -- In a local Makefile, make the first rule:
+#          allall: all 
+#    -- Then specify rules for building other .o files
+#       --- Copy the $(TARGETS).o rule to depend on the pch.
+#    -- List those .o files after OTHER_DEPENDS=
+#    -- Define TARGET=
+#    -- Then include this file.
+####################################################
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 -include $(SELF_DIR)/../local/magAOXApp.mk
 include $(SELF_DIR)/../Make/common.mk
 
-# Provide default build options in case they weren't defined in local/magAOXApp.mk
-ifeq ($UNAME,Darwin)  # macOS
-    USE_BLAS_FROM ?= Accelerate
-else
-    USE_BLAS_FROM ?= mkl
-endif
+#To ensure pre-compiled header gets used
+CXXFLAGS += -include ../../libMagAOX/libMagAOX.hpp
 
-#default FFT is fftw
-USE_FFT_FROM ?= fftw
+#Uncomment to test whether pre-compiled header is used
+#CXXFLAGS += -H 
 
-# Configure includes and libraries based on build options
-ifeq ($(USE_BLAS_FROM),mkl)
-    BLAS_INCLUDES ?= -DMXLIB_MKL -m64 -I${MKLROOT}/include
-    BLAS_LDFLAGS ?= -L${MKLROOT}/lib/intel64 -Wl,--no-as-needed
-    BLAS_LDLIBS ?= -lmkl_intel_lp64 -lmkl_gnu_thread -lmkl_core -lgomp -lpthread -lm -ldl
-endif
-ifeq ($(USE_BLAS_FROM),ATLAS)
-    BLAS_INCLUDES ?= -I/usr/local/atlas/include
-    BLAS_LDFLAGS ?= -L/usr/local/atlas/lib
-    BLAS_LDLIBS ?= -llapack -lf77blas -lcblas -latlas -lgfortran
-endif
-ifeq ($(USE_BLAS_FROM),Accelerate)
-    BLAS_LDFLAGS ?= -framework Accelerate
-endif
 
-ifeq ($(USE_FFT_FROM),fftw)
-    #Order matters, _threads first.
-    FFT_LDLIBS += -lfftw3_threads -lfftw3f_threads -lfftw3l_threads -lfftw3 -lfftw3f  -lfftw3l 
-endif
-
-OPTIMIZE ?= -O3 -fopenmp -ffast-math
-EXTRA_LDLIBS ?= -lmxlib -lsofa_c -llevmar -lcfitsio -lboost_system -lboost_filesystem -lgsl
-ifneq ($(UNAME),Darwin)
-    EXTRA_LDLIBS += -lrt
-endif
-EXTRA_LDFLAGS ?= -L$(PREFIX)/lib
-
-INCLUDES += $(BLAS_INCLUDES)
-LDLIBS += $(EXTRA_LDLIBS) $(BLAS_LDLIBS) $(FFT_LDLIBS) 
-LDFLAGS += $(BLAS_LDFLAGS) $(FFT_LDFLAGS) $(EXTRA_LDFLAGS)
-
-#Hard-code the paths to system libraries so setuid works
-LDLIBRPATH := $(shell echo $$LD_LIBRARY_PATH | sed 's/::/:/g' |  sed 's/:/ -Wl,-rpath,/g')
-LDLIBS += -Wl,-rpath,$(LDLIBRPATH)
-
-#Add libudev for device discovery
-#\todo this needs to be a define for compat
-LDLIBS += -ludev
-
-CFLAGS += -std=c99 -fPIC $(INCLUDES) $(OPTIMIZE)
-CXXFLAGS += -std=c++14 -fPIC $(INCLUDES) $(OPTIMIZE)
-
-#This is needed to force use of g++ for linking
-LINK.o = $(LINK.cc)
+########################################
+## Targets
+#######################################
 
 # Single-file app name can be supplied as `TARGET=`,
 # or `t=` for short
 TARGET ?= $(t)
-	
-all:  $(TARGET)
+
+all:  pch magaox_git_version.h $(TARGET)
+	rm magaox_git_version.h
+
+pch:
+	cd ../../libMagAOX; ${MAKE}
+
+$(TARGET).o: ../../libMagAOX/libMagAOX.hpp.gch $(TARGET).hpp
+
+$(TARGET):  $(TARGET).o  $(OTHER_DEPENDS)
+	$(LINK.o)  -o $(TARGET) $(TARGET).o $(LDFLAGS) $(LDLIBS)
 
 install: all
 	install -d $(BIN_PATH)
 	install $(TARGET) $(BIN_PATH)
 
-$(TARGET): magaox_git_version.h
-
+#The GIT status header
+#This always gets regenerated.
 .PHONY: magaox_git_version.h
 magaox_git_version.h:
 	gengithead.sh ../../ ./magaox_git_version.h MAGAOX
-
 
 .PHONY: clean
 clean:
