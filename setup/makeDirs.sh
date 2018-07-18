bb308d64fce094e3e33212d20eb418db082f7e8f
Jared Males <jaredmales@gmail.com>
2018-07-17 20:57:39 -0400

    updated for install on the MagAO-X partition scheme"

--- setup/makeDirs.sh
+++ setup/makeDirs.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-mkdir /opt/MagAOX
-mkdir /opt/MagAOX/bin
-mkdir /opt/MagAOX/drivers
-mkdir /opt/MagAOX/drivers/fifos
-mkdir /opt/MagAOX/config
 
-mkdir /opt/MagAOX/logs
-chown :xlog /opt/MagAOX/logs
-chmod g+rw /opt/MagAOX/logs
-chmod g+s /opt/MagAOX/logs
+##################################################
+#  setup the MagAOX directory tree.
+#
+# This is normally configured to run on an actual MagAO-X machine.
+#
+# IMPORTANT: cp to the local directory and edit there if you change it
+#            on your own machine.
+#
+# The only thing you should need to edit for use on a different machine
+# is the LOGDIR definition -- probably from /data/logs to /opt/MagAOX/logs
+###################################################
 
-mkdir /opt/MagAOX/sys
-mkdir /opt/MagAOX/secrets
+mkdir  /opt/MagAOX
+mkdir  /opt/MagAOX/bin
+mkdir  /opt/MagAOX/drivers
+mkdir  /opt/MagAOX/drivers/fifos
+mkdir  /opt/MagAOX/config
+
+LOGDIR=/data/logs
+#LOGDIR=/opt/MagAOX/logs
+
+mkdir $LOGDIR
+chown :magaox $LOGDIR 
+chmod g+rw $LOGDIR
+chmod g+s $LOGDIR
+
+ln -s $LOGDIR /opt/MagAOX/logs
+
+mkdir  /opt/MagAOX/sys
+mkdir  /opt/MagAOX/secrets
 chmod o-rwx /opt/MagAOX/secrets
 chmod g-rwx /opt/MagAOX/secrets
