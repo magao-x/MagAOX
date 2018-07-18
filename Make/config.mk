3bf1592ceefe94ddbb8e2398c3978289668679a4
Jared Males <jaredmales@gmail.com>
2018-07-18 16:37:06 -0700

    updated with optionals

--- Make/config.mk
+++ Make/config.mk
@@ -1,6 +1,9 @@
 
-PREFIX=/opt/MagAOX
+SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+-include $(SELF_DIR)/../local/config.mk
 
-BIN_PATH=$(PREFIX)/bin
-DRIVER_PATH=$(PREFIX)/drivers
-FIFO_PATH=$(PREFIX)/drivers/fifos
+PREFIX?=/opt/MagAOX
+
+BIN_PATH?=$(PREFIX)/bin
+DRIVER_PATH?=$(PREFIX)/drivers
+FIFO_PATH?=$(PREFIX)/drivers/fifos
