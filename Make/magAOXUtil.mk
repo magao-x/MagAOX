c8db71152db17dfc9914ba4e4d8bd12e677c14fa
Jared Males <jaredmales@gmail.com>
2018-07-18 20:16:20 -0400

    utils now get symlinked to /usr/local/bin

--- Make/magAOXUtil.mk
+++ Make/magAOXUtil.mk
@@ -2,4 +2,5 @@ SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(SELF_DIR)/../Make/magAOX.mk
 
 install: all
-	su -c 'install -d $(BIN_PATH) & install $(TARGET) $(BIN_PATH) & ln -s $(BIN_PATH)/$(TARGET) /usr/local/bin/$(TARGET)'
+	su -c 'install -d $(BIN_PATH) & install $(TARGET) $(BIN_PATH) & ln -s $(BIN_PATH)/$(TARGET) /usr/local/bin/$(TARGET) & \
+	          ln -sf $(BIN_PATH)/$(TARGET) /usr/local/bin/$(TARGET)'
--- utils/logdump/logdump.cpp
+++ utils/logdump/logdump.cpp
@@ -8,33 +8,35 @@
 // argv[2] (optional) = number of logs.  1 just shows latest, 2 last two, etc.
 
 /** \todo document this
-  * \todo make this an application with robust config 
-  */ 
+  * \todo make this an application with robust config
+  * \todo logdir and log extension should come form s/w config, look there first.
+  * \todo have c.l. opt for looking in a different directory though.
+  */
 int main(int argc, char **argv)
 {
    std::string dir;
    std::string prefix;
    int n;
-   
+
    dir = "./";
-   
-   if(argc < 2) 
+
+   if(argc < 2)
    {
       std::cerr << "Need application name\n";
       return -1;
    }
-   
+
    prefix = argv[1];
-   
+
    std::vector<std::string> logs = mx::getFileNames( dir, prefix, "", ".binlog");
-   
+
    if(logs.size() == 0)
    {
       std::cerr << "No logs found.\n";
       return -1;
    }
-   
-   
+
+
    if(argc < 3)
    {
       n = logs.size();
@@ -43,50 +45,50 @@ int main(int argc, char **argv)
    {
       n = atoi(argv[2]);
    }
-   
+
    if(n < 0) n = 0;
    if(n > logs.size()) n = logs.size();
-   
+
    using namespace MagAOX::logger;
-   
+
    for(int i=logs.size() - n; i < logs.size(); ++i)
    {
-      
-      std::string fname = logs[i]; 
+
+      std::string fname = logs[i];
       FILE * fin;
-   
+
       bufferPtrT head(new char[headerSize]);
-   
+
       bufferPtrT logBuff;
-   
+
       fin = fopen(fname.c_str(), "r");
-      
+
       size_t buffSz = 0;
       while(!feof(fin))
       {
          int nrd;
-   
+
          nrd = fread( head.get(), sizeof(char), headerSize, fin);
          if(nrd == 0) break;
 
          logLevelT lvl = logLevel(head);
          eventCodeT ec = eventCode(head);
          msgLenT len = msgLen(head);
-         
+
          if( headerSize + len > buffSz )
          {
             logBuff = bufferPtrT(new char[headerSize + len]);
          }
-      
+
          memcpy( logBuff.get(), head.get(), headerSize);
-      
+
          nrd = fread( logBuff.get() + headerSize, sizeof(char), len, fin);
 
          if(ec == eventCodes::GIT_STATE)
          {
             typename git_state::messageT msg;
             git_state::extract(msg, logBuff.get()+messageOffset, len);
-            
+
             if(msg.m_repoName == "MagAOX")
             {
                for(int i=0;i<80;++i) std::cout << '-';
@@ -95,27 +97,26 @@ int main(int argc, char **argv)
                std::cout << '\n';
             }
          }
-         
+
          if(lvl > logLevels::INFO)
          {
             std::cout << "\033[";
-            
+
             if(lvl == logLevels::WARNING) std::cout << "33";
             else std::cout << "31";
             std::cout << "m";
          }
-         
+
          logStdFormat(logBuff);
-         
+
          if(lvl > logLevels::INFO)
          {
             std::cout << "\033[0m";
          }
       }
-   
+
       fclose(fin);
    }
-   
+
    return 0;
 }
-
