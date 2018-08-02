4d8b12112ffd5e84af74ba83218530f8c9742e1b
Jared Males <jaredmales@gmail.com>
2018-08-01 23:51:04 -0700

    mx::app-ified logdump, added follow capability (-f, --follow)

--- utils/logdump/logdump.cpp
+++ utils/logdump/logdump.cpp
@@ -4,119 +4,15 @@
 
 #include "logdump.hpp"
 
-// argv[1] (required) = prefix of logs
-// argv[2] (optional) = number of logs.  1 just shows latest, 2 last two, etc.
 
 /** \todo document this
-  * \todo make this an application with robust config
-  * \todo logdir and log extension should come form s/w config, look there first.
-  * \todo have c.l. opt for looking in a different directory though.
+  * \todo add filters for loglevel, and type.
+  * \todo add config option for sleep time.
   */
 int main(int argc, char **argv)
 {
-   std::string dir;
-   std::string prefix;
-   int n;
+   logdump ld;
 
-   dir = "./";
+   return ld.main(argc, argv);
 
-   if(argc < 2)
-   {
-      std::cerr << "Need application name\n";
-      return -1;
-   }
-
-   prefix = argv[1];
-
-   std::vector<std::string> logs = mx::getFileNames( dir, prefix, "", ".binlog");
-
-   if(logs.size() == 0)
-   {
-      std::cerr << "No logs found.\n";
-      return -1;
-   }
-
-
-   if(argc < 3)
-   {
-      n = logs.size();
-   }
-   else
-   {
-      n = atoi(argv[2]);
-   }
-
-   if(n < 0) n = 0;
-   if(n > logs.size()) n = logs.size();
-
-   using namespace MagAOX::logger;
-
-   for(int i=logs.size() - n; i < logs.size(); ++i)
-   {
-
-      std::string fname = logs[i];
-      FILE * fin;
-
-      bufferPtrT head(new char[headerSize]);
-
-      bufferPtrT logBuff;
-
-      fin = fopen(fname.c_str(), "r");
-
-      size_t buffSz = 0;
-      while(!feof(fin))
-      {
-         int nrd;
-
-         nrd = fread( head.get(), sizeof(char), headerSize, fin);
-         if(nrd == 0) break;
-
-         logLevelT lvl = logLevel(head);
-         eventCodeT ec = eventCode(head);
-         msgLenT len = msgLen(head);
-
-         if( headerSize + len > buffSz )
-         {
-            logBuff = bufferPtrT(new char[headerSize + len]);
-         }
-
-         memcpy( logBuff.get(), head.get(), headerSize);
-
-         nrd = fread( logBuff.get() + headerSize, sizeof(char), len, fin);
-
-         if(ec == eventCodes::GIT_STATE)
-         {
-            typename git_state::messageT msg;
-            git_state::extract(msg, logBuff.get()+messageOffset, len);
-
-            if(msg.m_repoName == "MagAOX")
-            {
-               for(int i=0;i<80;++i) std::cout << '-';
-               std::cout << "\n\t\t\t\t SOFTWARE RESTART\n";
-               for(int i=0;i<80;++i) std::cout << '-';
-               std::cout << '\n';
-            }
-         }
-
-         if(lvl > logLevels::INFO)
-         {
-            std::cout << "\033[";
-
-            if(lvl == logLevels::WARNING) std::cout << "33";
-            else std::cout << "31";
-            std::cout << "m";
-         }
-
-         logStdFormat(logBuff);
-
-         if(lvl > logLevels::INFO)
-         {
-            std::cout << "\033[0m";
-         }
-      }
-
-      fclose(fin);
-   }
-
-   return 0;
 }
--- utils/logdump/logdump.hpp
+++ utils/logdump/logdump.hpp
@@ -8,11 +8,191 @@
 #include <iostream>
 #include <cstring>
 
-#include <mx/fileUtils.hpp>
+#include <mx/ioutils/fileUtils.hpp>
 
 #include "../../libMagAOX/libMagAOX.hpp"
 
+class logdump : public mx::application
+{
+protected:
+
+   std::string dir;
+   std::string ext;
+
+   std::vector<std::string> prefixes;
+
+   int nfiles {0};
+
+   bool m_follow {false};
+
+public:
+   virtual void setupConfig();
+
+   virtual void loadConfig();
+
+   virtual int execute();
+
+};
+
+void logdump::setupConfig()
+{
+
+   config.add("dir","d", "dir" , mx::argType::Required, "", "dir", false,  "string", "Directory to search for logs. MagAO-X default is normally used.");
+   config.add("ext","e", "ext" , mx::argType::Required, "", "ext", false,  "string", "The file extension of log files.  MagAO-X default is normally used.");
+   config.add("nfiles","n", "nfiles" , mx::argType::Required, "", "nfiles", false,  "int", "Number of log files to dump.  If 0, then all matching files dumped.");
+   config.add("follow","f", "follow" , mx::argType::True, "", "follow", false,  "bool", "Follow the log, printing new entries as they appear.");
+}
+
+void logdump::loadConfig()
+{
+   //First get default log dir
+   std::string tmpstr = mx::getEnv(MAGAOX_env_path);
+   if(tmpstr == "")
+   {
+      tmpstr = MAGAOX_path;
+   }
+   dir = tmpstr +  "/" + MAGAOX_logRelPath;;
+
+   //Now check for config option for dir
+   config(dir, "dir");
+
+   ext = ".";
+   ext += MAGAOX_default_logExt;
+   config(ext, "ext");
+
+   config(nfiles, "nfiles");
+
+   if(config.nonOptions.size() < 1)
+   {
+      std::cerr << "logdump: need application name. Try logdump -h for help.\n";
+   }
+
+   if(config.nonOptions.size() > 1)
+   {
+      std::cerr << "logdump: only one application at at time supported. Try logdump -h for help.\n";
+   }
+
+   prefixes.resize(config.nonOptions.size());
+   for(int i=0;i<config.nonOptions.size(); ++i)
+   {
+      prefixes[i] = config.nonOptions[i];
+   }
+
+   config(m_follow, "follow");
+   std::cerr << m_follow << "\n";
+}
+
+int logdump::execute()
+{
+
+   if(prefixes.size() !=1 ) return -1; //error message will have been printed in loadConfig.
+
+
+   std::vector<std::string> logs = mx::ioutils::getFileNames( dir, prefixes[0], "", ext);
+
+   if(nfiles <= 0)
+   {
+      nfiles = logs.size();
+   }
+
+   if(nfiles > logs.size()) nfiles = logs.size();
+
+   using namespace MagAOX::logger;
+
+   for(int i=logs.size() - nfiles; i < logs.size(); ++i)
+   {
+      std::string fname = logs[i];
+      FILE * fin;
+
+      bufferPtrT head(new char[headerSize]);
+
+      bufferPtrT logBuff;
+
+      fin = fopen(fname.c_str(), "r");
+
+      size_t buffSz = 0;
+      while(!feof(fin))
+      {
+         int nrd;
+
+         nrd = fread( head.get(), sizeof(char), headerSize, fin);
+         if(nrd == 0)
+         {
+            //If we're following and on the last log file, wait for more to show up.
+            if( m_follow == true  && i == logs.size()-1)
+            {
+               while(nrd == 0)
+               {
+                  sleep(1);
+                  clearerr(fin);
+                  nrd = fread( head.get(), sizeof(char), headerSize, fin);
+
+                  size_t oldsz = logs.size();
+                  logs = mx::ioutils::getFileNames( dir, prefixes[0], "", ext);
+                  if(logs.size() > oldsz) break;
+               }
+            }
+            else
+            {
+               break;
+            }
+         }
+
+         logLevelT lvl = logLevel(head);
+         eventCodeT ec = eventCode(head);
+         msgLenT len = msgLen(head);
+
+         if( headerSize + len > buffSz )
+         {
+            logBuff = bufferPtrT(new char[headerSize + len]);
+         }
+
+         memcpy( logBuff.get(), head.get(), headerSize);
+
+         ///\todo what do we do if nrd not equal to expected size?
+         nrd = fread( logBuff.get() + headerSize, sizeof(char), len, fin);
+
+         if(ec == eventCodes::GIT_STATE)
+         {
+            typename git_state::messageT msg;
+            git_state::extract(msg, logBuff.get()+messageOffset, len);
+
+            if(msg.m_repoName == "MagAOX")
+            {
+               for(int i=0;i<80;++i) std::cout << '-';
+               std::cout << "\n\t\t\t\t SOFTWARE RESTART\n";
+               for(int i=0;i<80;++i) std::cout << '-';
+               std::cout << '\n';
+            }
+         }
+
+         if(lvl > logLevels::INFO)
+         {
+            std::cout << "\033[";
+
+            if(lvl == logLevels::WARNING) std::cout << "33";
+            else std::cout << "31";
+            std::cout << "m";
+         }
+
+         logStdFormat(logBuff);
+
+         if(lvl > logLevels::INFO)
+         {
+            std::cout << "\033[0m";
+         }
+
+
+      }
+
+
+
+      fclose(fin);
+   }
+
+
+
+   return 0;
+}
+
 #endif //logdump_hpp
-
-
-
