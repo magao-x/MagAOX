bf71c707a68723303f8082962b4367d39e5952bd
Jared Males <jaredmales@gmail.com>
2018-08-13 17:48:06 -0400

    now processes renumber results, setsup stage vector, and reads positions

--- apps/zaberCtrl/za_serial.h
+++ apps/zaberCtrl/za_serial.h
@@ -1,11 +1,11 @@
-/** 
+/**
  * \file za_serial.h
  * \author Eric Dand
  * \version 1.0
  * \date 28 November 2014
  * \copyright Apache Software License Version 2.0
- * 
- * \brief Provides a set of functions for interacting with Zaber devices in 
+ *
+ * \brief Provides a set of functions for interacting with Zaber devices in
  * the ASCII protocol.
  */
 
@@ -24,18 +24,18 @@ extern "C"{
  * with ASCII replies. The names of the fields are taken straight from the
  * ASCII protocol manual.
  *
- * Note that the response data may contain more than one piece of data: 
+ * Note that the response data may contain more than one piece of data:
  * because many replies will return multiple pieces of data, the response_data
  * field will simply contain the end of the message, without the newline
  * terminators. (ie. "\r\n")
  */
 struct za_reply {
-	/** The message type will always be '@' for replies. 
+	/** The message type will always be '@' for replies.
 	 * Info will have the type '#', and alerts '!'. */
 	char message_type;
 	/** The address of the device, an integer between 1 and 99. */
 	int device_address;
-	/** The axis number: 0-9, where 0 refers to the whole device, 
+	/** The axis number: 0-9, where 0 refers to the whole device,
 	 * and 1-9 refer only to that specific axis. */
 	int axis_number;
 	/** Whether a command was accepted or rejected: either "OK" or "RJ". */
@@ -50,26 +50,26 @@ struct za_reply {
 	char response_data[128];
 };
 
-/** Connect to a serial port specified by \a port_name. 
+/** Connect to a serial port specified by \a port_name.
  *
- * Configures the port to the ASCII protocol defaults (115200 baud, 8N1). 
- * If you have set your device to run at a different baud rate, use 
+ * Configures the port to the ASCII protocol defaults (115200 baud, 8N1).
+ * If you have set your device to run at a different baud rate, use
  * za_setbaud() to change it after connecting using this function.
  *
- * On Linux the port name will likely be something like "/dev/ttyUSB0", 
+ * On Linux the port name will likely be something like "/dev/ttyUSB0",
  * on Windows "COM1", and on OS X and BSD systems "/dev/cu.usbserial-A4017CQX".
  * It is important that OS X/BSD systems use the "callout" (cu.* or cua.*) port
- * as opposed to the "dial in" ports with names starting with tty.*, 
+ * as opposed to the "dial in" ports with names starting with tty.*,
  * which will not work with Zaber devices.
  *
- * If you are re-using a `z_port`, make sure to use za_disconnect() to 
+ * If you are re-using a `z_port`, make sure to use za_disconnect() to
  * disconnect the old port before overwriting it with a new one.
  *
- * \param[out] port a pointer to a `z_port` to be written-over with the 
+ * \param[out] port a pointer to a `z_port` to be written-over with the
  * newly-connected port.
- * \param[in] port_name a string containing the name of the port to be opened. 
+ * \param[in] port_name a string containing the name of the port to be opened.
  *
- * \return Z_SUCCESS on success, Z_ERROR_NULL_PARAMETER if \a port or 
+ * \return Z_SUCCESS on success, Z_ERROR_NULL_PARAMETER if \a port or
  * \a port_name is NULL, or Z_ERROR_SYSTEM_ERROR in case of system error.
  */
 int za_connect(z_port *port, const char *port_name);
@@ -85,7 +85,7 @@ int za_disconnect(z_port port);
 /** Sends a command to a serial port.
  *
  * Automatically adds a '/' to begin the command and a '\\n' to end it if
- * these characters are omitted from \a command. 
+ * these characters are omitted from \a command.
  *
  * \param[in] port the port to which to send the command.
  * \param[in] command a string containing the command to be sent.
@@ -95,24 +95,24 @@ int za_disconnect(z_port port);
  */
 int za_send(z_port port, const char *command);
 
-/** Reads a message from the serial port. 
- * 
- * Blocks while reading until it encounters a newline character or its 
+/** Reads a message from the serial port.
+ *
+ * Blocks while reading until it encounters a newline character or its
  * timeout has elapsed.
  *
- * Note: It is recommended that your \a destination buffer be 256B long. 
- * This is long enough to hold any reply from a Zaber device using the 
+ * Note: It is recommended that your \a destination buffer be 256B long.
+ * This is long enough to hold any reply from a Zaber device using the
  * ASCII protocol.
  *
  * Note that this function will simply read the first message on the input
  * buffer, whatever it may be. If you have sent many commands without
  * receiving their corresponding replies, sorting them all out may be a real
  * headache. Note also that the input buffer is finite, and allowing it to
- * fill up will result in undefined behaviour. Try to receive responses to 
+ * fill up will result in undefined behaviour. Try to receive responses to
  * every command you send, or use za_drain() when necessary.
  *
- * Passing NULL for \a destination and 0 for \a length will read a single 
- * reply, discarding it as it is read. This is useful for keeping your 
+ * Passing NULL for \a destination and 0 for \a length will read a single
+ * reply, discarding it as it is read. This is useful for keeping your
  * commands and replies straight without using zb_drain() when you don't care
  * about the contents of most of the replies.
  *
@@ -120,8 +120,8 @@ int za_send(z_port port, const char *command);
  * \param[out] destination a pointer to which to write the reply read.
  * \param[in] length the length of the buffer pointed to by \a destination.
  *
- * \return the number of bytes read, Z_ERROR_SYSTEM_ERROR in case of system 
- * error, or Z_ERROR_BUFFER_TOO_SMALL if \a length is insufficient to store 
+ * \return the number of bytes read, Z_ERROR_SYSTEM_ERROR in case of system
+ * error, or Z_ERROR_BUFFER_TOO_SMALL if \a length is insufficient to store
  * the reply.
  */
 int za_receive(z_port port, char *destination, int length);
@@ -131,20 +131,20 @@ int za_receive(z_port port, char *destination, int length);
  * The #za_reply struct can then be used to gain easier access to the parts
  * of an ASCII reply.
  *
- * \param[out] destination a pointer to a za_reply struct to be populated 
+ * \param[out] destination a pointer to a za_reply struct to be populated
  * with the data found in \a reply.
  * \param[in] reply a pointer to a string containing a full reply from a Zaber
  * device, as specified by the ASCII protocol manual.
  *
- * \return Z_SUCCESS on success, Z_ERROR_NULL_PARAMETER if \a destination or 
- * \a reply is NULL, or Z_ERROR_COULD_NOT_DECODE if the reply is malformed. 
+ * \return Z_SUCCESS on success, Z_ERROR_NULL_PARAMETER if \a destination or
+ * \a reply is NULL, or Z_ERROR_COULD_NOT_DECODE if the reply is malformed.
  */
-int za_decode(struct za_reply *destination, char *reply);
+int za_decode(struct za_reply *destination, const char *reply);
 
-/** Changes the baud rate of both input and output. 
+/** Changes the baud rate of both input and output.
  *
  * This function is unlikely to be necessary for typical use, as za_connect()
- * already sets the baud rate to 115200, the recommended rate for the 
+ * already sets the baud rate to 115200, the recommended rate for the
  * ASCII protocol.
  *
  * Note: za_setbaud() flushes both input and output buffers to ensure a
@@ -165,18 +165,18 @@ int za_decode(struct za_reply *destination, char *reply);
 int za_setbaud(z_port port, int baud);
 
 /** Flushes all input received but not yet read, and attempts to drain all
- * input that would be incoming. 
+ * input that would be incoming.
  *
- * This function is intended to be used when many commands have been sent 
+ * This function is intended to be used when many commands have been sent
  * without reading any responses, and there is a need to read a response from
- * a certain command. In other words, call this function before sending a 
- * command whose response you would like to read if you have not been 
+ * a certain command. In other words, call this function before sending a
+ * command whose response you would like to read if you have not been
  * consistently reading the responses to previous commands.
  *
  * This function will always take at least 100ms to complete, as it tries to
  * read input until it is certain no more is arriving by waiting 100ms before
  * deciding there is no more input incoming. Do not use it for
- * time-sensitive operations, or in any kind of "chatty" setup, eg. multiple 
+ * time-sensitive operations, or in any kind of "chatty" setup, eg. multiple
  * devices daisy-chained together with move tracking enabled. In such a setup,
  * the only reliable way to retrieve reply data is to always follow
  * calls to za_send() with calls to za_receive().
@@ -187,13 +187,13 @@ int za_setbaud(z_port port, int baud);
  */
 int za_drain(z_port port);
 
-/** Sets whether errors and extra info are reported to `stderr`. 
- * 
+/** Sets whether errors and extra info are reported to `stderr`.
+ *
  * Set \a value to 0 to disable all output. Additionally, you can compile this
  * library with the macro NDEBUG defined, which will disable all output and
  * skip checks to "verbose" altogether in the compiled code.
  *
- * \param[in] value whether or not the program should output error messages 
+ * \param[in] value whether or not the program should output error messages
  * and info to `stderr`.
  */
 void za_set_verbose(int value);
--- apps/zaberCtrl/zaberCtrl.hpp
+++ apps/zaberCtrl/zaberCtrl.hpp
@@ -27,129 +27,151 @@ class zaberStage
 {
 protected:
    int m_deviceAddress;
-   
+
    int m_axisNumber{0};
-   
+
    bool m_commandStatus {true};
-   
+
    char m_deviceStatus {'U'};
-   
+
    bool m_warningState {false};
-   
-   double m_rawPos;
-   
+
+   long m_rawPos;
+
 public:
-   
+
    int deviceAddress()
    {
       return m_deviceAddress;
    }
-   
+
    int deviceAddress( const int & da )
    {
       m_deviceAddress = da;
       return 0;
    }
-   
+
    int axisNumber()
    {
       return m_axisNumber;
    }
-   
+
    int axisNumber( const int & an )
    {
       m_axisNumber = an;
       return 0;
    }
-   
+
    bool commandStatus()
    {
       return m_commandStatus;
    }
-   
+
    char deviceStatus()
    {
       return m_deviceStatus;
    }
-   
+
    bool warningState()
    {
       return m_warningState;
    }
-   
+
+   int getResponse(std::string & response, const std::string & repBuff)
+   {
+      za_reply rep;
+      int rv = za_decode(&rep, repBuff.c_str());
+      if(rv != Z_SUCCESS)
+      {
+         std::cerr << "za_decode !=Z_SUCCESS" << "\n";
+         return rv;
+      }
+
+      return getResponse(response, rep);
+   }
+
    int getResponse(std::string & response, const za_reply & rep)
    {
       if(rep.device_address == m_deviceAddress)
-      { 
+      {
          if(rep.reply_flags[0] == 'O') m_commandStatus = true;
          else m_commandStatus = false;
-               
+
          m_deviceStatus = rep.device_status[0];
-               
+
          if(rep.warning_flags[0] == '-') m_warningState = false;
          else m_warningState = true;
-         
+
          response = rep.response_data;
-               
+
          return 0;
       }
       else return -1;
-   }   
-   
+   }
+
    int sendCommand(std::string & response, z_port port, const std::string & command)
    {
+      //log<text_log>(std::string("Sending: ") + command, logLevels::DEBUG2);
+      std::cerr << "Sending: " << command << "\n";
       za_send(port, command.c_str());
-      
+
       char buff[256];
-   
+
       while(1)
       {
          int rv = za_receive(port, buff, sizeof(buff));
-         if(rv == Z_ERROR_TIMEOUT) 
+         if(rv == Z_ERROR_TIMEOUT)
          {
+            std::cerr << "Z_ERROR_TIMEOUT" << "\n";
             //log timeout
             break; //error but just get out.
          }
-         else if(rv != Z_SUCCESS)
+         else if(rv < 0)
          {
+            std::cerr << "za_receive!=Z_SUCCESS" << "\n";
             //log error
             break;
          }
          za_reply rep;
-      
+
+         //log<text_log>(std::string("Received: ") + buff, logLevels::DEBUG2);
+
          rv = za_decode(&rep, buff);
          if(rv != Z_SUCCESS)
          {
+            std::cerr << "za_decode !=Z_SUCCESS" << "\n";
             //log error
             break;
          }
-         
+
          if(rep.device_address == m_deviceAddress) return getResponse(response, rep);
       }
-      
+
       response = "";
-      
+
       return -1;
    }
-      
+
    int updatePos(z_port port)
    {
       std::string com = "/" + mx::ioutils::convertToString(m_deviceAddress) + " ";
-      com += "get pos\n";
-      
+      com += "get pos";
+
       std::string response;
-      
+
       int rv = sendCommand(response, port, com);
-      
+
       if(rv == 0)
       {
          if( m_commandStatus )
          {
-            m_rawPos = mx::ioutils::convertFromString<double>(response);
+            m_rawPos = mx::ioutils::convertFromString<long>(response);
+            std::cerr << "m_rawPos: " << m_rawPos << "\n";
             return 0;
          }
          else
          {
+            std::cerr << "Command Rejected\n";
             //Log rejected command
 
             return -1;
@@ -157,13 +179,14 @@ public:
       }
       else
       {
+         std::cerr << "Some other error\n";
          //trace
          return -1;
       }
    }
 };
 
-   
+
 class zaberCtrl : public MagAOXApp<>, public tty::usbDevice
 {
 
@@ -175,11 +198,13 @@ protected:
 
    int m_numStages {0};
 
-   z_port m_port;
+   z_port m_port {0};
+
+   std::vector<zaberStage> m_stages;
 
    ///Mutex for locking device communications.
    std::mutex m_devMutex;
-   
+
 public:
    /// Default c'tor.
    zaberCtrl();
@@ -265,11 +290,11 @@ void zaberCtrl::loadConfig()
 int zaberCtrl::testConnection()
 {
    std::lock_guard<std::mutex> guard(m_devMutex);
-   
+
    if(m_port <= 0)
    {
       int rv = euidCalled();
-      
+
       if(rv < 0)
       {
          log<software_trace_fatal>({__FILE__, __LINE__});
@@ -315,7 +340,9 @@ int zaberCtrl::testConnection()
       return ZC_NOT_CONNECTED; //Not an error, just no device talking.
    }
 
+   log<text_log>("Sending: /", logLevels::DEBUG);
    int nwr = za_send(m_port, "/");
+
    if(nwr == Z_ERROR_SYSTEM_ERROR)
    {
       za_disconnect(m_port);
@@ -334,8 +361,7 @@ int zaberCtrl::testConnection()
       if(nrd >= 0)
       {
          buffer[nrd] = '\0';
-         std::cerr << buffer << "\n";
-         //Here: process these messages -- have to be careful because we have the mutex and don't want to give it up!
+         log<text_log>(std::string("Received: ") + buffer, logLevels::DEBUG);
          ++stageCnt;
       }
       else if (nrd != Z_ERROR_TIMEOUT)
@@ -344,7 +370,11 @@ int zaberCtrl::testConnection()
          state(stateCodes::ERROR);
          return ZC_NOT_CONNECTED;
       }
-      else break; //timeout
+      else
+      {
+         log<text_log>("TIMEOUT", logLevels::DEBUG);
+         break; //timeout
+      }
    }
    if(stageCnt == 0)
    {
@@ -362,7 +392,7 @@ int zaberCtrl::appStartup()
       log<text_log>( "In appStartup but in state UNINITIALIZED.", logLevels::FATAL );
       return -1;
    }
-   
+
    //Get the USB device if it's in udev
    if(m_deviceName == "") state(stateCodes::NODEVICE);
    else
@@ -379,7 +409,7 @@ int zaberCtrl::appStartup()
 
 int zaberCtrl::appLogic()
 {
-   
+
    if( state() == stateCodes::INITIALIZED )
    {
       log<text_log>( "In appLogic but in state INITIALIZED.", logLevels::FATAL );
@@ -445,27 +475,40 @@ int zaberCtrl::appLogic()
    else if( state() == stateCodes::CONNECTED )
    {
       state(stateCodes::CONFIGURING);
-      
+
       std::lock_guard<std::mutex> guard(m_devMutex);
-      
-      int nwr = za_send(m_port, "renumber");
-      
+
+      log<text_log>("DRAINING", logLevels::DEBUG);
+      int rv = za_drain(m_port);
+      if(rv != Z_SUCCESS)
+      {
+         log<software_error>({__FILE__,__LINE__, rv, "error from za_drain"});
+         state(stateCodes::ERROR);
+         return 0;
+      }
+
+      char buffer[256];
+      int stageCnt = 0;
+
+      std::vector<std::string> renumberRes;
+
+      log<text_log>("Sending: /renumber", logLevels::DEBUG);
+      int nwr = za_send(m_port, "/renumber");
+
       if(nwr == Z_ERROR_SYSTEM_ERROR)
       {
          log<text_log>("Error sending renumber to stages", logLevels::ERROR);
          state(stateCodes::ERROR);
       }
 
-      char buffer[256];
-      int stageCnt = 0;
       while(1)
       {
          int nrd = za_receive(m_port, buffer, sizeof(buffer));
-         if(nrd >= 0 ) 
+         if(nrd >= 0 )
          {
             buffer[nrd] = '\0';
-            std::cerr << buffer << "\n";
-            //Need to track these? -- YES, what if something else comes in?
+            log<text_log>(std::string("Received: ")+buffer, logLevels::DEBUG);
+            renumberRes.push_back(buffer);
             ++stageCnt;
          }
          else if( nrd != Z_ERROR_TIMEOUT)
@@ -473,15 +516,47 @@ int zaberCtrl::appLogic()
             log<text_log>("Error receiving from stages", logLevels::ERROR);
             state(stateCodes::ERROR);
          }
-         else break; //Timeout ok.
+         else
+         {
+            log<text_log>("TIMEOUT", logLevels::DEBUG);
+            break; //Timeout ok.
+         }
       }
-      
+
       if(state() == stateCodes::CONFIGURING) //Check that nothing changed.
       {
          std::cerr << "stageCnt: " << stageCnt << "\n";
+         for(size_t i=0; i< renumberRes.size(); ++i)
+         {
+            std::cerr << renumberRes[i] << "\n";
+            zaberStage newst;
+            newst.deviceAddress(i+1);
+
+            std::string rstr;
+
+            if( newst.getResponse(rstr, renumberRes[i]) != Z_SUCCESS)
+            {
+               std::cerr << "Stage did not match.\n";
+               state(stateCodes::ERROR);
+               return 0;
+            }
+
+            m_stages.push_back(newst);
+            std::cerr << newst.deviceStatus() << "\n";
+
+         }
          state(stateCodes::LOGGEDIN);
       }
    } //mutex is given up at this point
+
+   if( state() == stateCodes::LOGGEDIN )
+   {
+      std::lock_guard<std::mutex> guard(m_devMutex);
+      for(size_t i=0; i < m_stages.size();++i)
+      {
+         m_stages[i].updatePos(m_port);
+      }
+   }
 
    if( state() == stateCodes::ERROR )
    {
@@ -523,7 +598,7 @@ int zaberCtrl::appLogic()
    {
       return -1;
    }
-   
+
    return 0;
 }
 
