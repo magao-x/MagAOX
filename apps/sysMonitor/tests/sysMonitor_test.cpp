e2c05458b8b3b4c272a54b25c430c6a5d2099471
cjbohlman <cbohlman@email.arizona.edu>
2018-08-31 14:41:18 -0700

    Updated testing files

--- apps/sysMonitor/tests/sysMonitor_test.cpp
+++ apps/sysMonitor/tests/sysMonitor_test.cpp
@@ -1,4 +1,11 @@
-
+/****
+   * Test following parsing functions
+   * int parseCPUTemperatures(std::string, std::vector<float>&);
+   * int parseCPULoads(std::string, float&);
+   * int parseDiskTemperature(std::string, float&);
+   * int parseDiskUsage(std::string, float&);
+   * int parseRamUsage(std::string, float&);
+   ****/
 
 #define CATCH_CONFIG_MAIN
 #include "../../../tests/catch2/catch.hpp"
@@ -10,18 +17,11 @@ SCENARIO( "System monitor is constructed and CPU temperature results are passed 
 {
    GIVEN("A default constructed system monitor object and an empty vector for temperatures")
    {
-      sysMonitor sm;
+      MagAOX::app::sysMonitor sm;
       int rv;
       std::vector<float> temps;
 
-      /****
-       * Test following parsing functions
-       * int parseCPUTemperatures(std::string, std::vector<float>&);
-       * int parseCPULoads(std::string, float&);
-       * int parseDiskTemperature(std::string, float&);
-       * int parseDiskUsage(std::string, float&);
-       * int parseRamUsage(std::string, float&);
-      ****/
+      // Fails with whitespace in front, but is this necessary to correct for?
       
       WHEN("Correct line is given")
       {
@@ -32,128 +32,92 @@ SCENARIO( "System monitor is constructed and CPU temperature results are passed 
       
       WHEN("Another correct line is given")
       {
-         rv = sm.parseCPUTemperatures("     Core   1:    +45.0°C    (high = +100.0°C, crit = +100.0°C)", temps);
+         rv = sm.parseCPUTemperatures("Core   1:    +45.0°C    (high = +100.0°C, crit = +100.0°C)", temps);
          REQUIRE(rv == 0);
-         REQUIRE(temps.size() == 2);
+         REQUIRE(temps.size() == 1);
       }
       
       WHEN("Another correct line is given")
       {
          rv = sm.parseCPUTemperatures("Core 2:      +91.0°C  (high = +100.0°C, crit = +100.0°C)", temps);
          REQUIRE(rv == 0);
-         REQUIRE(temps.size() == 3);
+         REQUIRE(temps.size() == 1);
       }
       
       WHEN("Incorrect line is given")
       {
          rv = sm.parseCPUTemperatures("coretemp-isa-0000", temps);
          REQUIRE(rv == 1);
-         REQUIRE(temps.size() == 3);
+         REQUIRE(temps.size() == 0);
       }
       
       WHEN("Incorrect line is given")
       {
          rv = sm.parseCPUTemperatures("Core 3:+91.0°C  (high = +100.0°C, crit = +100.0°C)", temps);
          REQUIRE(rv == 1);
-         REQUIRE(temps.size() == 3);
+         REQUIRE(temps.size() == 0);
       }
 
       WHEN("Incorrect line is given")
       {
          rv = sm.parseCPUTemperatures("Core2:      +91.0°C(high =+100.0°C, crit= +100.0°C)", temps);
          REQUIRE(rv == 1);
-         REQUIRE(temps.size() == 3);
+         REQUIRE(temps.size() == 0);
       }
    }
 }
-/*
-SCENARIO( "remoteDriver parses remote driver spec strings", "[remoteDriver]" ) 
+
+SCENARIO( "System monitor is constructed and CPU load results are passed in", "[sysMonitor]" ) 
 {
-   GIVEN("A default constructed remoteDriver")
+   GIVEN("A default constructed system monitor object and an empty vector for loads")
    {
-      remoteDriver rd;
+      MagAOX::app::sysMonitor sm;
       int rv;
-      WHEN("A nominal remote driver spec is parsed (no port)")
+      std::vector<float> loads;
+
+      // Fails with whitespace in front, but is this necessary to correct for?
+      
+      WHEN("Correct line is given")
       {
-         rv = rd.parse("newDrive@remote");
+         rv = sm.parseCPUTemperatures("02:35:43 PM    0    6.57    0.02    1.32    0.24    0.00    0.00    0.00    0.00    0.00   91.85", loads);
          REQUIRE(rv == 0);
-         REQUIRE(rd.name() == "newDrive");
-         REQUIRE(rd.host() == "remote");
-         REQUIRE(rd.port() == INDI_DEFAULT_PORT);
-         REQUIRE(rd.hostSpec() == "remote:7624");
-         REQUIRE(rd.fullSpec() == "newDrive@remote:7624");
- 
+         REQUIRE(loads.size() == 1);
       }
       
-      WHEN("A whitespace-full remote driver spec is parsed (no port)")
+      WHEN("Another correct line is given")
       {
-         rv = rd.parse("next drive @ 9. 8. 7 .6");
+         rv = sm.parseCPUTemperatures("", loads);
          REQUIRE(rv == 0);
-         REQUIRE(rd.name() == "nextdrive");
-         REQUIRE(rd.host() == "9.8.7.6");
-         REQUIRE(rd.port() == INDI_DEFAULT_PORT);
+         REQUIRE(loads.size() == 1);
       }
       
-      WHEN("A nominal remote driver spec is parsed (w/ port)")
+      WHEN("Another correct line is given")
       {
-         rv = rd.parse("nowDrive@remoteish:7");
+         rv = sm.parseCPUTemperatures("", loads);
          REQUIRE(rv == 0);
-         REQUIRE(rd.name() == "nowDrive");
-         REQUIRE(rd.host() == "remoteish");
-         REQUIRE(rd.port() == 7);
+         REQUIRE(loads.size() == 1);
       }
       
-      WHEN("A whitespace-full remote driver spec is parsed (w/ port)")
+      WHEN("Incorrect line is given")
       {
-         rv = rd.parse("next B drive @ 9. 3. 7 .6 : 6 5 7");
-         REQUIRE(rv == 0);
-         REQUIRE(rd.name() == "nextBdrive");
-         REQUIRE(rd.host() == "9.3.7.6");
-         REQUIRE(rd.port() == 657);
+         rv = sm.parseCPUTemperatures("02:35:43 PM  CPU    %%usr   %%nice    %%sys %%iowait    %%irq   %%soft  %%steal  %%guest  %%gnice   %%idle", loads);
+         REQUIRE(rv == 1);
+         REQUIRE(loads.size() == 0);
       }
       
-      WHEN("A valid remote driver spec is parsed (w/ empty port)")
+      WHEN("Incorrect line is given")
       {
-         rv = rd.parse("Q@B:");
-         REQUIRE(rv == 0);
-         REQUIRE(rd.name() == "Q");
-         REQUIRE(rd.host() == "B");
-         REQUIRE(rd.port() == INDI_DEFAULT_PORT);
+         rv = sm.parseCPUTemperatures("02:35:43 PM  all    6.59    0.02    1.32    0.42    0.00    0.00    0.00    0.00    0.00   91.65", loads);
+         REQUIRE(rv == 1);
+         REQUIRE(loads.size() == 0);
       }
-      
-      WHEN("An invalid driver spec is parsed, @ first")
+
+      WHEN("Incorrect line is given")
       {
-         rv = rd.parse("@dname");
-         REQUIRE(rv < 0);
-         REQUIRE(rd.name() == "");
-         REQUIRE(rd.host() == "");
+         rv = sm.parseCPUTemperatures("02:35:43 PM0    6.57    zzz50.02    2vc51.32    0.24    0.00    0.00  c3t  c350.00    0.00    0.00   ct3591.85
+", loads);
+         REQUIRE(rv == 1);
+         REQUIRE(loads.size() == 0);
       }
-      
-      WHEN("An invalid driver spec is parsed, @ last")
-      {
-         rv = rd.parse("@dname");
-         REQUIRE(rv < 0);
-         REQUIRE(rd.name() == "");
-         REQUIRE(rd.host() == "");
-      }
-      
-      WHEN("An invalid driver spec is parsed, no @")
-      {
-         rv = rd.parse("dname");
-         REQUIRE(rv < 0);
-         REQUIRE(rd.name() == "");
-         REQUIRE(rd.host() == "");
-      }
-      
-      WHEN("An invalid driver spec is parsed, : before @")
-      {
-         rv = rd.parse("dname:vg@:367");
-         REQUIRE(rv < 0);
-         REQUIRE(rd.name() == "");
-         REQUIRE(rd.host() == "");
-         REQUIRE(rd.port() == INDI_DEFAULT_PORT);
-      }
-      
    }
 }
-*/
