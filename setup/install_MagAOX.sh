7191c0c6a88987851613d68b1e5276207cba37a6
Joseph Long <github@joseph-long.com>
2018-09-07 17:26:59 -0700

    Rootify the config clone permissions setting

--- setup/install_MagAOX.sh
+++ setup/install_MagAOX.sh
@@ -10,11 +10,11 @@ if [[ ! -e /opt/MagAOX/config ]]; then
     cd /opt/MagAOX/config
     echo "Config branches available:"
     git branch
-    chown -R :magaox-dev /opt/MagAOX/config
-    chmod -R g=rwX /opt/MagAOX/config
+    /bin/sudo chown -R :magaox-dev /opt/MagAOX/config
+    /bin/sudo chmod -R g=rwX /opt/MagAOX/config
     # n.b. can't be recursive because g+s on files means something else
     # so we find all directories and individually chmod them:
-    find /opt/MagAOX/config -type d -exec chmod -v g+s {} \;
+    /bin/sudo find /opt/MagAOX/config -type d -exec chmod -v g+s {} \;
 fi
 echo "Building flatlogs"
 cd /opt/MagAOX/source/MagAOX/flatlogs/src
