cd5b9553a84d9db1244e886c06666c8b16f1ef62
Joseph Long <github@joseph-long.com>
2018-09-08 14:29:15 -0700

    Install ncurses-devel dependency in provisioning

--- setup/install_dependencies.sh
+++ setup/install_dependencies.sh
@@ -172,7 +172,7 @@ cd $DEPSROOT
 # MagAOX dependencies
 #
 FLATBUFFERS_VERSION="1.9.0"
-yum install -y cmake zlib-devel libudev-devel
+yum install -y cmake zlib-devel libudev-devel ncurses-devel
 #
 # Flatbuffers
 #
