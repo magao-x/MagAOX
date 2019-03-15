d9b0db56f9bda28904c8b90fbe497f8ab2971191
Jared Males <jaredmales@gmail.com>
2019-03-14 20:06:05 -0700

    updated pwr gui, cursesINDI debugging

--- gui/apps/pwr/pwr_main.cpp
+++ gui/apps/pwr/pwr_main.cpp
@@ -2,28 +2,36 @@
 #include <QApplication>
 
 #include "pwrGUI.hpp"
-
+#include <iostream>
    
 int main(int argc, char *argv[])
 {
+   std::cerr << "1\n";
    //int data_type;
    QApplication app(argc, argv);
-
-   multiIndiPublisher client("pwrGUI", "127.0.0.1", 7624);
-
-   xqt::pwrGUI pwr;
+// 
+    std::cerr << "2\n";
+    multiIndiPublisher client("pwrGUI", "127.0.0.1", 7624);
+// 
+    std::cerr << "3\n";
+    xqt::pwrGUI pwr;
+//    
+    std::cerr << "4\n";
+    pwr.subscribe(&client);
+//    
+    std::cerr << "5\n";
+    pwr.show();
+// 
+    std::cerr << "6\n";
+    client.activate();
+//    
+    std::cerr << "7\n";
+    int rv = app.exec();
+//    
+    std::cerr << "8\n";
+    client.quitProcess();
+    client.deactivate();
    
-   pwr.subscribe(&client);
-   
-   pwr.show();
-
-   client.activate();
-   
-   int rv = app.exec();
-   
-   client.quitProcess();
-   client.deactivate();
-   
-   return rv;
+    return rv;
 }
    
