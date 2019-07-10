c5b1ff6ccf7eeef4d4d905a10ed7d9b30bd62918
Joseph Long <github@joseph-long.com>
2019-07-10 14:34:54 -0700

    Add MKL vars to profile.d

--- setup/install_mkl.sh
+++ setup/install_mkl.sh
@@ -6,3 +6,4 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 yum install -y intel-mkl-64bit
+echo "source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh intel64" > /etc/profile.d/mklvars.sh
