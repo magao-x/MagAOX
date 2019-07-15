3ed0fd59a02a78ee1fedce2684bee0549ffb1bb2
Joseph Long <github@joseph-long.com>
2019-07-15 13:42:03 -0700

    Add Picam setup script

--- setup/steps/install_picam.sh
+++ setup/steps/install_picam.sh
@@ -9,10 +9,17 @@ PICAM_URL=ftp://ftp.piacton.com/Public/Software/Official/PICam/$PICAM_RUNFILE
 if [[ ! -e $PICAM_RUNFILE ]]; then
     curl -O $PICAM_URL
 fi
-if [[ $(md5sum Picam_SDK.run) != "*$PICAM_RUNFILE_CHECKSUM*" ]]; then
-    log_error "$PICAM_RUNFILE has md5 checksum $(md5sum Picam_SDK.run)"
+if [[ $(md5sum ./$PICAM_RUNFILE) != *$PICAM_RUNFILE_CHECKSUM* ]]; then
+    log_error "$PICAM_RUNFILE has md5 checksum $(md5sum ./$PICAM_RUNFILE)"
     log_error "Expected $PICAM_RUNFILE_CHECKSUM"
     log_error "(Either revise ${BASH_SOURCE[0]} or get the old runfile somewhere)"
     exit 1
 fi
-exit 1
+chmod +x ./$PICAM_RUNFILE
+if [[ ! -e /opt/PrincetonInstruments/picam ]]; then
+    yes yes | ./$PICAM_RUNFILE
+fi
+chmod a+rX -R /opt/pleora
+chmod a+rX -R /opt/PrincetonInstruments
+chmod g+xr,o+xr /usr/local/lib/libftd2xx.so.1.4.6
+echo "if [[ \"\$EUID\" != 0 ]]; then source /opt/pleora/ebus_sdk/x86_64/bin/set_puregev_env; fi" > /etc/profile.d/picam_pleora_env.sh
