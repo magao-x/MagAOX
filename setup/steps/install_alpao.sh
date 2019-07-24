3a350446b8cb078a2c3e0dd08d1ca153b94ba9b8
Joseph Long <github@joseph-long.com>
2019-07-23 19:50:52 -0700

    Make ALPAO patch application idempotent

--- setup/steps/install_alpao.sh
+++ setup/steps/install_alpao.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
 cd alpao
-patch -p1 < alpao_build_fix.patch
+patch -Np1 < alpao_build_fix.patch || true
 # option 2 - "Install ASDK and Interface Corp. PEX-292144 support"
 echo 2 | sudo bash Linux/InstallASDK.sh
