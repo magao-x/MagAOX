910806ee7d701efc57f0b5f89026637a34ac283e
Joseph Long <github@joseph-long.com>
2019-07-23 23:54:53 -0700

    Restore working directory after installing cppzmq

--- setup/steps/install_milkzmq.sh
+++ setup/steps/install_milkzmq.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
 
+# install cppzmq (dependency of milkzmq)
 CPPZMQ_COMMIT=213da0b04ae3b4d846c9abc46bab87f86bfb9cf4
 if [[ ! -d cppzmq ]]; then
     git clone https://github.com/zeromq/cppzmq.git
@@ -14,8 +15,9 @@ fi
 git checkout $CPPZMQ_COMMIT
 /bin/sudo cp *.hpp /usr/local/include/
 
+cd ..
+# install milkzmq
 MILKZMQ_COMMIT=f427ed35986b3956a937adef349820d1d3a626f0
-
 if [[ ! -d ./milkzmq ]]; then
     git clone https://github.com/jaredmales/milkzmq.git milkzmq
     cd ./milkzmq
