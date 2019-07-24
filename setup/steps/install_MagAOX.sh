0653ac54fb32756af5d8dc67ac8b46d5474f0367
Joseph Long <github@joseph-long.com>
2019-07-23 20:17:29 -0700

    Add calib repo to install scripts

--- setup/steps/install_MagAOX.sh
+++ setup/steps/install_MagAOX.sh
@@ -3,18 +3,32 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
 
-if [[ ! -e /opt/MagAOX/config ]]; then
-    echo "Cloning new copy of MagAOX config files"
-    /bin/sudo git clone https://github.com/magao-x/config.git /opt/MagAOX/config
-    cd /opt/MagAOX/config
-    echo "Config branches available:"
-    git branch
-    /bin/sudo chown -R :magaox-dev /opt/MagAOX/config
-    /bin/sudo chmod -R g=rwX /opt/MagAOX/config
+function clone_or_update_and_cd() {
+
+    if [[ ! -e /opt/MagAOX/$1 ]]; then
+        echo "Cloning new copy of MagAOX config files"
+        /bin/sudo git clone https://github.com/magao-x/$1.git /opt/MagAOX/$1
+        cd /opt/MagAOX/$1
+        log_success "Cloned new /opt/MagAOX/$1"
+    else
+        cd /opt/MagAOX/$1
+        git pull
+        log_success "Updated /opt/MagAOX/$1"
+    fi
+    sudo chown -R :magaox-dev /opt/MagAOX/$1
+    sudo chmod -R g=rwX /opt/MagAOX/$1
     # n.b. can't be recursive because g+s on files means something else
     # so we find all directories and individually chmod them:
-    /bin/sudo find /opt/MagAOX/config -type d -exec chmod -v g+s {} \;
-fi
+    sudo find /opt/MagAOX/$1 -type d -exec chmod g+s {} \;
+    log_success "Normalized permissions on /opt/MagAOX/$1"
+}
+
+clone_or_update_and_cd config
+echo "Config branches available:"
+git branch
+
+clone_or_update_and_cd calib
+
 echo "Building flatlogs"
 cd /opt/MagAOX/source/MagAOX/flatlogs/src
 make
