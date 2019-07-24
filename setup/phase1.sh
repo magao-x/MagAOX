cf7fe47d87e8cce95d9fb94880300ef9cab0c130
Joseph Long <github@joseph-long.com>
2019-07-23 20:11:48 -0700

    be more tidy in phase1.sh with rpm caching

--- setup/phase1.sh
+++ setup/phase1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/_common.sh
+mkdir -p /opt/MagAOX/vendor
+cd /opt/MagAOX/vendor
 /bin/sudo $DIR/steps/install_rt_kernel_pinned.sh
 if [[ "$EUID" == 0 ]]; then
     log_error "Can't add you to the magaox-dev group when you're running as root! Aborting."
