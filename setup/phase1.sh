8b2cd7c395d1d409ed441d1030589b624b5b7476
Joseph Long <github@joseph-long.com>
2019-07-24 21:04:16 -0700

    add kernel cmdline workaround for alpao pcie card

--- setup/phase1.sh
+++ setup/phase1.sh
@@ -9,5 +9,12 @@ if [[ "$EUID" == 0 ]]; then
     log_error "Can't add you to the magaox-dev group when you're running as root! Aborting."
     exit 1
 fi
+if ! grep "hardened_usercopy=off" /etc/default/grub; then
+    # without this option, the ALPAO DM driver (really the Interface Corp card driver) will
+    # trigger protections against suspicious copying between kernel and userspace
+    # and *bring down the whole system* (by rebooting when you try to run
+    # https://github.com/magao-x/ALPAO-interface/blob/master/initalpaoPCIe )
+    echo GRUB_CMDLINE_LINUX_DEFAULT="hardened_usercopy=off" | sudo tee -a /etc/default/grub
+fi
 /bin/sudo $DIR/setup_users_and_groups.sh
 log_success "Installed PREEMPT_RT Linux kernel and configured groups. Reboot before proceeding."
