daf5c6f54c5e89da6ae56b57d034456ed97c6357
Joseph Long <github@joseph-long.com>
2019-07-26 14:31:55 -0700

    Make all permissions explicit in set_permissions.sh

--- setup/steps/set_permissions.sh
+++ setup/steps/set_permissions.sh
@@ -3,26 +3,55 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
 
-chown -R root:root /opt/MagAOX
+function setgid_all() {
+    # n.b. can't be recursive because g+s on files means something else
+    # so we find all directories and individually chmod them:
+    find $1 -type d -exec chmod g+s {} \;
+}
 
-# Operators should be able to access sources
-# but modifications should be setGID (+s)
-chown -R :magaox-dev /opt/MagAOX/source
-chmod -R g=rwX /opt/MagAOX/source
-# n.b. can't be recursive because g+s on files means something else
-# so we find all directories and individually chmod them:
-find /opt/MagAOX/source -type d -exec chmod g+s {} \;
+chown root:root /opt/MagAOX
 
-# Set logs to writable for non-admin users like xsup
-chown -RP xsup:magaox /opt/MagAOX/logs
-# Let operators (not in group magaox) read logs but not write:
-chmod -R u=rwX,g=rwX,o=rX /opt/MagAOX/logs
+chown -R root:magaox-dev /opt/MagAOX/bin
+chmod -R u=rwX,g=rwX,o=rX /opt/MagAOX/bin
+setgid_all /opt/MagAOX/bin
 
-# Set rawimages to writable for non-admin users like xsup
-chown -RP xsup:magaox /opt/MagAOX/rawimages
-# Let operators (not in group magaox) read rawimages but not write:
-chmod -R u=rwX,g=rwX,o=rX /opt/MagAOX/rawimages
+chown -R root:magaox-dev /opt/MagAOX/calib
+chmod -R u=rwX,g=rwX,o=rX /opt/MagAOX/calib
+setgid_all /opt/MagAOX/calib
 
-# Hide secrets
-chmod o-rwx /opt/MagAOX/secrets
-chmod g-rwx /opt/MagAOX/secrets
+chown -R root:magaox-dev /opt/MagAOX/config
+chmod -R u=rwX,g=rwX,o=rX /opt/MagAOX/config
+setgid_all /opt/MagAOX/config
+
+chown -R root:root /opt/MagAOX/drivers
+chmod -R u=rwX,g=rwX,o=rX /opt/MagAOX/drivers
+
+LOGS_DIR=/opt/MagAOX/logs
+if [[ -h $LOGS_DIR ]]; then
+    LOGS_DIR=$(readlink $LOGS_DIR)
+fi
+chown -RP xsup:magaox $LOGS_DIR
+chmod -R u=rwX,g=rwX,o=rX $LOGS_DIR
+setgid_all $LOGS_DIR
+
+RAWIMAGES_DIR=/opt/MagAOX/rawimages
+if [[ -h $RAWIMAGES_DIR ]]; then
+    RAWIMAGES_DIR=$(readlink $RAWIMAGES_DIR)
+fi
+chown -RP xsup:magaox $RAWIMAGES_DIR
+chmod -R u=rwX,g=rwX,o=rX $RAWIMAGES_DIR
+setgid_all $RAWIMAGES_DIR
+
+chown -R root:root /opt/MagAOX/secrets
+chmod -R u=rwX,g=,o= /opt/MagAOX/secrets
+
+chown -R root:magaox-dev /opt/MagAOX/source
+chmod -R u=rwX,g=rwX,o=rX /opt/MagAOX/source
+setgid_all /opt/MagAOX/source
+
+chown -R root:root /opt/MagAOX/sys
+chmod -R u=rwX,g=rX,o=rX /opt/MagAOX/sys
+
+chown -R root:magaox-dev /opt/MagAOX/vendor
+chmod -R u=rwX,g=rwX,o=rX /opt/MagAOX/vendor
+setgid_all /opt/MagAOX/vendor
