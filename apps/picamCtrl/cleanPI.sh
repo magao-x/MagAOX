47f789d7b11e12aa37a23019b56ba6cdd0cb4aed
Jared Males <jaredmales@gmail.com>
2019-08-03 13:44:08 -0700

    added PI cleanup script

---
+++ apps/picamCtrl/cleanPI.sh
@@ -0,0 +1,6 @@
+sudo rm /dev/shm/PrincetonInstruments\:\:Pi*
+sudo rm /dev/shm/sem.PrincetonInstruments\:\:Pi*
+sudo rm /dev/shm/Pits\:\:WaitForAnySharedMemory*
+sudo rm /dev/shm/sem.*GenICam_XML
+sudo rm /dev/shm/sem.Pits:*
+sudo rm /var/run/pits/*
