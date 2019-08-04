e0719dbfc14d3e270c082b3667e4cd03f89bb6c4
Jared Males <jaredmales@gmail.com>
2019-08-04 14:11:57 -0700

    added mzmqClient, shmimMontir basic imp

---
+++ apps/mzmqClient/mzmqClient.hpp
@@ -0,0 +1,306 @@
+/** \file mzmqClient.hpp
+  * \brief The MagAO-X milkzmqClient wrapper
+  *
+  * \author Jared R. Males (jaredmales@gmail.com)
+  *
+  * \ingroup mzmqClient_files
+  */
+
+#ifndef mzmqClient_hpp
+#define mzmqClient_hpp
+
+
+#include <ImageStruct.h>
+#include <ImageStreamIO.h>
+
+#include <milkzmqClient.hpp>
+
+#include <mx/timeUtils.hpp>
+
+#include "../../libMagAOX/libMagAOX.hpp" //Note this is included on command line to trigger pch
+#include "../../magaox_git_version.h"
+
+
+
+namespace MagAOX
+{
+namespace app
+{
+
+/** \defgroup mzmqClient ImageStreamIO 0mq Stream Client
+  * \brief Reads the contents of an ImageStreamIO image stream over a zeroMQ channel
+  *
+  *  <a href="../apps_html/page_module_mzmqClient.html">Application Documentation</a>
+  *
+  * \ingroup apps
+  *
+  */
+
+/** \defgroup mzmqClient_files ImageStreamIO Stream Synchronization
+  * \ingroup mzmqClient
+  */
+
+/** MagAO-X application to control reading ImageStreamIO streams from a zeroMQ channel
+  *
+  * \ingroup mzmqClient
+  * 
+  */
+class mzmqClient : public MagAOXApp<>, public milkzmq::milkzmqClient
+{
+
+         
+public:
+
+   ///Default c'tor
+   mzmqClient();
+
+   ///Destructor
+   ~mzmqClient() noexcept;
+
+   /// Setup the configuration system (called by MagAOXApp::setup())
+   virtual void setupConfig();
+
+   /// load the configuration system results (called by MagAOXApp::setup())
+   virtual void loadConfig();
+
+   /// Startup functions
+   /** Sets up the INDI vars.
+     *
+     */
+   virtual int appStartup();
+
+   /// Implementation of the FSM for the Siglent SDG
+   virtual int appLogic();
+
+
+   /// Do any needed shutdown tasks.  Currently nothing in this app.
+   virtual int appShutdown();
+   
+protected:
+   
+   std::vector<std::string> m_shMemImNames;
+   
+   /** \name SIGSEGV & SIGBUS signal handling
+     * These signals occur as a result of a ImageStreamIO source server resetting (e.g. changing frame sizes).
+     * When they occur a restart of the framegrabber and framewriter thread main loops is triggered.
+     * 
+     * @{
+     */ 
+   bool m_restart {false};
+   
+   static mzmqClient * m_selfClient; ///< Static pointer to this (set in constructor).  Used for getting out of the static SIGSEGV handler.
+
+   ///Sets the handler for SIGSEGV and SIGBUS
+   /** These are caused by ImageStreamIO server resets.
+     */
+   int setSigSegvHandler();
+   
+   ///The handler called when SIGSEGV or SIGBUS is received, which will be due to ImageStreamIO server resets.  Just a wrapper for handlerSigSegv.
+   static void _handlerSigSegv( int signum,
+                                siginfo_t *siginf,
+                                void *ucont
+                              );
+
+   ///Handles SIGSEGV and SIGBUS.  Sets m_restart to true.
+   void handlerSigSegv( int signum,
+                        siginfo_t *siginf,
+                        void *ucont
+                      );
+   ///@}
+
+
+};
+
+//Set self pointer to null so app starts up uninitialized.
+mzmqClient * mzmqClient::m_selfClient = nullptr;
+
+inline
+mzmqClient::mzmqClient() : MagAOXApp(MAGAOX_CURRENT_SHA1, MAGAOX_REPO_MODIFIED)
+{
+   m_powerMgtEnabled = false;
+ 
+   return;
+}
+
+inline
+mzmqClient::~mzmqClient() noexcept
+{
+   return;
+}
+
+inline
+void mzmqClient::setupConfig()
+{
+   config.add("server.imagePort", "", "imagePort", argType::Required, "server", "imagePort", false, "int", "");
+   
+   config.add("server.shmimNames", "", "server.shmimNames", argType::Required, "server", "shmimNames", false, "string", "");
+   
+   
+ 
+}
+
+
+
+inline
+void mzmqClient::loadConfig()
+{
+   m_argv0 = m_configName;
+   
+   config(m_imagePort, "server.imagePort");
+   
+   config(m_shMemImNames, "server.shmimNames");
+   
+   
+}
+
+
+#include <sys/syscall.h>
+
+inline
+int mzmqClient::appStartup()
+{
+
+   //Now set up the framegrabber and writer threads.
+   // - need SIGSEGV and SIGBUS handling for ImageStreamIO restarts
+   // - initialize the semaphore 
+   // - start the threads
+   
+   if(setSigSegvHandler() < 0)
+   {
+      log<software_error>({__FILE__, __LINE__});
+      return -1;
+   }
+
+   
+   for(size_t n=0; n < m_shMemImNames.size(); ++n)
+   {
+      shMemImName(m_shMemImNames[n]);
+   }
+   
+//    if(serverThreadStart() < 0)
+//    {
+//       log<software_critical>({__FILE__, __LINE__});
+//       return -1;
+//    }
+
+   for(size_t n=0; n < m_imageThreads.size(); ++n)
+   {
+      if( imageThreadStart(n) > 0)
+      {
+         log<software_critical>({__FILE__, __LINE__, "Starting image thread " + m_imageThreads[n].m_imageName});
+         return -1;
+      }
+   }
+   
+
+   std::cerr << "Main Thread: " << syscall(SYS_gettid) << "\n";   
+   return 0;
+
+}
+
+
+
+inline
+int mzmqClient::appLogic()
+{
+   //first do a join check to see if other threads have exited.
+   
+   for(size_t n=0; n < m_imageThreads.size(); ++n)
+   {
+      if(pthread_tryjoin_np(m_imageThreads[n].m_thread->native_handle(),0) == 0)
+      {
+         log<software_error>({__FILE__, __LINE__, "image thread " + m_imageThreads[n].m_imageName + " has exited"});
+      
+         return -1;
+      }
+   }
+   
+   
+   return 0;
+
+}
+
+inline
+int mzmqClient::appShutdown()
+{
+   m_timeToDie = true;
+   
+   for(size_t n=0; n < m_imageThreads.size(); ++n)
+   {
+      if( m_imageThreads[n].m_thread->joinable())
+      {
+         m_imageThreads[n].m_thread->join();
+      }
+   }
+   
+   return 0;
+}
+
+inline
+int mzmqClient::setSigSegvHandler()
+{
+   struct sigaction act;
+   sigset_t set;
+
+   act.sa_sigaction = &mzmqClient::_handlerSigSegv;
+   act.sa_flags = SA_SIGINFO;
+   sigemptyset(&set);
+   act.sa_mask = set;
+
+   errno = 0;
+   if( sigaction(SIGSEGV, &act, 0) < 0 )
+   {
+      std::string logss = "Setting handler for SIGSEGV failed. Errno says: ";
+      logss += strerror(errno);
+
+      log<software_error>({__FILE__, __LINE__, errno, 0, logss});
+
+      return -1;
+   }
+
+   errno = 0;
+   if( sigaction(SIGBUS, &act, 0) < 0 )
+   {
+      std::string logss = "Setting handler for SIGBUS failed. Errno says: ";
+      logss += strerror(errno);
+
+      log<software_error>({__FILE__, __LINE__, errno, 0,logss});
+
+      return -1;
+   }
+
+   log<text_log>("Installed SIGSEGV/SIGBUS signal handler.", logPrio::LOG_DEBUG);
+
+   return 0;
+}
+
+inline
+void mzmqClient::_handlerSigSegv( int signum,
+                                    siginfo_t *siginf,
+                                    void *ucont
+                                  )
+{
+   m_selfClient->handlerSigSegv(signum, siginf, ucont);
+}
+
+inline
+void mzmqClient::handlerSigSegv( int signum,
+                                   siginfo_t *siginf,
+                                   void *ucont
+                                 )
+{
+   static_cast<void>(signum);
+   static_cast<void>(siginf);
+   static_cast<void>(ucont);
+   
+   m_restart = true;
+
+   return;
+}
+
+
+
+
+}//namespace app
+} //namespace MagAOX
+#endif
--- libMagAOX/app/dev/shmimMonitor.hpp
+++ libMagAOX/app/dev/shmimMonitor.hpp
@@ -22,20 +22,20 @@ namespace app
 {
 namespace dev 
 {
-   
-
-
 
 /** MagAO-X generic frame grabber
   *
   * 
   * The derived class `derivedT` must expose the following interface
-  * \code 
+   \code 
     
-  * \endcode  
+   \endcode  
   * Each of the above functions should return 0 on success, and -1 on an error. 
   * 
-  *
+  * This class should be declared a friend in the derived class, like so:
+   \code 
+    friend class dev::shmimMonitor<derivedT>;
+   \endcode
   *
   * Calls to this class's `setupConfig`, `loadConfig`, `appStartup`, `appLogic` and `appShutdown`
   * functions must be placed in the derived class's functions of the same name.
@@ -53,21 +53,19 @@ protected:
    std::string m_shmimName {""}; ///< The name of the shared memory image, is used in `/tmp/<shmimName>.im.shm`. Derived classes should set a default.
       
    int m_smThreadPrio {2}; ///< Priority of the shmimMonitor thread, should normally be > 00.
-    
-    
+   
    ///@}
+   
+   int m_semaphoreNumber {0}; ///< The image structure semaphore index.
    
    uint32_t m_width {0}; ///< The width of the images in the stream
    uint32_t m_height {0}; ///< The height of the images in the stream
    
    uint8_t m_dataType{0}; ///< The ImageStreamIO type code.
    size_t m_typeSize {0}; ///< The size of the type, in bytes.  Result of sizeof.
-   
-   
+
    IMAGE m_imageStream; ///< The ImageStreamIO shared memory buffer.
-   
-   
-   
+
 public:
 
    /// Setup the configuration system
@@ -129,20 +127,47 @@ public:
    
 protected:
    
+   /** \name SIGSEGV & SIGBUS signal handling
+     * These signals occur as a result of a ImageStreamIO source server resetting (e.g. changing frame sizes).
+     * When they occur a restart of the framegrabber and framewriter thread main loops is triggered.
+     * 
+     * @{
+     */ 
+   bool m_restart {false}; ///< Flag indicating tha the shared memory should be reinitialized.
+   
+   static shmimMonitor * m_selfMonitor; ///< Static pointer to this (set in constructor).  Used for getting out of the static SIGSEGV handler.
 
-   /** \name shmimMonitor Thread
-     * This thread waits on the stream semaphore
+   ///Sets the handler for SIGSEGV and SIGBUS
+   /** These are caused by ImageStreamIO server resets.
+     */
+   int setSigSegvHandler();
+   
+   ///The handler called when SIGSEGV or SIGBUS is received, which will be due to ImageStreamIO server resets.  Just a wrapper for handlerSigSegv.
+   static void _handlerSigSegv( int signum,
+                                siginfo_t *siginf,
+                                void *ucont
+                              );
+
+   ///Handles SIGSEGV and SIGBUS.  Sets m_restart to true.
+   void handlerSigSegv( int signum,
+                        siginfo_t *siginf,
+                        void *ucont
+                      );
+   ///@}
+   
+    /** \name shmimmonitor Thread
+     * This thread actually monitors the shared memory buffer
      * @{
      */
    
    bool m_smThreadInit {true}; ///< Synchronizer for thread startup, to allow priority setting to finish.
    
-   std::thread m_smThread; ///< A separate thread for the actual framegrabbings
+   std::thread m_smThread; ///< A separate thread for the actual monitoring
 
    ///Thread starter, called by MagAOXApp::threadStart on thread construction.  Calls smThreadExec.
    static void smThreadStart( shmimMonitor * s /**< [in] a pointer to a shmimMonitor instance (normally this) */);
 
-   /// Execute the thread monitoring
+   /// Execute the monitoring thread
    void smThreadExec();
    
    ///@}
@@ -180,6 +205,12 @@ private:
       return *static_cast<derivedT *>(this);
    }
 };
+
+//Set self pointer to null so app starts up uninitialized.
+template<class derivedT>
+shmimMonitor<derivedT> * shmimMonitor<derivedT>::m_selfMonitor = nullptr;
+
+
 
 template<class derivedT>
 void shmimMonitor<derivedT>::setupConfig(mx::app::appConfigurator & config)
@@ -231,6 +262,14 @@ int shmimMonitor<derivedT>::appStartup()
    m_indiP_frameSize.add(pcf::IndiElement("height"));
    m_indiP_frameSize["height"] = 0;
    
+   if(setSigSegvHandler() < 0)
+   {
+      #ifndef SHMIMMONITOR_TEST_NOLOG
+      derivedT::template log<software_error>({__FILE__,__LINE__});
+      #endif
+      return -1;
+   }
+      
    if( derived().registerIndiPropertyNew( m_indiP_frameSize, nullptr) < 0)
    {
       #ifndef SHMIMMONITOR_TEST_NOLOG
@@ -278,13 +317,71 @@ int shmimMonitor<derivedT>::appShutdown()
       {
       }
    }
-   
-   
-   
+
    return 0;
 }
 
+template<class derivedT>
+int shmimMonitor<derivedT>::setSigSegvHandler()
+{
+   struct sigaction act;
+   sigset_t set;
 
+   act.sa_sigaction = &shmimMonitor<derivedT>::_handlerSigSegv;
+   act.sa_flags = SA_SIGINFO;
+   sigemptyset(&set);
+   act.sa_mask = set;
+
+   errno = 0;
+   if( sigaction(SIGSEGV, &act, 0) < 0 )
+   {
+      std::string logss = "Setting handler for SIGSEGV failed. Errno says: ";
+      logss += strerror(errno);
+
+      derivedT::template log<software_error>({__FILE__, __LINE__, errno, 0, logss});
+
+      return -1;
+   }
+
+   errno = 0;
+   if( sigaction(SIGBUS, &act, 0) < 0 )
+   {
+      std::string logss = "Setting handler for SIGBUS failed. Errno says: ";
+      logss += strerror(errno);
+
+      derivedT::template log<software_error>({__FILE__, __LINE__, errno, 0,logss});
+
+      return -1;
+   }
+
+   derivedT::template log<text_log>("Installed SIGSEGV/SIGBUS signal handler.", logPrio::LOG_DEBUG);
+
+   return 0;
+}
+
+template<class derivedT>
+void shmimMonitor<derivedT>::_handlerSigSegv( int signum,
+                                              siginfo_t *siginf,
+                                              void *ucont
+                                            )
+{
+   m_selfMonitor->handlerSigSegv(signum, siginf, ucont);
+}
+
+template<class derivedT>
+void shmimMonitor<derivedT>::handlerSigSegv( int signum,
+                                             siginfo_t *siginf,
+                                             void *ucont
+                                           )
+{
+   static_cast<void>(signum);
+   static_cast<void>(siginf);
+   static_cast<void>(ucont);
+   
+   m_restart = true;
+
+   return;
+}
 
 template<class derivedT>
 void shmimMonitor<derivedT>::smThreadStart( shmimMonitor * s)
@@ -296,15 +393,153 @@ void shmimMonitor<derivedT>::smThreadStart( shmimMonitor * s)
 template<class derivedT>
 void shmimMonitor<derivedT>::smThreadExec()
 {
-   timespec writestart;
-   
-   //Wait fpr the thread starter to finish initializing this thread.
-   while(m_smThreadInit == true && derived().shutdown() == 0)
+   //Wait for the thread starter to finish initializing this thread.
+   while( m_smThreadInit == true && derived().shutdown() == 0)
    {
       sleep(1);
    }
    
-   while(1);
+   bool opened = false;
+   
+   sem_t * sem {nullptr}; ///< The semaphore to monitor for new image data
+      
+   while(derived().shutdown() == 0)
+   {
+      /* Initialize ImageStreamIO
+       */
+      opened = false;
+      m_restart = false; //Set this up front, since we're about to restart.
+      
+      while(!opened && !derived().m_shutdown && !m_restart)
+      {
+         if( ImageStreamIO_openIm(&m_imageStream, m_shmimName.c_str()) == 0)
+         {
+            if(m_imageStream.md[0].sem <= m_semaphoreNumber) ///<\todo this isn't right--> isn't there a define in cacao to use? 
+            {
+               ImageStreamIO_closeIm(&m_imageStream);
+               mx::sleep(1); //We just need to wait for the server process to finish startup.
+            }
+            else
+            {
+               opened = true;
+            }
+         }
+         else
+         {
+            mx::sleep(1); //be patient
+         }
+      }
+      
+      if(derived().m_shutdown || !opened) return;
+    
+      m_semaphoreNumber = ImageStreamIO_getsemwaitindex(&m_imageStream, m_semaphoreNumber); //ask for semaphore we had before
+      if(m_semaphoreNumber == -1)
+      {
+         derivedT::template log<software_critical>({__FILE__,__LINE__, "could not get semaphore index"});
+         return;
+      }
+      
+      derivedT::template log<software_info>({__FILE__,__LINE__, "got semaphore index " + std::to_string(m_semaphoreNumber) });
+      
+      sem = m_imageStream.semptr[m_semaphoreNumber];
+      
+      m_dataType = m_imageStream.md[0].datatype;
+      m_typeSize = ImageStreamIO_typesize(m_dataType);
+      m_width = m_imageStream.md[0].size[0];
+      m_height = m_imageStream.md[0].size[1];
+      size_t length = m_imageStream.md[0].size[2];
+
+      
+      if( derived().allocate() < 0)
+      {
+         derivedT::template log<software_critical>({__FILE__,__LINE__});
+         break;
+      }
+      
+      uint8_t atype;
+      size_t snx, sny, snz;
+      uint64_t curr_image; //The current cnt1 index
+      
+      //This is the main image grabbing loop.
+      while( derived().shutdown() == 0 && !m_restart)
+      {
+         
+         timespec ts;
+         
+         if(clock_gettime(CLOCK_REALTIME, &ts) < 0)
+         {
+            derivedT::template log<software_critical>({__FILE__,__LINE__,errno,0,"clock_gettime"}); 
+            return;
+         }
+         
+         ts.tv_sec += 1;
+         
+         if(sem_timedwait(sem, &ts) == 0)
+         {
+            if(m_imageStream.md[0].size[2] > 0) ///\todo change to naxis?
+            {
+               curr_image = m_imageStream.md[0].cnt1;
+            }
+            else curr_image = 0;
+
+            atype = m_imageStream.md[0].datatype;
+            snx = m_imageStream.md[0].size[0];
+            sny = m_imageStream.md[0].size[1];
+            snz = m_imageStream.md[0].size[2];
+         
+            if( atype!= m_dataType || snx != m_width || sny != m_height || snz != length )
+            {
+               break; //exit the nearest while loop and get the new image setup.
+            }
+         
+            if(derived().shutdown() != 0 || m_restart) break; //Check for exit signals
+         
+            char * curr_src = (char *)  m_imageStream.array.raw + curr_image*m_width*m_height*m_typeSize;
+            
+            if( derived().processImage(curr_src) < 0)
+            {
+               derivedT::template log<software_error>({__FILE__,__LINE__});
+            }
+         }
+         else
+         {
+            if(m_imageStream.md[0].sem <= 0) break; //Indicates that the server has cleaned up.
+            
+            //Check for why we timed out
+            if(errno == EINTR) break; //This indicates signal interrupted us, time to restart or shutdown, loop will exit normally if flags set.
+            
+            //ETIMEDOUT just means we should wait more.
+            //Otherwise, report an error.
+            if(errno != ETIMEDOUT)
+            {
+               derivedT::template log<software_error>({__FILE__, __LINE__,errno, "sem_timedwait"});
+               break;
+            }
+         }
+      }
+       
+      //*******
+      // call derived().cleanup()
+      //*******
+      
+      if(opened) 
+      {
+         ImageStreamIO_closeIm(&m_imageStream);
+         opened = false;
+      }
+      
+   } //outer loop, will exit if m_shutdown==true
+      
+      
+   //*******
+   // call derived().cleanup()
+   //*******   
+   
+   if(opened) 
+   {
+      ImageStreamIO_closeIm(&m_imageStream);
+      opened = false;
+   }
    
 }
 
