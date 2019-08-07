da0db8035bde9d023f448d409a803328222fb745
Kyle Van Gorkom <kvangorkom@gmail.com>
2019-08-06 19:52:57 -0700

    add miniconda install script

---
+++ setup/steps/install_python.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../_common.sh
+set -euo pipefail
+MINICONDA_VERSION="3-4.7.10"
+#
+# MINICONDA
+#
+MINICONDA_INSTALLER="Miniconda$MINICONDA_VERSION-Linux-x86_64.sh"
+if [[ ! -e $MINICONDA_INSTALLER ]]; then
+    curl -OL "https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER"
+fi
+sudo bash $MINICONDA_INSTALLER -b -p /opt/miniconda3
+# Set environment variables for miniconda
+echo "__conda_setup=\"\$(CONDA_REPORT_ERRORS=false '/opt/miniconda3/bin/conda' shell.bash hook 2> /dev/null)\"
+if [ \$? -eq 0 ]; then
+    \eval \"\$__conda_setup\"
+else
+    if [ -f \"/opt/miniconda3/etc/profile.d/conda.sh\" ]; then
+        . \"/opt/miniconda3/etc/profile.d/conda.sh\"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        \export PATH=\"/opt/miniconda3/bin:\$PATH\"
+    fi
+fi
+unset __conda_setup" > /etc/profile.d/miniconda.sh
+#
+# TO DO: Build the python environment from an environment.yml file
+# TO DO: Manually install pyImageStreamIO in cacao/src
+# TO DO: Clone git repos for any MagAO-X python utils and install
