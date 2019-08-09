3a78f190e86dc14a0e557138189d12f4ba86dab9
Joseph Long <github@joseph-long.com>
2019-08-08 21:07:40 -0700

    Move provisioning things around, hoping for success.

--- setup/steps/install_cuda.sh
+++ setup/steps/install_cuda.sh
@@ -2,6 +2,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
+TARGET_ENV=$1
+
+if [[ "$TARGET_ENV" == "vm" || "$TARGET_ENV" == "ci" ]]; then
+  mkdir -p $HOME/tmp
+  CUDA_FLAGS="--silent --toolkit --tmpdir=$HOME/tmp"
+elif [[ "$TARGET_ENV" == "instrument" ]]; then
+  CUDA_FLAGS="--silent --driver --toolkit"
+else
+  echo "Unknown TARGET_ENV passed as argument 1"
+  exit 1
+fi
 CUDA_PACKAGE_DIR=./cuda
 mkdir -p $CUDA_PACKAGE_DIR
 cd $CUDA_PACKAGE_DIR
@@ -12,7 +23,7 @@ CUDA_RUNFILE=cuda_10.1.168_418.67_linux.run
 CUDA_URL=https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/$CUDA_RUNFILE
 if [[ ! -e /usr/local/cuda-$CUDA_VERSION ]]; then
     _cached_fetch $CUDA_URL $CUDA_RUNFILE
-    sh $CUDA_RUNFILE --silent --driver --toolkit
+    sh $CUDA_RUNFILE $CUDA_FLAGS
 fi
 echo "export CUDADIR=/usr/local/cuda" > /etc/profile.d/cuda.sh
 echo "export CUDA_ROOT=/usr/local/cuda" >> /etc/profile.d/cuda.sh
