f9c900feb23ac4e9bebef7b39a0ad514863cd50d
Joseph Long <github@joseph-long.com>
2019-08-08 23:19:45 -0700

    Typo fix (and cleanup)

--- setup/steps/install_cppzmq.sh
+++ setup/steps/install_cppzmq.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 CPPZMQ_COMMIT=213da0b04ae3b4d846c9abc46bab87f86bfb9cf4
 if [[ ! -d ./cppzmq-$CPPZMQ_COMMIT ]]; then
     _cached_fetch https://github.com/zeromq/cppzmq/archive/$CPPZMQ_COMMIT.tar.gz cppzmq-$CPPZMQ_COMMIT.tar.gz
-    unzip cppzmq-$CPPZMQ_COMMIT.tar.gz
+    tar xzf cppzmq-$CPPZMQ_COMMIT.tar.gz
 fi
 cd ./cppzmq-$CPPZMQ_COMMIT
 sudo cp *.hpp /usr/local/include/
