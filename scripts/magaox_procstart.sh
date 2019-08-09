d3a72cc3f42b6588f6e1d5edfe98dd0a17b39221
Joseph Long <github@joseph-long.com>
2019-08-09 13:27:16 -0700

    New magaox_status.sh script to quickly interrogate process state

--- scripts/magaox_procstart.sh
+++ scripts/magaox_procstart.sh
@@ -11,27 +11,44 @@ set -euo pipefail
 #   $2 is the system name (RTC, ICC, AOC, etc.)
 
 magaox_path=/opt/MagAOX
-
+source $magaox_path/source/MagAOX/scripts/_common.sh
 procname=$1
 
 #TODO: verify that line returned by grep is valid, i.e. first word matches $1, verify that execname exists,.
 execname=$(grep "\b$procname\b" $magaox_path/config/proclist_$2.txt | awk '{print $2}')
 
-echo "process name = $procname"
-echo "executable name = $execname"
+log_info "process name = $procname"
+log_info "executable name = $execname"
 
 #1) check if session exists
 if tmux ls | grep -q "\b$1\b"; then
-   echo "Session $procname exists.  Doing nothing."
+   log_info "Session $procname exists.  Doing nothing."
+   if [[ ! -e $magaox_path/sys/$procname/pid ]]; then
+        log_error "$procname: not started"
+        log_info "To inspect: tmux at -t $procname"
+    else
+        pid=$(cat $magaox_path/sys/$procname/pid)
+        if ps -p $pid > /dev/null; then
+            log_success "$procname: running (pid: $pid)"
+        else
+            log_error "$procname: dead (stale pid: $pid)"
+            log_info "To inspect: tmux at -t $procname"
+        fi
+    fi
    exit 0
 fi
 
 #2) Start new session and execute command
-echo "Session $procname does not exist -> creating and executing"
+log_info "Session $procname does not exist"
 
-command="$magaox_path/bin/$execname -n $1"
-echo "Executing: $command"
+execpath="$magaox_path/bin/$execname"
+if [[ ! -x $execpath ]]; then
+    log_error "No executable $execpath found"
+    exit 1
+fi
+command="$execpath -n $1"
 
 tmux new -s $procname -d
+log_info "Created tmux session \'$procname\'"
 tmux send-keys -t $procname "$command" Enter
-
+log_success "Executed in $procname session: $command"
--- scripts/magaox_shutdown.sh
+++ scripts/magaox_shutdown.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -euo pipefail
-
 magaox_path=/opt/MagAOX
+source $magaox_path/source/MagAOX/scripts/_common.sh
 
 #!/usr/bin/bash
 filename="$magaox_path/config/proclist_$1.txt"
 
-while read -r line; do
-   nocomment=${line%%#*}
-   procname=$(echo $nocomment | awk '{print $1}')
-   
+grep -v '^#' < $filename | { while read -r line; do
+   procname=$(echo $line | awk '{print $1}')
+
    #if not empty, do the shutdown
    if [[ !  -z  ${procname// }  ]]; then
-      tmux send-keys -t $procname C-c
-      tmux send-keys -t $procname "exit" Enter
+      if tmux ls 2>/dev/null | grep $procname > /dev/null; then
+         tmux send-keys -t $procname C-c
+         tmux send-keys -t $procname "exit" Enter
+      else
+         echo "no running tmux session for $procname"
+      fi
+   else
+      echo "Empty procname in line: $line"
    fi
-done < "$filename"
+done }
