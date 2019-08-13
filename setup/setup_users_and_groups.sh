91753f7e469ec16dbe82edd9f612386a3f8769f5
Joseph Long <github@joseph-long.com>
2019-08-13 14:38:55 -0700

    Patch /etc/pam.d/su properly instead of overwriting

--- setup/setup_users_and_groups.sh
+++ setup/setup_users_and_groups.sh
@@ -7,27 +7,13 @@ creategroup magaox
 creategroup magaox-dev
 createuser xsup
 if grep -vq magaox-dev /etc/pam.d/su; then
-  sudo tee /etc/pam.d/su >/dev/null <<EOF
-#%PAM-1.0
-auth            sufficient      pam_rootok.so
+  cat <<'HERE' | sudo sed -i '/pam_rootok.so$/r /dev/stdin' /etc/pam.d/su
 auth            [success=ignore default=1] pam_succeed_if.so user = xsup
 auth            sufficient      pam_succeed_if.so use_uid user ingroup magaox-dev
-# Uncomment the following line to implicitly trust users in the "wheel" group.
-#auth           sufficient      pam_wheel.so trust use_uid
-# Uncomment the following line to require a user to be in the "wheel" group.
-#auth           required        pam_wheel.so use_uid
-auth            substack        system-auth
-auth            include         postlogin
-account         sufficient      pam_succeed_if.so uid = 0 use_uid quiet
-account         include         system-auth
-password        include         system-auth
-session         include         system-auth
-session         include         postlogin
-session         optional        pam_xauth.so
-EOF
-  log_info "Installed new /etc/pam.d/su"
+HERE
+  log_info "Modified /etc/pam.d/su"
 else
-  log_info "/etc/pam.d/su already includes reference to magaox-dev, not overwriting"
+  log_info "/etc/pam.d/su already includes reference to magaox-dev, not modifying"
 fi
 if [[ $EUID != 0 ]]; then
   if [[ -z $(groups | grep magaox-dev) ]]; then
