b34ea66c74a00d0670aaecf01303d768ccc72dcd
Jared Males <jaredmales@gmail.com>
2019-08-26 23:59:07 -0700

    zaberCtrl now INDI compliant

--- apps/zaberCtrl/zaberCtrl.hpp
+++ apps/zaberCtrl/zaberCtrl.hpp
@@ -34,11 +34,13 @@ namespace app
   * 
   * \ingroup zaberCtrl
   */
-class zaberCtrl : public MagAOXApp<>
+class zaberCtrl : public MagAOXApp<>, public dev::stdMotionStage<zaberCtrl>
 {
 
    //Give the test harness access.
    friend class zaberCtrl_test;
+   
+   friend class dev::stdMotionStage<zaberCtrl>;
 
 protected:
 
@@ -99,6 +101,31 @@ public:
      */
    virtual int appShutdown();
 
+   /// Start a high-level homing sequence.
+   /** 
+     * 
+     * \returns 0 on success.
+     * \returns -1 on error.
+     */
+   int startHoming();
+
+   /// Stop the stage motion immediately.
+   /**
+     * \returns 0 on success.
+     * \returns -1 on error.
+     */
+   int stop();
+   
+   double presetNumber();
+   
+   /// Move to a new position in mm.
+   /** 
+     * 
+     * \returns 0 on success.
+     * \returns -1 on error.
+     */
+   int moveTo( const double & pos /**< [in] The new position in mm*/ );
+   
 protected:
 
    //INDI properties for user interaction:   
@@ -106,11 +133,8 @@ protected:
    pcf::IndiProperty m_indiP_rawpos;
    pcf::IndiProperty m_indiP_temp;
    
-   pcf::IndiProperty m_indiP_home;
-   
    INDI_NEWCALLBACK_DECL(zaberCtrl, m_indiP_pos);
    INDI_NEWCALLBACK_DECL(zaberCtrl, m_indiP_rawpos);
-   INDI_NEWCALLBACK_DECL(zaberCtrl, m_indiP_home);
    
    //INDI properties for interacting with Low-Level
    pcf::IndiProperty m_indiP_stageState;
@@ -129,6 +153,7 @@ protected:
 
 zaberCtrl::zaberCtrl() : MagAOXApp(MAGAOX_CURRENT_SHA1, MAGAOX_REPO_MODIFIED)
 {
+   m_defaultPositions = false;
    
    return;
 }
@@ -140,6 +165,8 @@ void zaberCtrl::setupConfig()
    config.add("stage.stageName", "", "stage.stageName", argType::Required, "stage", "stageName", false, "string", "the name of this stage in the low-level process INDI properties.  Default is the configuration name.");
    
    config.add("stage.countsPerMillimeter", "", "stage.countsPerMillimeter", argType::Required, "stage", "countsPerMillimeter", false, "float", "The counts per mm calibration of the stage.  Default is 10078.74.");
+   
+   dev::stdMotionStage<zaberCtrl>::setupConfig(config);
 }
 
 
@@ -153,6 +180,7 @@ int zaberCtrl::loadConfigImpl( mx::app::appConfigurator & _config )
    _config(m_countsPerMillimeter, "stage.countsPerMillimeter");
    
    
+   dev::stdMotionStage<zaberCtrl>::loadConfig(_config);
    
    return 0;
 }
@@ -164,29 +192,50 @@ void zaberCtrl::loadConfig()
 
 int zaberCtrl::appStartup()
 {
-   REG_INDI_NEWPROP(m_indiP_pos, "position", pcf::IndiProperty::Number);
-   m_indiP_pos.add (pcf::IndiElement("current"));
-   m_indiP_pos["current"] = -1;
-   m_indiP_pos.add (pcf::IndiElement("target"));
-   m_indiP_pos.add (pcf::IndiElement("max"));
    
-   REG_INDI_NEWPROP(m_indiP_rawpos, "rawpos", pcf::IndiProperty::Number);
-   m_indiP_rawpos.add (pcf::IndiElement("current"));
-   m_indiP_rawpos["current"] = -1;
-   m_indiP_rawpos.add (pcf::IndiElement("target"));
-   m_indiP_rawpos.add (pcf::IndiElement("max"));
+   createStandardIndiNumber<float>( m_indiP_pos, "position", 0.0, std::numeric_limits<float>::max(), 0.0, "%0.3d");
+   m_indiP_pos["current"].set(0);
+   m_indiP_pos["target"].set(0);
+   if( registerIndiPropertyNew( m_indiP_pos, INDI_NEWCALLBACK(m_indiP_pos)) < 0)
+   {
+      #ifndef ZABERCTRL_TEST_NOLOG
+      log<software_error>({__FILE__,__LINE__});
+      #endif
+      return -1;
+   }
+      
+   createStandardIndiNumber<unsigned long>( m_indiP_rawpos, "rawpos", 0.0, std::numeric_limits<unsigned long>::max(), 0.0, "%lu");
+   m_indiP_rawpos["current"].set(0);
+   m_indiP_rawpos["target"].set(0);
+   if( registerIndiPropertyNew( m_indiP_rawpos, INDI_NEWCALLBACK(m_indiP_rawpos)) < 0)
+   {
+      #ifndef ZABERCTRL_TEST_NOLOG
+      log<software_error>({__FILE__,__LINE__});
+      #endif
+      return -1;
+   }
+      
+   createROIndiNumber( m_indiP_temp, "temp", "Stage Temperature [C]");    
+   indi::addNumberElement( m_indiP_temp, "current", -50,100, 0, "%0.1f");
+   registerIndiPropertyReadOnly(m_indiP_temp);
    
-   REG_INDI_NEWPROP(m_indiP_home, "home", pcf::IndiProperty::Text);
-   m_indiP_home.add (pcf::IndiElement("request"));
-   
-   REG_INDI_NEWPROP_NOCB(m_indiP_temp, "temp", pcf::IndiProperty::Number);
-   m_indiP_temp.add (pcf::IndiElement("current"));
-   
+   //Register for the low level status reports
    REG_INDI_SETPROP(m_indiP_stageState, m_lowLevelName, "curr_state");
    REG_INDI_SETPROP(m_indiP_stageMaxRawPos, m_lowLevelName, std::string("max_pos"));
    REG_INDI_SETPROP(m_indiP_stageRawPos, m_lowLevelName, std::string("curr_pos"));
    REG_INDI_SETPROP(m_indiP_stageTgtPos, m_lowLevelName, std::string("tgt_pos"));
    REG_INDI_SETPROP(m_indiP_stageTemp, m_lowLevelName, std::string("temp"));
+   
+   
+   if(m_presetNames.size() != m_presetPositions.size())
+   {
+      return log<text_log,-1>("must set a position for each preset", logPrio::LOG_CRITICAL);
+   }
+   
+   m_presetNames.insert(m_presetNames.begin(), "none");
+   m_presetPositions.insert(m_presetPositions.begin(), -1);
+   
+   dev::stdMotionStage<zaberCtrl>::appStartup();
    
    return 0;
 }
@@ -207,11 +256,114 @@ int zaberCtrl::appLogic()
 
    //Otherwise we don't do anything differently
    
+   static int last_moving = -1;
+   
+   bool changed = false;
+   if(last_moving != m_moving)
+   {
+      changed = true;
+      last_moving = m_moving;
+   }
+   
+   if(changed)
+   {
+      if(m_moving)
+      {
+         m_indiP_pos.setState(INDI_BUSY);
+         m_indiP_rawpos.setState(INDI_BUSY);
+      }
+      else
+      {
+         m_indiP_pos.setState(INDI_IDLE);
+         m_indiP_pos["target"] = m_pos;
+         m_indiP_rawpos.setState(INDI_IDLE);
+         m_indiP_rawpos["target"] = m_rawPos;
+      }
+      m_indiDriver->sendSetProperty(m_indiP_pos);
+      m_indiDriver->sendSetProperty(m_indiP_rawpos);
+   }
+   
+   int n = presetNumber();
+   if(n == -1)
+   {
+      m_preset = 0;
+      m_preset_target = 0;
+   }
+   else
+   {
+      m_preset = n;
+      m_preset_target = n;
+   }
+
+   dev::stdMotionStage<zaberCtrl>::updateINDI();
+   
+
+      
    return 0;
 }
 
 int zaberCtrl::appShutdown()
 {
+   return 0;
+}
+
+int zaberCtrl::startHoming()
+{
+   updateSwitchIfChanged(m_indiP_home, "request", pcf::IndiElement::Off, INDI_IDLE);
+   
+   pcf::IndiProperty indiP_stageHome = pcf::IndiProperty(pcf::IndiProperty::Text);
+   indiP_stageHome.setDevice(m_lowLevelName);
+   indiP_stageHome.setName("req_home");
+   indiP_stageHome.setPerm(pcf::IndiProperty::ReadWrite); 
+   indiP_stageHome.setState(pcf::IndiProperty::Idle);
+   indiP_stageHome.add(pcf::IndiElement(m_stageName));
+   
+   if( sendNewProperty(indiP_stageHome, m_stageName, "1") < 0 ) return log<software_error,-1>({__FILE__,__LINE__});
+   
+   return 0;
+}
+
+int zaberCtrl::stop()
+{
+   updateSwitchIfChanged(m_indiP_stop, "request", pcf::IndiElement::Off, INDI_IDLE);
+   
+   pcf::IndiProperty indiP_stageHalt = pcf::IndiProperty(pcf::IndiProperty::Text);
+   indiP_stageHalt.setDevice(m_lowLevelName);
+   indiP_stageHalt.setName("req_halt");
+   indiP_stageHalt.setPerm(pcf::IndiProperty::ReadWrite); 
+   indiP_stageHalt.setState(pcf::IndiProperty::Idle);
+   indiP_stageHalt.add(pcf::IndiElement(m_stageName));
+   
+   if( sendNewProperty(indiP_stageHalt, m_stageName, "1") < 0 ) return log<software_error,-1>({__FILE__,__LINE__});
+   
+   return 0;
+}
+
+double zaberCtrl::presetNumber()
+{
+   for( size_t n=1; n < m_presetPositions.size(); ++n)
+   {
+      if( fabs(m_pos-m_presetPositions[n]) < 1./m_countsPerMillimeter) return n;
+   }
+   
+   return 0;
+}
+
+int zaberCtrl::moveTo( const double & target)
+{
+   if(target < 0) return 0;
+   
+   long tgt = (target*m_countsPerMillimeter + 0.5);
+   
+   pcf::IndiProperty indiP_stageTgtPos = pcf::IndiProperty(pcf::IndiProperty::Number);
+   indiP_stageTgtPos.setDevice(m_lowLevelName);
+   indiP_stageTgtPos.setName("tgt_pos");
+   indiP_stageTgtPos.setPerm(pcf::IndiProperty::ReadWrite); 
+   indiP_stageTgtPos.setState(pcf::IndiProperty::Idle);
+   indiP_stageTgtPos.add(pcf::IndiElement(m_stageName));
+   
+   if( sendNewProperty(indiP_stageTgtPos, m_stageName, tgt) < 0 ) return log<software_error,-1>({__FILE__,__LINE__});
+   
    return 0;
 }
 
@@ -242,19 +394,7 @@ INDI_NEWCALLBACK_DEFN( zaberCtrl, m_indiP_pos)(const pcf::IndiProperty &ipRecv)
       }
    }
       
-   long tgt = (target*m_countsPerMillimeter + 0.5);
-   
-   pcf::IndiProperty indiP_stageTgtPos = pcf::IndiProperty(pcf::IndiProperty::Text);
-   indiP_stageTgtPos.setDevice(m_lowLevelName);
-   indiP_stageTgtPos.setName("tgt_pos");
-   indiP_stageTgtPos.setPerm(pcf::IndiProperty::ReadWrite); 
-   indiP_stageTgtPos.setState(pcf::IndiProperty::Idle);
-   indiP_stageTgtPos.add(pcf::IndiElement(m_stageName));
-   
-   if( sendNewProperty(indiP_stageTgtPos, m_stageName, tgt) < 0 ) return log<software_error,-1>({__FILE__,__LINE__});
-   
-
-   return 0;
+   return moveTo(target);
 }
 
 INDI_NEWCALLBACK_DEFN( zaberCtrl, m_indiP_rawpos)(const pcf::IndiProperty &ipRecv)
@@ -297,30 +437,7 @@ INDI_NEWCALLBACK_DEFN( zaberCtrl, m_indiP_rawpos)(const pcf::IndiProperty &ipRec
    return 0;
 }
 
-INDI_NEWCALLBACK_DEFN( zaberCtrl, m_indiP_home)(const pcf::IndiProperty &ipRecv)
-{
-   if( ipRecv.getName() != m_indiP_home.getName())
-   {
-      log<software_error>({__FILE__, __LINE__, "Invalid INDI property."});
-      return -1;
-   }
-   
-   if( !ipRecv.find("request") )
-   {
-      return 0;
-   }
-   
-   pcf::IndiProperty indiP_stageHome = pcf::IndiProperty(pcf::IndiProperty::Text);
-   indiP_stageHome.setDevice(m_lowLevelName);
-   indiP_stageHome.setName("req_home");
-   indiP_stageHome.setPerm(pcf::IndiProperty::ReadWrite); 
-   indiP_stageHome.setState(pcf::IndiProperty::Idle);
-   indiP_stageHome.add(pcf::IndiElement(m_stageName));
-   
-   if( sendNewProperty(indiP_stageHome, m_stageName, "1") < 0 ) return log<software_error,-1>({__FILE__,__LINE__});
-   
-   return 0;
-}
+
 
 INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageState)(const pcf::IndiProperty &ipRecv)
 {
@@ -339,14 +456,41 @@ INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageState)(const pcf::IndiProperty &i
    
    std::string sstr = ipRecv[m_stageName].get<std::string>();
    
-   if(sstr == "POWEROFF") state(stateCodes::POWEROFF);
-   if(sstr == "POWERON") state(stateCodes::POWERON);
-   if(sstr == "NOTHOMED") state(stateCodes::NOTHOMED);
-   if(sstr == "HOMING") state(stateCodes::HOMING);
-   if(sstr == "READY") state(stateCodes::READY);
-   if(sstr == "OPERATING") state(stateCodes::OPERATING);
-   if(sstr == "SHUTDOWN") state(stateCodes::NOTCONNECTED);
-   
+   if(sstr == "POWEROFF") 
+   {
+      state(stateCodes::POWEROFF);
+      m_moving = 0;
+   }
+   else if(sstr == "POWERON") 
+   {
+      state(stateCodes::POWERON);
+      m_moving = 0;
+   }
+   else if(sstr == "NOTHOMED") 
+   {
+      state(stateCodes::NOTHOMED);
+      m_moving = 0;
+   }
+   else if(sstr == "HOMING") 
+   {
+      state(stateCodes::HOMING);
+      m_moving = 1;
+   }
+   else if(sstr == "READY") 
+   {
+      state(stateCodes::READY);
+      m_moving = 0;
+   }
+   else if(sstr == "OPERATING") 
+   {
+      state(stateCodes::OPERATING);
+      m_moving = 1;
+   }
+   else if(sstr == "SHUTDOWN") 
+   {
+      state(stateCodes::NOTCONNECTED);
+      m_moving = 0;
+   }
 
    return 0;
 }
@@ -368,8 +512,8 @@ INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageMaxRawPos )(const pcf::IndiProper
    
    m_maxPos = m_maxRawPos / m_countsPerMillimeter;
 
-   updateIfChanged(m_indiP_rawpos, "max", m_maxRawPos);   
-   updateIfChanged(m_indiP_pos, "max", m_maxPos);
+   //updateIfChanged(m_indiP_rawpos, "max", m_maxRawPos);   
+   //updateIfChanged(m_indiP_pos, "max", m_maxPos);
    
    
 
@@ -393,9 +537,16 @@ INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageRawPos )(const pcf::IndiProperty 
    
    m_pos = m_rawPos / m_countsPerMillimeter;
 
-   updateIfChanged(m_indiP_rawpos, "current", m_rawPos);   
-   updateIfChanged(m_indiP_pos, "current", m_pos);
-   
+   if(m_moving)
+   {
+      updateIfChanged(m_indiP_rawpos, "current", m_rawPos, INDI_BUSY);   
+      updateIfChanged(m_indiP_pos, "current", m_pos, INDI_BUSY);
+   }
+   else
+   {
+      updateIfChanged(m_indiP_rawpos, "current", m_rawPos, INDI_IDLE);   
+      updateIfChanged(m_indiP_pos, "current", m_pos, INDI_IDLE);
+   }
    
 
    return 0;
@@ -419,17 +570,25 @@ INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageTgtPos )(const pcf::IndiProperty 
    
    if(test == "")
    {
-      updateIfChanged(m_indiP_rawpos, "target", std::string(""));   
-      updateIfChanged(m_indiP_pos, "target", std::string(""));
+//       updateIfChanged(m_indiP_rawpos, "target", std::string(""));   
+//       updateIfChanged(m_indiP_pos, "target", std::string(""));
       return 0;
    }
    
    m_tgtRawPos = ipRecv[m_stageName].get<double>();
    m_tgtPos = m_tgtRawPos / m_countsPerMillimeter;
    
-   updateIfChanged(m_indiP_rawpos, "target", m_tgtRawPos);   
-   updateIfChanged(m_indiP_pos, "target", m_tgtPos);
-
+   if(m_moving)
+   {
+      updateIfChanged(m_indiP_rawpos, "target", m_tgtRawPos, INDI_BUSY);   
+      updateIfChanged(m_indiP_pos, "target", m_tgtPos, INDI_BUSY);
+   }
+   else
+   {
+      updateIfChanged(m_indiP_rawpos, "target", m_tgtRawPos, INDI_IDLE);   
+      updateIfChanged(m_indiP_pos, "target", m_tgtPos, INDI_IDLE);
+   }
+   
    return 0;
 }
 
