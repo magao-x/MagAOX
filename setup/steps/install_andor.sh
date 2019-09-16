a8ae3df7bde26f9d6dc504516d85dce0234fdf45
Joseph Long <github@joseph-long.com>
2019-09-16 11:04:00 -0700

    Automate Andor installation

--- setup/steps/install_andor.sh
+++ setup/steps/install_andor.sh
@@ -2,4 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
-echo "This is where I'll put my Andor install script"
+cd /opt/MagAOX/vendor/andor
+sudo patch -Np1 < unattended_install.patch || true
+sudo bash install_andor
+log_info "install_andor complete"
