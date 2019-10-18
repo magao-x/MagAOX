5590062ba971184564dafcd71e6e08674b5e3f9b
Joseph Long <github@joseph-long.com>
2019-10-18 11:45:40 -0700

    install_python.sh: fold conda env creation back into install_python.sh

--- setup/steps/install_python.sh
+++ setup/steps/install_python.sh
@@ -37,7 +37,69 @@ channels:
 changeps1: false
 EOF
 fi
+
+#
+# Create the standard MagAOX user python environment
+#
+ENVS=$(conda env list)
+if [[ $ENVS != *py37* ]]; then
+	conda env create -f /opt/MagAOX/config/conda_env_py37.yml
+	log_success "created conda env py37"
+else
+	log_info "py37 environment already exists"
+fi
+
+#
+# Clone to create a development environment
+#
+if [[ $ENVS != *dev* ]]; then
+	conda create --name dev --clone py37
+	log_success "created conda env dev from env py37"
+else
+	log_info "dev environment already exists"
+fi
+
+#
+# Set up auto-starting xsup Jupyter Lab instance
+#
+if [[ $MAGAOX_ROLE == vm ]]; then
+	JUPYTERLAB_OPTIONS='--ip="0.0.0.0"'
+else
+	JUPYTERLAB_OPTIONS=''
+fi
+JUPYTERLAB_CONFIG_PATH=/opt/MagAOX/config/jupyter_notebook_config.py
+
+# Note that there's a race condition where /vagrant isn't available yet
+# when jupyterlab tries to start, so we make a copy within the VM's local
+# storage.
+if [[ $MAGAOX_ROLE == vm ]]; then
+	cp $JUPYTERLAB_CONFIG_PATH /opt/miniconda3/envs/py37/etc/jupyter_notebook_config.py
+	JUPYTERLAB_CONFIG_PATH=/opt/miniconda3/envs/py37/etc/jupyter_notebook_config.py
+fi
+JUPYTER_SCRIPT=/opt/miniconda3/envs/py37/bin/start_jupyterlab.sh
+sudo tee $JUPYTER_SCRIPT >/dev/null <<HERE
+#!/bin/bash
+source /etc/profile
+/opt/miniconda3/envs/py37/bin/jupyter lab --config=$JUPYTERLAB_CONFIG_PATH $JUPYTERLAB_OPTIONS
+HERE
+chmod +x $JUPYTER_SCRIPT
+UNIT_PATH=/etc/systemd/system/
+
+if [[ $MAGAOX_ROLE != RTC && $MAGAOX_ROLE != ICC && $MAGAOX_ROLE != ci ]]; then
+	cp /opt/MagAOX/config/jupyterlab.service $UNIT_PATH/jupyterlab.service
+	log_success "Installed jupyterlab.service to $UNIT_PATH"
+	if [[ $MAGAOX_ROLE == vm ]]; then
+		sed -iE "s_WorkingDirectory=/home/xsup_WorkingDirectory=/vagrant_g" $UNIT_PATH/jupyterlab.service
+		sed -iE "s/xsup/vagrant/g" $UNIT_PATH/jupyterlab.service
+		log_info "Rewrote service for vagrant"
+	fi
+	systemctl enable jupyterlab
+	log_success "Enabled jupyterlab service"
+	systemctl start jupyterlab
+	log_success "Started jupyterlab service"
+fi
+
+
 # set group and permissions such that only magaox-dev has write access
 chgrp -R magaox-dev /opt/miniconda3
 chmod -R g=rwX /opt/miniconda3
-setgid_all /opt/miniconda3
