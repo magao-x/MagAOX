746312e05075336fc8b9695fa79e5907158432a9
Joseph Long <github@joseph-long.com>
2019-11-12 19:53:56 -0300

    Install Docker deps manually

--- setup/steps/install_docker.sh
+++ setup/steps/install_docker.sh
@@ -9,8 +9,15 @@ source $DIR/../_common.sh
 set -euo pipefail
 # See https://docs.docker.com/install/linux/docker-ce/centos/#install-from-a-package
 cd /opt/MagAOX/vendor
-DOCKER_RPMFILE=docker-ce-19.03.4-3.el7.x86_64.rpm
-_cached_fetch https://download.docker.com/linux/centos/7/x86_64/stable/Packages/$DOCKER_RPMFILE $DOCKER_RPMFILE
-yum install -y $DOCKER_RPMFILE || yum upgrade -y $DOCKER_RPMFILE
+BASEURL="https://download.docker.com/linux/centos/7/x86_64/stable/Packages"
+DOCKER_RPMFILE="docker-ce-19.03.4-3.el7.x86_64.rpm"
+CONTAINERD_RPMFILE="containerd.io-1.2.6-3.3.el7.x86_64.rpm"
+DOCKERCLI_RPMFILE="docker-ce-cli-19.03.4-3.el7.x86_64.rpm"
+
+_cached_fetch $BASEURL/$CONTAINERD_RPMFILE $CONTAINERD_RPMFILE
+_cached_fetch $BASEURL/$DOCKERCLI_RPMFILE $DOCKERCLI_RPMFILE
+_cached_fetch $BASEURL/$DOCKER_RPMFILE $DOCKER_RPMFILE
+
+yum install -y $DOCKER_RPMFILE $DOCKERCLI_RPMFILE $CONTAINERD_RPMFILE || yum upgrade -y $DOCKER_RPMFILE $DOCKERCLI_RPMFILE $CONTAINERD_RPMFILE
 systemctl start docker
 docker run hello-world
