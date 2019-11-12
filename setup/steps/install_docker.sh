090e8874e8304f541f37157c094ae528d9af011c
Joseph Long <github@joseph-long.com>
2019-11-12 19:47:07 -0300

    add install Docker script

---
+++ setup/steps/install_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# If not started as root, sudo yourself
+if [[ "$EUID" != 0 ]]; then
+    sudo bash -l $0 "$@"
+    exit $?
+fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../_common.sh
+set -euo pipefail
+# See https://docs.docker.com/install/linux/docker-ce/centos/#install-from-a-package
+cd /opt/MagAOX/vendor
+DOCKER_RPMFILE=docker-ce-19.03.4-3.el7.x86_64.rpm
+_cached_fetch https://download.docker.com/linux/centos/7/x86_64/stable/Packages/$DOCKER_RPMFILE
+yum upgrade -y $DOCKER_RPMFILE
+systemctl start docker
+docker run hello-world
