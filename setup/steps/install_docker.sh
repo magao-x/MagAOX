64c722ddee1aef825ff6f334ed64307713baf9f8
Joseph Long <github@joseph-long.com>
2019-11-12 19:48:52 -0300

    Typo fix

--- setup/steps/install_docker.sh
+++ setup/steps/install_docker.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 # See https://docs.docker.com/install/linux/docker-ce/centos/#install-from-a-package
 cd /opt/MagAOX/vendor
 DOCKER_RPMFILE=docker-ce-19.03.4-3.el7.x86_64.rpm
-_cached_fetch https://download.docker.com/linux/centos/7/x86_64/stable/Packages/$DOCKER_RPMFILE
+_cached_fetch https://download.docker.com/linux/centos/7/x86_64/stable/Packages/$DOCKER_RPMFILE $DOCKER_RPMFILE
 yum upgrade -y $DOCKER_RPMFILE
 systemctl start docker
 docker run hello-world
