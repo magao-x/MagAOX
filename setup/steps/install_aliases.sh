8e3c9f3fbe24dbf5beba2b43261f17cf892d1146
Jared Males <jaredmales@gmail.com>
2020-07-19 18:36:25 -0700

    updated teldump to point at /opt/MagAOX

--- setup/steps/install_aliases.sh
+++ setup/steps/install_aliases.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "alias teldump='logdump --dir=/data/telem --ext=.bintel'" | sudo tee /etc/profile.d/teldump.sh
+echo "alias teldump='logdump --dir=/opt/MagAOX/telem --ext=.bintel'" | sudo tee /etc/profile.d/teldump.sh
