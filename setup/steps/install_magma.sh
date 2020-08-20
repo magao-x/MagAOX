57e4e2371f54c8d7a3634f5c6bacee59e9cd0d49
Joseph Long <me@joseph-long.com>
2020-08-20 14:19:54 -0700

    magma install path cd

--- setup/steps/install_magma.sh
+++ setup/steps/install_magma.sh
@@ -7,6 +7,7 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
+cd /opt/MagAOX/vendor
 MAGMA_VERSION=2.5.3
 MAGMA_FOLDER=./magma-$MAGMA_VERSION
 if [[ ! -d $MAGMA_FOLDER ]]; then
