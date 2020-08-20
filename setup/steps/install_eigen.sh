d90febd92f10e019a9f4685f8708a0c5aff576a6
Joseph Long <github@joseph-long.com>
2020-08-20 14:29:50 -0700

    Update Eigen extraction result dir name

--- setup/steps/install_eigen.sh
+++ setup/steps/install_eigen.sh
@@ -7,9 +7,11 @@ EIGEN_VERSION="3.3.4"
 # Eigen
 #
 if [[ ! -e /usr/local/include/Eigen ]]; then
-    _cached_fetch https://gitlab.com/libeigen/eigen/-/archive/$EIGEN_VERSION/eigen-$EIGEN_VERSION.tar.gz eigen-$EIGEN_VERSION.tar.gz
-    tar xzf eigen-$EIGEN_VERSION.tar.gz
-    EIGEN_DIR=$(realpath $(find . -type d -name "eigen-eigen-*" | head -n 1))
+    EIGEN_DIR="/opt/MagAOX/vendor/eigen-$EIGEN_VERSION"
+    if [[ ! -d $EIGEN_DIR ]]; then
+        _cached_fetch https://gitlab.com/libeigen/eigen/-/archive/$EIGEN_VERSION/eigen-$EIGEN_VERSION.tar.gz eigen-$EIGEN_VERSION.tar.gz
+        tar xzf eigen-$EIGEN_VERSION.tar.gz
+    fi
     ln -sv "$EIGEN_DIR/Eigen" "/usr/local/include/Eigen"
     echo "/usr/local/include/Eigen is now a symlink to $EIGEN_DIR"
 fi
