c3c7bb72cd3cf61b1a273ed7225760cc6d182513
Joseph Long <github@joseph-long.com>
2020-08-20 11:58:33 -0700

    Change the way versions are pinned for tuned and kernel-rt

--- setup/steps/install_rt_kernel.sh
+++ setup/steps/install_rt_kernel.sh
@@ -16,32 +16,11 @@ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 EOF
 
 sudo yum update -y || true
-if ! rpm -q tuned-2.9.0-1.el7fdp.noarch; then
-    # Newer version installed by default, too new for the tuned-profiles-realtime package:
-    sudo yum remove -y tuned
-fi
-sudo yum install -y tuned-2.9.0-1.el7fdp.noarch
+# Newer version installed by default, but the tuned-profiles-realtime package lags, so reinstall:
+sudo yum remove -y tuned
+sudo yum install -y tuned-profiles-realtime
 sudo yum versionlock tuned
-sudo yum install -y kernel-rt-3.10.0-957.21.3.rt56.935.el7 kernel-rt-devel-3.10.0-957.21.3.rt56.935.el7
+# Install latest kernel and pin it if we're installing the first time:
+sudo yum install -y kernel-rt kernel-rt-devel
 sudo yum versionlock kernel-rt
 sudo yum versionlock kernel
-# Dependencies Resolved
-
-# =====================================================================================================
-#  Package                        Arch          Version                              Repository   Size
-# =====================================================================================================
-# Installing:
-#  kernel-rt                      x86_64        3.10.0-957.21.3.rt56.935.el7         rt           45 M
-# Installing for dependencies:
-#  python-ethtool                 x86_64        0.8-8.el7                            base         34 k
-#  rt-setup                       x86_64        2.0-6.el7                            rt           20 k
-#  tuna                           noarch        0.13-9.el7                           base        141 k
-#  tuned-profiles-realtime        noarch        2.9.0-1.el7fdp                       rt           26 k
-
-# Transaction Summary
-# =====================================================================================================
-# Install  1 Package (+4 Dependent packages)
-
-# Total download size: 45 M
-# Installed size: 169 M
-# Is this ok [y/d/N]:
