5458a2095b86f4c7109bf9e17dcd060562c7fb2e
Jacob <jtrzaska@email.arizona.edu>
2021-06-24 03:22:37 +0000

    Improved the GaussFit() function to handle rotated and asymmetric gaussians.

--- apps/camtipCorr/cc-functions.cpp
+++ apps/camtipCorr/cc-functions.cpp
@@ -1,5 +1,6 @@
 #include "cc-functions.hpp"
 
+#define NEW_TEST 1
 
 void GaussFit(size_t ROWS,    // number of rows in input 
               size_t COLS,    // number of cols in input 
@@ -8,77 +9,124 @@ void GaussFit(size_t ROWS,    // number of rows in input
               double *result  // holds results of the fit 
               )
 {
-        Eigen::MatrixXd coeff(4,4);
-        Eigen::VectorXd b(4);
-        int x[2*SZ+1];
-        int y[2*SZ+1];
-        int di=0, dj=0;
-        double *image2 = (double *) malloc((2*SZ+1)*(2*SZ+1)*sizeof(double));
+   size_t range = ((2 * SZ) + 1);
+
+   Eigen::MatrixXd coeff(6,6);
+   Eigen::VectorXd b(6);
+   int x[range];
+   int y[range];
+   int di {0}, dj {0};
+   double *image2 = (double *) malloc((2*SZ+1)*(2*SZ+1)*sizeof(double));
     
-        // initialize matrix 
-        for (size_t i=0; i<4; i++)
-                for (size_t j=0; j<4; j++)
-                        coeff(i,j) = 0;
+   // initialize matrix 
+   for (size_t i {0}; i < 6; ++i)
+      for (size_t j {0}; j < 6; ++j)
+         coeff(i,j) = 0;
 
-        for (size_t i=0; i<4; i++) { b(i)=0; }
+   for (size_t i {0}; i < 6; ++i) { b(i) = 0; }
  
-        locate_max(ROWS, COLS, input, &di, &dj);
-        // crop (2*SZ+1)x(2*SZ+1) into image
-        crop_image(image2, input, ROWS, COLS, dj, di, SZ);
+   locate_max(ROWS, COLS, input, &di, &dj);
+   // crop (2*SZ+1)x(2*SZ+1) into image
+   crop_image(image2, input, ROWS, COLS, dj, di, SZ);
 
-        // linearize the data in the (2*SZ+1)x(2*SZ+1) grid
-        for (size_t i=0; i<2*SZ+1; i++)
-                for (size_t j=0; j<2*SZ+1; j++)
-                        image2[j+i*(2*SZ+1)] 
-                                = log(image2[j+i*(2*SZ+1)]/(ROWS*COLS));
+   // linearize the data in the (2*SZ+1)x(2*SZ+1) grid
+   for (size_t i {0}; i < range; ++i)
+      for (size_t j {0}; j < range; ++j)
+         image2[j + i * range] = log(image2[j+i*range]/(ROWS*COLS));
 
-        for (size_t i=0; i<2*SZ+1; i++) {
-                x[i] = dj-SZ+i;
-                y[i] = di-SZ+i;
-        } 
+   for (size_t i {0}; i < range; ++i) {
+      x[i] = dj-SZ+i;
+      y[i] = di-SZ+i;
+   } 
 
 
-        // setup least squares matrix and vector
-        coeff(0,0) = (2*SZ+1) * (2*SZ+1);
+   /* Setup the least-square regression matrix */
 
-        for (size_t a=0; a<(2*SZ+1); a++){
-                coeff(0,1) += x[a];
-                coeff(0,2) += y[a];
-                coeff(1,1) += x[a]*x[a];
-                coeff(2,2) += y[a]*y[a];
-        }
+   // Calculate the single-sum elements
+   double tx, ty;
+   coeff(5,5) = range * range;
 
-        coeff(0,1) = (2*SZ+1)*coeff(0,1);
-        coeff(0,2) = (2*SZ+1)*coeff(0,2);
-        coeff(1,1) = (2*SZ+1)*coeff(1,1);
-        coeff(2,2) = (2*SZ+1)*coeff(2,2);
+   for (size_t a {0}; a < range; ++a) {
+      tx = x[a] * x[a];
+      ty = y[a] * y[a];
+      coeff(0,0) += tx *tx;
+      coeff(2,2) += ty * ty;
+      coeff(3,3) += tx;
+      coeff(4,4) += ty;
+      coeff(4,5) += y[a];
+      coeff(3,5) += x[a];
+      coeff(0,3) += tx * x[a];
+      coeff(2,4) += ty * y[a];
+   }
 
-        coeff(0,3) = (coeff(1,1) + coeff(2,2));
 
-        double t;
-        for (size_t i=0; i<(2*SZ+1); i++)
-                for (size_t j=0; j<(2*SZ+1); j++){
-                        coeff(1,2) += x[j]*y[i];
-                        t = x[j]*x[j]+y[i]*y[i];
-                        coeff(1,3) += t*x[j];
-                        coeff(2,3) += t*y[i];
-                        coeff(3,3) += t*t;
+      coeff(0,0) = range * coeff(0,0);
+      coeff(2,2) = range * coeff(2,2);
+      coeff(3,3) = range * coeff(3,3);
+      coeff(4,4) = range * coeff(4,4);
+      coeff(4,5) = range * coeff(4,5);
+      coeff(3,5) = range * coeff(3,5);
+      coeff(0,3) = range * coeff(0,3);
+      coeff(2,4) = range * coeff(2,4);
 
-                        b(0) += image2[j+i*(2*SZ+1)];
-                        b(1) += x[j]*image2[j+i*(2*SZ+1)];
-                        b(2) += y[i]*image2[j+i*(2*SZ+1)];
-                        b(3) += t*image2[j+i*(2*SZ+1)];
-                }
+   // Use redundancy in upper triangle
+   coeff(0,5) = coeff(3,3);
+   coeff(2,5) = coeff(4,4);
 
-        // exploit matrix symmetry
-        coeff(1,0)=coeff(0,1); coeff(2,0)=coeff(0,2); coeff(3,0)=coeff(0,3);
-        coeff(2,1)=coeff(1,2); coeff(3,1)=coeff(1,3); coeff(3,2)=coeff(2,3);
+   // ca
+   double txy;
+   for (size_t i {0}; i < range; ++i)
+      for (size_t j {0}; j < range; ++j) {
+         tx  = x[j] * x[j];
+         ty  = y[i] * y[i];
+         txy = x[j] * y[i];
+         coeff(0,1) += tx * txy;
+         coeff(1,1) += tx * ty;
+         coeff(1,2) += txy * ty;
+         coeff(1,3) += tx * y[i];
+         coeff(2,3) += x[j] * ty;
+         coeff(3,4) += txy;
 
-        // solve system with Eigen tools; calculate shifts
-        Eigen::VectorXd v(4);
-        v = coeff.fullPivHouseholderQr().solve(b);                    
-        result[0] = -0.5*v(2)/v(3); // y-component
-        result[1] = -0.5*v(1)/v(3); // x-component
+         b(0) +=   tx * image2[j + i * range];
+         b(1) +=  txy * image2[j + i * range];
+         b(2) +=   ty * image2[j + i * range];
+         b(3) += x[j] * image2[j + i * range];
+         b(4) += y[i] * image2[j + i * range];
+         b(5) +=        image2[j + i * range];
+      }
+
+   // Use redundancy of upper triangle
+   coeff(0,2) = coeff(1,1);
+   coeff(0,4) = coeff(1,3);
+   coeff(1,4) = coeff(2,3);
+   coeff(1,5) = coeff(3,4);
+
+   // Use matrix symmetry
+   coeff(1,0) = coeff(0,1);
+   coeff(2,0) = coeff(0,2);
+   coeff(3,0) = coeff(0,3);
+   coeff(4,0) = coeff(0,4);
+   coeff(5,0) = coeff(0,5);
+
+   coeff(2,1) = coeff(1,2);
+   coeff(3,1) = coeff(1,3);
+   coeff(4,1) = coeff(1,4);
+   coeff(5,1) = coeff(1,5);
+
+   coeff(3,2) = coeff(2,3);
+   coeff(4,2) = coeff(2,4);
+   coeff(5,2) = coeff(2,5);
+
+   coeff(4,3) = coeff(3,4);
+   coeff(5,3) = coeff(3,5);
+
+   coeff(5,4) = coeff(4,5);   
+
+   // solve system with Eigen tools; calculate shifts
+   Eigen::VectorXd v(6);
+   v = coeff.fullPivHouseholderQr().solve(b);                    
+   result[0] = (v(0)*v(4) - 2*v(1)*v(3)) / (v(1)*v(1) - 4*v(0)*v(2)); // y-component
+   result[1] = (2*v(2)*v(3) - v(1)*v(4)) / (v(1)*v(1) - 4*v(0)*v(2)); // x-component
 }
 
 
