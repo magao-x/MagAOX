de42607a81221d974c2d1014fcd12e8ed77ca360
Joseph Long <github@joseph-long.com>
2021-08-19 12:21:55 -0700

    setup: add configure_nfs to provision.sh

--- setup/steps/configure_nfs.sh
+++ setup/steps/configure_nfs.sh
@@ -18,7 +18,7 @@ if [[ $MAGAOX_ROLE == AOC ]]; then
     for remote in rtc icc; do
         for path in /data/logs /data/rawimages /data/telem; do
             if ! grep -q "$remote:$path" /etc/fstab; then
-                mountpoint="/data/$remote$path"
+                mountpoint="/data/$remote${path/\/data\///}"
                 sudo mkdir -p $mountpoint
                 echo "$remote:$path $mountpoint	nfs	defaults	0 0" | sudo tee -a /etc/fstab
                 sudo mount $mountpoint || true
