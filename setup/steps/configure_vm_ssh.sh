46b7bfd6d47e2139e32e70e857ad745193db985f
Joseph Long <me@joseph-long.com>
2022-03-19 22:51:47 -0700

    Edits to build under WSL

--- setup/steps/configure_vm_ssh.sh
+++ setup/steps/configure_vm_ssh.sh
@@ -5,11 +5,16 @@ set -euo pipefail
 #
 # Pre-populate known hosts and hostname aliases for SSH tunneling from the VM
 #
-if [[ $MAGAOX_ROLE == vm ]]; then
-  mkdir -p /vagrant/vm/ssh
-  if [[ ! -e /vagrant/vm/ssh/config ]]; then
-    cat <<'HERE' | sudo tee /vagrant/vm/ssh/config
-IdentityFile /vagrant/vm/ssh/magaox_ssh_key
+if [[ $VM_KIND == "vagrant" ]]; then
+  username=vagrant
+else
+  username=$USER
+fi
+if [[ $MAGAOX_ROLE == vm && $VM_WINDOWS_HOST == 0 ]]; then
+  mkdir -p "$VM_SHARED_FOLDER/ssh"
+  if [[ ! -e "$VM_SHARED_FOLDER/ssh/config" ]]; then
+    cat << "HERE" | sudo tee "$VM_SHARED_FOLDER/ssh/config"
+IdentityFile $VM_SHARED_FOLDER/ssh/magaox_ssh_key
 Host aoc
   HostName exao1.magao-x.org
 Host rtc
@@ -21,26 +26,26 @@ Host icc
 Host tic
   HostName exao0.as.arizona.edu
 Host *
-  User YOURUSERNAME
+  User YOURMAGAOXUSERNAME
 HERE
   fi
-  mkdir -p /home/vagrant/.ssh
-  if [[ ! -e /home/vagrant/.ssh/known_hosts ]]; then
-      cat <<'HERE' | sudo tee /home/vagrant/.ssh/known_hosts
+  mkdir -p /home/$username/.ssh
+  if [[ ! -e /home/$username/.ssh/known_hosts ]]; then
+      cat <<'HERE' | sudo tee /home/$username/.ssh/known_hosts
 rtc ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFmgoTzcAVYXDZjPFNLfpPz/T/0DQvrXSe9XOly9SD7NcjwN/fRTk+DhrWzdPN5aBsDnnmMS8lFGIcRwnlhUN6o=
 icc ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNpRRN65o8TcP2DnkXHdzIqAJ9CAoiz2guLSXjobx7L4meAtphb30nSx5pQqOeysU+otN9PEJH6TWr8KUXBDw6I=
 exao1.magao-x.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMsOYTn6tlmcatxt1pDfowTtBTsmJ77OMSPl3rNl8+OBKhmpVpX+iBUMKsBDwwVIlqEAa9BfJPbSrpWEWZABv3s=
 HERE
   else
-      log_info "/home/vagrant/.ssh/known_hosts exists, not overwriting"
+      log_info "/home/$username/.ssh/known_hosts exists, not overwriting"
   fi
-  if [[ ! -e /home/vagrant/.ssh/config ]]; then
-    cat <<'HERE' | sudo tee /home/vagrant/.ssh/config
-Include /vagrant/vm/ssh/config
+  if [[ ! -e /home/$username/.ssh/config ]]; then
+    cat << "HERE" | sudo tee /home/$username/.ssh/config
+Include $VM_SHARED_FOLDER/ssh/config
 HERE
   else
-      log_info "/home/vagrant/.ssh/config exists, not overwriting"
+      log_info "/home/$username/.ssh/config exists, not overwriting"
   fi
-  sudo chown -R vagrant:vagrant /home/vagrant/.ssh/
-  sudo chmod -R u=rwX,g=,o= /home/vagrant/.ssh/
+  sudo chown -R $username:$username /home/$username/.ssh/
+  sudo chmod -R u=rwX,g=,o= /home/$username/.ssh/
 fi
