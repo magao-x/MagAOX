2410b15f33f6f75dd7d10c0a82c021519dc8eb40
Joseph Long <github@joseph-long.com>
2022-10-17 10:20:14 -0700

    setup: ubuntu-specific config

--- setup/steps/configure_ubuntu_22.04.sh
+++ setup/steps/configure_ubuntu_22.04.sh
@@ -3,4 +3,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
 
-log_info "No custom configuration for Ubuntu 22.04"
+if [[ $MAGAOX_ROLE == AOC || $MAGAOX_ROLE == ICC || $MAGAOX_ROLE == RTC || $MAGAOX_ROLE == TIC || $MAGAOX_ROLE == TOC ]]; then
+    log_info "Purging cloud-init"
+    sudo apt-get purge -y cloud-init || exit 1
+    sudo apt autoremove -y || true
+fi
+
+log_info "Disable waiting for LAN config during boot"
+systemctl mask systemd-networkd-wait-online.service || true
+
+
+log_info "Configuring package source list with Intel oneAPI repositories"
+if [[ ! -e /usr/share/keyrings/oneapi-archive-keyring.gpg ]]; then
+    wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null || exit
+fi
+if [[ ! -e /etc/apt/sources.list.d/oneAPI.list ]]; then
+    echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list || exit
+fi
+sudo apt update
+log_info "Done with custom configuration for Ubuntu 22.04"
