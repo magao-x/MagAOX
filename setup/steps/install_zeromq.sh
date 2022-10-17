07ce0aba22035efe73e5c555fdf5a483e148d26a
Joseph Long <github@joseph-long.com>
2022-10-17 10:23:28 -0700

    setup: zeromq build: remove typo

--- setup/steps/install_zeromq.sh
+++ setup/steps/install_zeromq.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
 ZEROMQ_VERSION=4.3.4
-ZEROMQ_DIR=./zeromq-${ZEROMQ_VERSION}
+ZEROMQ_DIR=zeromq-${ZEROMQ_VERSION}
 ZEROMQ_TARFILE=$ZEROMQ_DIR.tar.gz
 if [[ ! -d $ZEROMQ_DIR ]]; then
     _cached_fetch https://github.com/zeromq/libzmq/releases/download/v${ZEROMQ_VERSION}/$ZEROMQ_TARFILE $ZEROMQ_TARFILE
