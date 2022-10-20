e00b32a04561fc66e5ccb391031eb6f8a3d86a37
Joseph Long <me@joseph-long.com>
2022-10-20 21:07:26 +0000

    uncomment container run

--- build_and_run_container.sh
+++ build_and_run_container.sh
@@ -5,3 +5,4 @@ sudo umount -f /dev/cpuset
 podman build --security-opt=seccomp=unconfined -t xcontainer .
 # podman run --rm --privileged -v /opt/MagAOX:/opt/MagAOX -v /data:/data -it xcontainer bash
 # podman run --rm --privileged -v /data:/data -it xcontainer bash
+podman run --rm --privileged -it xcontainer bash
