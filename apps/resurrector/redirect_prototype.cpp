b02503bfd76e11c3c2959ee899e79b9e661538c3
Brian Carcich <briantcarcich@gmail.com>
2023-02-21 17:00:49 -0500

    Improve comments; minor changes to prototype
    N.B. This code is still not ready for merge
    
    apps/resurrector/redirect_prototype.cpp
    
    - Expand and add comments; use Doxygen format
    - Rearrange some statements; add a missing break; rename routines
    - Remove unused code
    - Change #includes to require -I../../libMagAOX/common for compilation
    
    apps/resurrector/Makefile
    
    - Add include directory for redirect_prototpye.cpp compilation

--- apps/resurrector/redirect_prototype.cpp
+++ apps/resurrector/redirect_prototype.cpp
@@ -18,88 +18,58 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#include "../../libMagAOX/common/paths.hpp"
-#include "../../libMagAOX/common/environment.hpp"
-
-#if 0//Unused code:  this redirects STDOUT/ERR for C++-based code only
-void
-cout_cerr_redirect(int argc, char** argv)
-{
-  enum STEPS 
-  { STEPS_INIT = 0
-  , STEPS_CHECK_ARGC
-  , STEPS_INIT_UMASK
-  , STEPS_OPEN_FCOUT
-  , STEPS_OPEN_FCERR
-  , STEPS_REST_UMASK
-  , STEPS_REDIR_COUT
-  , STEPS_REDIR_CERR
-  , STEPS_LAST = STEPS_REDIR_CERR
-  };
-  int step{STEPS_INIT};
-  int old_umask{0};
-  int tmp_umask{0};
-  static std::ofstream fcout;
-  static std::ofstream fcerr;
-  while (!errno && step < STEPS_LAST)
-  {
-    ++step;
-    switch(step)
-    {
-    case STEPS_CHECK_ARGC: errno = (argc == 3) ? 0 : EINVAL; break;
-    case STEPS_INIT_UMASK: old_umask = umask(0);
-    case STEPS_OPEN_FCOUT: fcout.open(argv[1],std::ios_base::trunc); break;
-    case STEPS_OPEN_FCERR: fcerr.open(argv[2],std::ios_base::trunc); break;
-    case STEPS_REST_UMASK: tmp_umask = umask(old_umask); break;
-    case STEPS_REDIR_COUT: std::cout.rdbuf(fcout.rdbuf()); break;
-    case STEPS_REDIR_CERR: std::cerr.rdbuf(fcerr.rdbuf()); break;
-    default: break;
-    }
-  }
-  if (getenv("REDIRECT_DEBUG"))
-  {
-    std::cerr
-    << "{" << step
-    << "," << old_umask
-    << "," << tmp_umask
-    << "}={step,old_umask,tmp_umask}"
-    << std::endl;
-  }
-}
-#endif//0//Unused
+// -I../../libMagAOX/common
+#include "paths.hpp"
+#include "environment.hpp"
 
 /// A class to handle directory names for MagAO-X applications.
 /**
-  * \todo implement libMagAOX error handling? (a stack?)
-  * \todo make m_powerMgtEnabled a template parameter, and static_assert checki if _useINDI== false and power management is true
-  *
   * \ingroup magaoxapp
   */
 class MagAOXPathUtil
 {
 private:
 
-    static std::string magaox_base;           ///< This will be either from envvar MagAOX_PATH, or  "/opt/MagAOX" from macro MAGAOX_path
+    static std::string magaox_base;  ///< MagAOX base path, typicallly "/ope/MagAOX" from MAGAOX_path macro
+                                     ///< or from either from environment variable "MagAOX_PATH|
 
-    static bool singleton;                    ///< Only check envvar MagAOX_PATH once
+    static bool singleton;           ///< Flag to ensure magaox_base is assigned but once
 
     typedef std::vector<std::string> strvec;
     typedef strvec::iterator strvecit;
 
 public:
 
-    /// Constructor:  initialize class-static data (magaox_base)
+    /// Constructor:  initialize class-static data (magaox_base), once
     MagAOXPathUtil()
     {
-        if (MagAOXPathUtil::singleton) { return; }
+        if (MagAOXPathUtil::singleton) { return; }             // once!
+        MagAOXPathUtil::singleton = true;
+
+        // Check MagAOX base directory override environment variable:
+        // - if envvar exists,  then use its value;
+        // - otherwise use MAGAOX_path macro (typically /opt/MagAOX)
         char* ge = getenv(MAGAOX_env_path);
         MagAOXPathUtil::magaox_base = ge ? ge : MAGAOX_path;
-        MagAOXPathUtil::singleton = true;
     }
 
-    /// Break down paths into individual directory names
-    /** \returns vector of individual sub-directory namess
-     **/
+    /**
+     * \brief Break down paths into individual sub-directory names,
+     *
+     * Prepare for equivalent of \c mkdir \c -p
+     *
+     * Take null-terminated char* vararg strings that are to be
+     * concatenated with slashes to represent a directory path, extract
+     * individual sub-directory names in the hierarchary.
+     *
+     * E.g. \c  build("a/b","c",nullptr) will return \c {"a","b","c"}
+     *
+     * N.B. leading and trailing slashes are ignored
+     *
+     * \returns vector of individual sub-directory namess
+     * \arg \c arg0[in] the first char* string to parse
+     * \arg \c args[in] subsequent char* varargs, terminated by nullptr
+     */
     strvec const
     build(char* arg0, va_list args)
     {
@@ -121,20 +91,23 @@ public:
         return rtn;
     }
 
-    /// Build directory paths from individual components
-    /** \return - composite directory path
-     ** on return - errno will be zero on success, non-zero on failure
-     **           - returned composite may be incomplete of failure
-     **
-     ** Individual arguments are (char*) pointers
-     ** - Base MagAOX directory (/opt/MagAOX) prefix is assumed
-     ** - arguments will be split at slashes (/) by build(...) above
-     ** - leading, trailing, and duplicate slashes will be removed
-     **/ 
+    /** Build directory paths from individual components
+      *
+      * on return - errno will be zero on success, non-zero on failure
+      *           - returned composite may be incomplete of failure
+      *
+      * Individual arguments are (char*) pointers
+      * - Base MagAOX directory (/opt/MagAOX) prefix is assumed
+      * - arguments will be split at slashes (/) by build(...) above
+      * - leading, trailing, and duplicate slashes will be removed
+      *
+      * \return - composite directory path
+      * \arg \c mode[in] - mode_t permission bitss for created directories.  N.B. x (execute) will be added to each user/group/other triplet ir r(ead) or w(rite) is 1
+      */
     std::string const
     make_dirs(mode_t mode, char* arg0, ...)
     {
-        mode_t old_umask{umask(0)};        /// Save mode (umask)
+        mode_t old_umask{umask(0)};        /// Set 0 umask; save old val
 
         mode |= (mode&0006) ? 0001 : 000;  /// Set x perm bits as needed
         mode |= (mode&0060) ? 0010 : 000;
@@ -143,7 +116,7 @@ public:
         /// Init list with MagAOX base path (/opt/MagAOX, if no envvar)
         strvec toks(1,MagAOXPathUtil::magaox_base);
 
-        /// Extract single subdirectory names from arguments
+        /// Extract sub-directory names from vararg arguments (...)
         va_list args;
         va_start(args,arg0);
         strvec motoks = build(arg0, args);
@@ -152,7 +125,7 @@ public:
         /// Append arguments' names to list
         toks.insert(toks.begin()+1,motoks.begin(),motoks.end());
 
-        std::string fullpath{""};  ///< Path that is being built
+        std::string fullpath{""};  ///< Path to be built and returned
 
         /// Build path from list, create dirs that do not yet exist
         /// N.B. this loop will exit if errno is non-zero
@@ -170,12 +143,12 @@ public:
                 /// If directory does not (yet) exist, ...
                 if (errno==ENOENT)
                 {
-                    /// ... then Try to create subdirectory
+                    /// ... then try to create sub-directory
                     errno = 0;
                     mkdir(fullpath.c_str(),mode);
                     if (errno) { perror("MagAOXPathUtils::make_dirs, mkdir error"); }
                 }
-                /// Recycle loop on any stat(...) failure
+                /// Recycle loop on any other stat(...) failure
                 continue;
             }
 
@@ -200,11 +173,15 @@ public:
     ////////////////////////////////////////////////////////////////////
 
 
-    /// Wrapper for make_dirs(...), for /opt/MagAOX/sys/devicename/
-    /** \return - complete path
-     ** The created directory will contain file [pid], plus any
-     ** redirected output
-     **/
+    /** Wrapper for make_dirs(...), for /opt/MagAOX/sys/devicename/
+      *
+      * The created directory will contain file [pid], plus a file with
+      * any redirected output
+      *
+      * \return - complete path
+      * \arg \c mode[in] - Permissions mode bits
+      * \arg \c device[in] - INDI device name
+      */
     std::string  const
     make_sys_device_dirs(mode_t mode, std::string devicename)
     {
@@ -218,32 +195,38 @@ public:
 
 }; // class MagAOXPathUtil
 
-/// One-time initialization; assume no environment variable MagAOX_PATH
+/// One-time initialization for MagAOXPathUtil static variables;
+/// - Assume no environment variable MagAOX_PATH
 bool MagAOXPathUtil::singleton = false;
 std::string MagAOXPathUtil::magaox_base = MAGAOX_path;
+////////////////////////////////////////////////////////////////////////
 
-/// One-time initialization; assume no environment variable MagAOX_PATH
-void
-make_magaox_indi_device_dir(mode_t mode, std::string devicename)
-{
-    std::string fullpath{
-        MagAOXPathUtil().make_sys_device_dirs(mode ,devicename)
-                        };
-    return;
-}
-
+/** \brief Routine to redirect STDOUT and STDERR to a device under the
+  *        MagAOX system directory (/opt/MagAOX/sys/devicename/outputs)
+  *
+  * \returns - none, but errno will be non-zero if any system calls fail
+  * \arg \c devicename[in] - INDI driver device name
+  */
 void
 stdout_stderr_redirect(std::string devicename)
 {
-    std::string fullpath{""};
-    enum STEPS 
-    { S_BUILD_SUBDIR = 0, S_CHANGE_UMASK, S_OPEN_FSTDXXX, S_RESTOR_UMASK
-    , S_REDIR_STDOUT, S_REDIR_STDERR, S_CLOS_FSTDXXX
-    , S_LAST
+    std::string fullpath{""};   // Path to output file
+    int fstdxxx{-1};            // File descriptor for open output file
+    int old_umask{0};           // Saved umask
+    int fopts{O_CREAT|O_TRUNC|O_WRONLY|O_DSYNC};  // open(2) flags
+
+    enum STEPS
+    { S_BUILD_SUBDIR = 0  // Build /opt/MagAOX/sys/devicename dir path
+    , S_CHANGE_UMASK      // Change umask temporarily
+    , S_OPEN_FSTDXXX      // Open file in /opt/MagAOX/sys/devicename
+    , S_RESTOR_UMASK      // Restore umask
+    , S_REDIR_STDOUT      // Redirect STDOUT to that file
+    , S_REDIR_STDERR      // Redirect STDERR to that file
+    , S_CLOS_FSTDXXX      // Close open file descriptor
+    , S_LAST              // End of loop
     };
-    int fstdxxx{-1};
-    int old_umask{0};
-    int fopts{O_CREAT|O_TRUNC|O_WRONLY|O_DSYNC};
+
+    // Loop over those steps, exit loop early if errno becomes non-zero
     for (int step = S_BUILD_SUBDIR
         ; !errno && step < S_LAST
         ; ++step
@@ -252,11 +235,12 @@ stdout_stderr_redirect(std::string devicename)
         switch(step)
         {
         case S_BUILD_SUBDIR:
+            // Build directory path, append output filename
             fullpath = MagAOXPathUtil()
                        .make_sys_device_dirs(0644 ,devicename)
                      + "/outputs";
             break;
-        case S_CHANGE_UMASK: old_umask = umask(0);
+        case S_CHANGE_UMASK: old_umask = umask(0); break;
         case S_OPEN_FSTDXXX: fstdxxx = open(fullpath.c_str(),fopts,0644); break;
         case S_RESTOR_UMASK: umask(old_umask); break;
         case S_REDIR_STDOUT: dup2(fstdxxx, STDOUT_FILENO); break;
@@ -265,42 +249,77 @@ stdout_stderr_redirect(std::string devicename)
         default: break;
         }
     }
-}
+} // void stdout_stderr_redirect(std::string devicename)
 
 
+/** \brief Test harness subroutine for stdout_stderr_redirect above
+  *
+  * Test harness subroutine to read newline-terminated data from STDIN
+  * and write those data to either STDOUT and STDERR
+  */
 void
-forked_child()
+TEST_forked_child()
 {
-  bool use_cout{false};
-  do
+  bool use_cerr{false};  // Alternate writing to STDOUT and to STDERR
+  while (1)
   {
     std::string s;
-    std::cin >> s;
-    if (std::cin.eof()) { break; }
-    if (use_cout) { std::cerr << "(std::cerr)" << s; write(STDERR_FILENO,"STDERR_FILENO",13); write(STDERR_FILENO,s.c_str(), s.length());  }
-    else          { std::cout << "std::cout[" << s << "]" << std::endl; write(STDOUT_FILENO,"STDOUT_FILENO[",14); write(STDOUT_FILENO,s.c_str(),s.length()); write(STDOUT_FILENO,"]\n",2); }
-    use_cout ^= true;
-  } while (1);
+    std::cin >> s;                  // Read string
+    if (std::cin.eof()) { break; }  // Exit loop on EOF (Control-D?)
+
+    // Write data either to cerr/STDERR or to cout/STDOUT
+    if (use_cerr)
+    {
+        std::cerr << "(std::cerr)" << s;
+        write(STDERR_FILENO,"(STDERR_FILENO)",15);
+        write(STDERR_FILENO,s.c_str(), s.length());
+        fprintf(stderr, "(stderr)%s",s.c_str());
+    }
+    else
+    {
+        std::cout << "std::cout[" << s << "]" << std::endl;
+        write(STDOUT_FILENO,"STDOUT_FILENO[",14);
+        write(STDOUT_FILENO,s.c_str(),s.length());
+        write(STDOUT_FILENO,"]\n",2);
+        fprintf(stdout, "stdout[%s]\n",s.c_str());
+    }
+    use_cerr ^= true;  // Alternate writing to STDOUT and to STDERR
+  }
 }
 
+/** \brief Test harness main routine for stdout_stderr_redirect above
+  *
+  * Test harness to exercise stdout_stderr_redirect.
+  *
+  * With one argument (argc=2; argv[1]=devicnname), parent process will
+  * fork a child and then wait for that child to exit.  Child process
+  * redirects it STDOUT and STDERR output to a file with full path of
+  * /opt/MagAOX/sys/<devicename>/outputs, and then run this app with
+  * no arguments.
+  *
+  * With no argument, (i.e. forked child), this will run the subroutine
+  * TEST_forked_child() above to send test data to STDOUT and to STDERR,
+  * which test data should be redirected to /opt/MagAOX/sys/.../outputs
+  */
 int
-main(int argc, char** argv)
+TEST_MAIN(int argc, char** argv)
 {
   pid_t pid{0};
   pid_t pidwait {0};
   int wstatus{0};
-  fprintf(stderr,"%d,%llx,%lu={argc,argv,getpid()}\n"
-         ,argc,(long long unsigned) argv,(unsigned long)getpid());
+  fprintf(stderr,"%d,%lu={argc,getpid()}\n"
+         ,argc,(unsigned long)getpid());
 
-  if (argc==1) { forked_child(); exit(errno ? 1 : 0); }
+  // Run test data to redirected outputs, and exit
+  if (argc==1) { TEST_forked_child(); exit(errno ? 1 : 0); }
 
   if (argc==2)
   {
     errno = 0;
     if (!(pid=fork()))
     {
-      fprintf(stderr,"%d,%llx,%lu={argc,argv,getpid()}\n"
-         ,argc,(long long unsigned) argv,(unsigned long)getpid());
+      fprintf(stderr,"%d,%lu={argc,getpid()}\n"
+             ,argc,(unsigned long)getpid());
       stdout_stderr_redirect(argv[1]);
       execlp(argv[0], argv[0], NULL);
       exit(errno ? 2 : 0);
