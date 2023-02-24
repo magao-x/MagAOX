d61dd9b4b43c8b8078a75a4a36a09d84b09cee87
Joseph Long <github@joseph-long.com>
2023-02-23 22:29:50 -0300

    setup: more backup restore script, exit provisioning on failing to install MKL

--- setup/restore_users_from_backup.sh
+++ setup/restore_users_from_backup.sh
@@ -4,6 +4,20 @@ source $DIR/_common.sh
 if [[ -z $1 ]]; then
     error_exit "Specify the path to a folder of backed-up home directories"
 fi
-for username in $1/*; do
-    log_info "User: $(basename $username)"
+homePrefix=$1
+for userFolder in $homePrefix/*; do
+    userName=$(basename $userFolder)
+    if [[ $userName == '*' ]]; then
+        error_exit "No folders in $homePrefix to match"
+    fi
+    log_info "User: $userName"
+    if getent passwd $userName > /dev/null 2>&1; then
+        log_info "User account $userName exists"
+    else
+        createuser $userName
+    fi
+    uid=$(id -u $userName)
+    gid=$(id -g $userName)
+    rsync -av $userFolder/ /home/$userName/
+    chown -vR $uid:$gid /home/$userName/
 done
