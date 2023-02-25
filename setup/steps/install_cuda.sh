ddf0a203d6d6b4ab358c438b1bb540d14c13055b
Joseph Long <github@joseph-long.com>
2023-02-25 15:33:08 -0300

    setup: cuda install make sudo distro-independent

--- setup/steps/install_cuda.sh
+++ setup/steps/install_cuda.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # If not started as root, sudo yourself
 export BUILDING_KERNEL_STUFF=1  # disable loading devtoolset-7 for agreement w/ kernel gcc
+if [[ $(which sudo) == *devtoolset* ]]; then
+  REAL_SUDO=/usr/bin/sudo
+else
+  REAL_SUDO=$(which sudo)
+fi
+
 if [[ "$EUID" != 0 ]]; then
-    echo "Becoming root, disabling loading devtoolset-7 for agreement w/ kernel gcc..."
-    /usr/bin/sudo --preserve-env=BUILDING_KERNEL_STUFF bash -l $0 "$@"
+    echo "Becoming root..."
+    $REAL_SUDO --preserve-env=BUILDING_KERNEL_STUFF bash -l $0 "$@"
     exit $?
 fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
