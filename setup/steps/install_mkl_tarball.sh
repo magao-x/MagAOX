b9f477a94f12aac837b33cd628769ee9dcc6c4b8
Joseph Long <github@joseph-long.com>
2023-02-25 14:00:32 -0300

    setup: restore shell doodad setting MKLROOT

--- setup/steps/install_mkl_tarball.sh
+++ setup/steps/install_mkl_tarball.sh
@@ -15,5 +15,6 @@ else
     log_warn "/opt/intel already exists"
 fi
 echo "/opt/intel/oneapi/mkl/latest/lib/intel64" | sudo tee /etc/ld.so.conf.d/mkl.conf || exit 1
+echo "export MKLROOT=/opt/intel/oneapi/mkl/latest" | sudo tee /etc/profile.d/mkl.sh || exit 1
 sudo ldconfig
 log_info "Finished MKL tarball install"
