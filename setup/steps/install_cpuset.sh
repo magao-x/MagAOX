3ffe253be8552e8c5987402b9ac795f6394c3ea9
Joseph Long <me@joseph-long.com>
2023-06-11 14:50:49 -0700

    setup: build cpuset rpm from source

---
+++ setup/steps/install_cpuset.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+if [[ "$EUID" != 0 ]]; then
+    echo "Becoming root..."
+    sudo bash -l $0 "$@"
+    exit $?
+fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../_common.sh
+set -euo pipefail
+
+# From https://github.com/lpechacek/cpuset.git
+mkdir -p /opt/MagAOX/vendor
+cd /opt/MagAOX/vendor
+
+_cached_fetch https://github.com/lpechacek/cpuset/archive/refs/tags/v1.6.zip cpuset-v1.6.zip
+unzip -o cpuset-v1.6.zip
+cd cpuset-1.6/
+sudo yum install -y python-devel xmlto
+/bin/python setup.py bdist_rpm
+rpmFile=/opt/MagAOX/vendor/cpuset-1.6/dist/cpuset-1.6-1.noarch.rpm
+sudo yum install -y $rpmFile || true
+cat <<'HERE' | sudo tee /etc/cset.conf || exit 1
+mountpoint = /sys/fs/cgroup/cpuset
+HERE
