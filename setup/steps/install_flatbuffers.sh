825929648789928643e38b3bef736cafcef3bdaf
Joseph Long <github@joseph-long.com>
2023-06-22 02:45:13 +0000

    Update flatbuffers version

--- setup/steps/install_flatbuffers.sh
+++ setup/steps/install_flatbuffers.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
-FLATBUFFERS_VERSION="1.11.0"
+FLATBUFFERS_VERSION="23.5.26"
 #
 # Flatbuffers
 #
@@ -12,8 +12,6 @@ if [[ ! -d $FLATBUFFERS_DIR ]]; then
     tar xzf $FLATBUFFERS_DIR.tar.gz
 fi
 cd $FLATBUFFERS_DIR
-if ! command -v flatc; then
-    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
-    make
-    make install
-fi
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+make
+make install
