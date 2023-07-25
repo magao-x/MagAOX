3a423a899ba45e6e266f861574a058c6310ab9f0
Joseph Long <github@joseph-long.com>
2023-07-25 18:20:54 +0000

    Keep binary schemata declarations namespaced

---
+++ libMagAOX/logger/logBinarySchemata.cpp
@@ -0,0 +1,17 @@
+/** \file logFileName.cpp
+  * \brief Defines the variables holding binary flatbuffers schemata
+  * \author Joseph D. Long (jdl@fastmail.com)
+  *
+  * \ingroup logger_files
+  * 
+  */
+
+
+namespace MagAOX
+{
+namespace logger
+{
+#include "generated/binarySchemata.inc"
+} //namespace logger
+} //namespace MagAOX
+
