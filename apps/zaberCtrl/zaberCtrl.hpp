95fa881f9c1a940c0525c4850c01368e987efc24
jrmales <jaredmales@gmail.com>
2023-09-24 15:29:15 -0700

    added INDI_VALIDATE_CALLBACK_PROPS macro.  Started using on zabers and mcbl fws.

--- apps/zaberCtrl/zaberCtrl.hpp
+++ apps/zaberCtrl/zaberCtrl.hpp
@@ -478,11 +478,7 @@ int zaberCtrl::moveTo( const double & target)
 
 INDI_NEWCALLBACK_DEFN( zaberCtrl, m_indiP_pos)(const pcf::IndiProperty &ipRecv)
 {
-   if( ipRecv.getName() != m_indiP_pos.getName())
-   {
-      log<software_error>({__FILE__, __LINE__, "Invalid INDI property."});
-      return -1;
-   }
+    INDI_VALIDATE_CALLBACK_PROPS(m_indiP_pos, ipRecv); 
    
    double target =-1;
    if( ipRecv.find("target") ) //Just not our stage.
@@ -524,11 +520,7 @@ INDI_NEWCALLBACK_DEFN( zaberCtrl, m_indiP_pos)(const pcf::IndiProperty &ipRecv)
 
 INDI_NEWCALLBACK_DEFN( zaberCtrl, m_indiP_rawPos)(const pcf::IndiProperty &ipRecv)
 {
-   if( ipRecv.getName() != m_indiP_rawPos.getName())
-   {
-      log<software_error>({__FILE__, __LINE__, "Invalid INDI property."});
-      return -1;
-   }
+    INDI_VALIDATE_CALLBACK_PROPS(m_indiP_rawPos, ipRecv); 
    
    double target =-1;
    if( ipRecv.find("target") ) //Just not our stage.
@@ -582,11 +574,7 @@ INDI_NEWCALLBACK_DEFN( zaberCtrl, m_indiP_rawPos)(const pcf::IndiProperty &ipRec
 
 INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageState)(const pcf::IndiProperty &ipRecv)
 {
-   if( ipRecv.getName() != m_indiP_stageState.getName())
-   {
-      log<software_error>({__FILE__, __LINE__, "Invalid INDI property."});
-      return -1;
-   }
+   INDI_VALIDATE_CALLBACK_PROPS(m_indiP_stageState, ipRecv); 
    
    if( ipRecv.find(m_stageName) != true ) //Just not our stage.
    {
@@ -678,11 +666,7 @@ INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageState)(const pcf::IndiProperty &i
 
 INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageMaxRawPos )(const pcf::IndiProperty &ipRecv)
 {
-   if( ipRecv.getName() != m_indiP_stageMaxRawPos.getName())
-   {
-      log<software_error>({__FILE__, __LINE__, "Invalid INDI property."});
-      return -1;
-   }
+   INDI_VALIDATE_CALLBACK_PROPS(m_indiP_stageMaxRawPos, ipRecv); 
    
    if( ipRecv.find(m_stageName) != true ) //Just not our stage.
    {
@@ -705,11 +689,7 @@ INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageMaxRawPos )(const pcf::IndiProper
 
 INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageRawPos )(const pcf::IndiProperty &ipRecv)
 {
-   if( ipRecv.getName() != m_indiP_stageRawPos.getName())
-   {
-      log<software_error>({__FILE__, __LINE__, "Invalid INDI property."});
-      return -1;
-   }
+   INDI_VALIDATE_CALLBACK_PROPS(m_indiP_stageRawPos, ipRecv); 
    
    if( ipRecv.find(m_stageName) != true ) //Just not our stage.
    {
@@ -740,11 +720,7 @@ INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageRawPos )(const pcf::IndiProperty 
 
 INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageTgtPos )(const pcf::IndiProperty &ipRecv)
 {
-   if( ipRecv.getName() != m_indiP_stageTgtPos.getName())
-   {
-      log<software_error>({__FILE__, __LINE__, "Invalid INDI property."});
-      return -1;
-   }
+   INDI_VALIDATE_CALLBACK_PROPS(m_indiP_stageTgtPos, ipRecv); 
    
    if( ipRecv.find(m_stageName) != true ) //Just not our stage.
    {
@@ -780,11 +756,7 @@ INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageTgtPos )(const pcf::IndiProperty 
 
 INDI_SETCALLBACK_DEFN( zaberCtrl, m_indiP_stageTemp )(const pcf::IndiProperty &ipRecv)
 {
-   if( ipRecv.getName() != m_indiP_stageTemp.getName())
-   {
-      log<software_error>({__FILE__, __LINE__, "Invalid INDI property."});
-      return -1;
-   }
+   INDI_VALIDATE_CALLBACK_PROPS(m_indiP_stageTemp, ipRecv); 
    
    if( ipRecv.find(m_stageName) != true ) //Just not our stage.
    {
