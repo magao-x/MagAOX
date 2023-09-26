b447e6d2742213ed8710fb794b554d1780b41821
Joseph Long <github@joseph-long.com>
2023-09-26 15:06:49 -0400

    use root:root in CI

--- setup/_common.sh
+++ setup/_common.sh
@@ -15,6 +15,9 @@ if [[ $MAGAOX_ROLE == vm ]]; then
     instrument_group=ubuntu
     instrument_dev_group=ubuntu
   fi
+else if [[ $MAGAOX_ROLE == ci ]]; then
+  instrument_user=root
+  instrument_group=root
 fi
 
 function log_error() {
