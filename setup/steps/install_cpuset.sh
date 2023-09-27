3318088deabac741104ba0c504fc615132840eec
Joseph Long <github@joseph-long.com>
2023-09-27 15:45:48 -0400

    Fix artisanal cpuset install

--- setup/steps/install_cpuset.sh
+++ setup/steps/install_cpuset.sh
@@ -8,6 +8,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
 
+if [[ -e /usr/bin/python ]]; then
+    osPython=/usr/bin/python
+elif [[ -e /bin/python ]]; then
+    osPython=/bin/python
+else
+    osPython=python
+fi
+
 # From https://github.com/lpechacek/cpuset.git
 mkdir -p /opt/MagAOX/vendor
 cd /opt/MagAOX/vendor
@@ -16,7 +24,7 @@ conda deactivate || true
 _cached_fetch https://github.com/PythonCharmers/python-future/archive/refs/tags/v0.18.3.zip python-future-v0.18.3.zip
 unzip -o python-future-v0.18.3.zip
 cd python-future-0.18.3
-/bin/python setup.py bdist_rpm
+$osPython setup.py bdist_rpm
 rpmFile=/opt/MagAOX/vendor/python-future-0.18.3/dist/future-0.18.3-1.noarch.rpm
 sudo yum install -y $rpmFile || true
 
@@ -24,7 +32,8 @@ _cached_fetch https://github.com/lpechacek/cpuset/archive/refs/tags/v1.6.zip cpu
 unzip -o cpuset-v1.6.zip
 cd cpuset-1.6/
 sudo yum install -y python-devel xmlto
-/bin/python setup.py bdist_rpm
+
+$osPython setup.py bdist_rpm
 rpmFile=/opt/MagAOX/vendor/cpuset-1.6/dist/cpuset-1.6-1.noarch.rpm
 sudo yum install -y $rpmFile || true
 source /etc/profile.d/cgroups1_cpuset_mountpoint.sh
