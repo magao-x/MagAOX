4d087809bfa3ee040d72a9810a76d49086d437de
jrmales <jaredmales@gmail.com>
2024-01-18 00:47:39 -0700

    added closedLoopIndi

--- libMagAOX/app/indiMacros.hpp
+++ libMagAOX/app/indiMacros.hpp
@@ -268,7 +268,8 @@ if( registerIndiPropertySet( prop,devName,  propName, INDI_SETCALLBACK(prop)) < 
 }                                                                                           
 
 /// Create and register a NEW INDI property as a standard number as float, using the standard callback name.
-/** This wraps createStandardIndiNumber and registerIndiPropertyNew, with error checking
+/** This wraps createStandardIndiNumber and registerIndiPropertyNew, with error checking.
+  * \p prop will have elements "current" and "target".
   *
   * \param prop   [out] the property to create and setup
   * \param name   [in] the name of the property
@@ -295,6 +296,7 @@ if( registerIndiPropertySet( prop,devName,  propName, INDI_SETCALLBACK(prop)) < 
 
 /// Create and register a NEW INDI property as a standard number as int, using the standard callback name.
 /** This wraps createStandardIndiNumber and registerIndiPropertyNew, with error checking
+  * \p prop will have elements "current" and "target".
   *
   * \param prop   [out] the property to create and setup
   * \param name   [in] the name of the property
@@ -317,6 +319,33 @@ if( registerIndiPropertySet( prop,devName,  propName, INDI_SETCALLBACK(prop)) < 
     {                                                                                           \
         log<software_error>({__FILE__,__LINE__, "error from registerIndiPropertyNew"});         \
         return -1;                                                                              \
+    }
+
+/// Create and register a NEW INDI property as a standard number as unsigned int, using the standard callback name.
+/** This wraps createStandardIndiNumber and registerIndiPropertyNew, with error checking
+  * \p prop will have elements "current" and "target".
+  *
+  * \param prop   [out] the property to create and setup
+  * \param name   [in] the name of the property
+  * \param min    [in] the minimum value for the elements, applied to both target and current
+  * \param max    [in] the minimum value for the elements, applied to both target and current
+  * \param step   [in] the step size for the elements, applied to both target and current
+  * \param format [in] the _ value for the elements, applied to both target and current.  Set to "" to use the MagAO-X standard for type.
+  * \param label  [in] [optional] the GUI label suggestion for this property
+  * \param group  [in] [optional] the group for this property
+  * 
+  * \ingroup indi
+  */
+#define CREATE_REG_INDI_NEW_NUMBERU( prop, name, min, max, step, format, label, group)               \
+    if( createStandardIndiNumber<unsigned>( prop, name, min, max, step, format, label, group) < 0)   \
+    {                                                                                                \
+        log<software_error>({__FILE__,__LINE__, "error from createStandardIndiNumber"});             \
+        return -1;                                                                                   \
+    }                                                                                                \
+    if( registerIndiPropertyNew( prop, INDI_NEWCALLBACK(prop)) < 0)                                  \
+    {                                                                                                \
+        log<software_error>({__FILE__,__LINE__, "error from registerIndiPropertyNew"});              \
+        return -1;                                                                                   \
     }
 
 /// Create and register a NEW INDI property as a standard toggle switch, using the standard callback name.
