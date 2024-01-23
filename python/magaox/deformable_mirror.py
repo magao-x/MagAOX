f831d469128abcb93eb9d9918f46c57557a770ca
Sebastiaan Haffert <shaffert@email.arizona.edu>
2024-01-23 15:22:39 -0700

    add python deformable mirror interface

---
+++ python/magaox/deformable_mirror.py
@@ -0,0 +1,84 @@
+from hcipy import *
+import numpy as np
+
+from magpyx.utils import ImageStream
+
+class XDeformableMirror(DeformableMirror):
+    def __init__(self, dm='woofer', channel=2, mode_basis_generator=lambda x : np.eye(x.size)):
+		''' The MagAO-X python interface for the deformable mirrors.
+
+		Parameters
+		----------
+		dm : string
+			The name of the dm that we are connecting to.
+		channel : int
+			The dm channel that is being used.
+		mode_basis_generator : Field generator
+			A function that calculates the modes for the DM.
+		'''
+        
+        if dm == 'woofer':
+            self.dmindex = 0
+            self.num_across = 11
+			self.size = np.array([1, 1])
+        elif dm == 'tweeter':
+            self.dmindex = 1
+            self.num_across = 50
+			self.size = np.array([1, 1])
+        elif dm == 'ncpc':
+            self.dmindex = 2
+            self.num_across = 34
+			self.size = np.array([1, np.sqrt(2.0)])
+        else:
+            self.dmindex = 0
+            self.num_across = 11
+        
+		# DM write channel
+        self.dmwrite = ImageStream('dm0{:d}disp0{:d}'.format(self.dmindex, channel))
+		
+		# The DM modes
+        self.grid = make_pupil_grid(self.num_across, 1)
+		modes = mode_basis_generator(self.grid)
+        super().__init__(modes)
+
+    def send(self, sleep=None):
+        self.dmwrite.write(self.surface.shaped.astype(np.float32))        
+        if sleep is not None:
+            time.sleep(sleep)
+    
+    def reset(self):
+        self.flatten()
+        self.send()
+
+class XZernikeMirror(XDeformableMirror):
+	def __init__(self, starting_mode=2, number_of_modes=5, dm='woofer', channel=2):
+		''' The MagAO-X python interface for a Zernike-modes basis deformable mirror.
+		Parameters
+		----------
+		starting_mode : int
+			The index of the first mode of the mode basis.
+		number_of_modes : int
+			The number of modes in the created mode basis.
+		dm : string
+			The name of the dm that we are connecting to.
+		channel : int
+			The dm channel that is being used.
+		'''
+
+		mode_basis_generator = lambda grid: make_zernike_basis(number_of_modes, 1.0, grid, starting_mode=starting_mode)
+		super().__init__(dm=dm, channel=channel, mode_basis_generator)
+
+class XFourierMirror(XDeformableMirror):
+	def __init__(self, fourier_grid, dm='woofer', channel=2):
+		''' The MagAO-X python interface for a Zernike-modes basis deformable mirror.
+		Parameters
+		----------
+		fourier_grid : grid
+			The spatial frequencies of the each mode in units of lambda/D.
+		dm : string
+			The name of the dm that we are connecting to.
+		channel : int
+			The dm channel that is being used.
+		'''
+		mode_basis_generator = lambda grid: make_fourier_basis(grid, fourier_grid.scaled(2 * np.pi))
+		super().__init__(dm=dm, channel=channel, mode_basis_generator)
