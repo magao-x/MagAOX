1099c8cc712ed9852ca561c7e770a2b0f43eac74
Joseph Long <github@joseph-long.com>
2024-04-08 11:56:10 -0400

    ci test: become root

--- setup/xvm/create_oemdrv.sh
+++ setup/xvm/create_oemdrv.sh
@@ -5,11 +5,11 @@ if [[ $(uname) == "Darwin" ]]; then
     qemu-img convert -f dmg -O qcow2 ./input/oemdrv.dmg ./input/oemdrv.qcow2
 else
     qemu-img create -f raw oemdrv.img 1G
-    parted oemdrv.img --script -- mklabel msdos
-    parted oemdrv.img --script -- mkpart primary fat32 1MiB 100%
-    mkfs.fat -n OEMDRV -F 32 oemdrv.img
-    mount -o loop oemdrv.img /mnt/oemdrv
+    sudo parted oemdrv.img --script -- mklabel msdos
+    sudo parted oemdrv.img --script -- mkpart primary fat32 1MiB 100%
+    sudo mkfs.fat -n OEMDRV -F 32 oemdrv.img
+    sudo mount -o loop oemdrv.img /mnt/oemdrv
     cp -R ./input/kickstart/* /mnt/oemdrv/
-    umount /mnt/oemdrv
+    sudo umount /mnt/oemdrv
     qemu-img convert -f raw -O qcow2 oemdrv.img ./input/oemdrv.qcow2
 fi
