be654b56fa815dcf0c24511869c9d09c129c9d1b
Joseph Long <github@joseph-long.com>
2024-04-08 12:25:48 -0400

    ci test: try actually building VM

--- setup/xvm/create_oemdrv.sh
+++ setup/xvm/create_oemdrv.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 mkdir -p input output
-# make ssh key pair
-if [[ ! -e ./output/xvm_key ]]; then
-    ssh-keygen -q -t ed25519 -f ./output/xvm_key -N ''
-fi
-cp ./output/xvm_key.pub ./input/kickstart/authorized_keys
 # copy kickstart files
 cp -R ./kickstart ./input/
+cp ./output/xvm_key.pub ./input/kickstart/authorized_keys
 rm -f ./input/oemdrv.{dmg,qcow2}
 if [[ $(uname) == "Darwin" ]]; then
     hdiutil create -srcfolder ./input/kickstart -format UDRO -volname "OEMDRV" -fs "MS-DOS FAT32" ./input/oemdrv.dmg
