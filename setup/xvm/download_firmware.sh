6f16c639eb079a7505444f2c423fb9456c2a1bf4
Joseph Long <github@joseph-long.com>
2024-04-08 12:58:12 -0400

    ci test: make rpm extraction platform neutral

--- setup/xvm/download_firmware.sh
+++ setup/xvm/download_firmware.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 mkdir -p ./input/firmware
 cd ./input/firmware
+platform=$(uname)
 if [[ ! -e AAVMF_CODE.fd ]]; then
     curl -OL https://dl.rockylinux.org/pub/rocky/9/AppStream/aarch64/os/Packages/e/edk2-aarch64-20230524-4.el9_3.2.noarch.rpm
-    tar xvf edk2-aarch64-20230524-4.el9_3.2.noarch.rpm
+    if [[ $platform == "Darwin" ]]; then
+        tar xvf edk2-aarch64-20230524-4.el9_3.2.noarch.rpm
+    else
+        rpm2cpio edk2-aarch64-20230524-4.el9_3.2.noarch.rpm | cpio -idmv
+    fi
     cp usr/share/AAVMF/AAVMF_*.fd ./
 fi
 if [[ ! -e OVMF_CODE.fd ]]; then
     curl -OL https://dl.rockylinux.org/pub/rocky/9/AppStream/x86_64/os/Packages/e/edk2-ovmf-20230524-4.el9_3.2.noarch.rpm
-    tar xvf edk2-ovmf-20230524-4.el9_3.2.noarch.rpm
+    if [[ $platform == "Darwin" ]]; then
+        tar xvf edk2-ovmf-20230524-4.el9_3.2.noarch.rpm
+    else
+        rpm2cpio edk2-ovmf-20230524-4.el9_3.2.noarch.rpm | cpio -idmv
+    fi
     cp usr/share/edk2/ovmf/OVMF_*.fd ./
 fi
 rm -rf usr *.rpm
