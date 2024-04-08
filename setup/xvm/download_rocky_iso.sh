7a0be69b24a02a73b6515705797d40a39c272db6
Joseph Long <github@joseph-long.com>
2024-04-08 12:10:38 -0400

    ci test: try to fix ISO cache

--- setup/xvm/download_rocky_iso.sh
+++ setup/xvm/download_rocky_iso.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 mkdir -p ./input/iso
 cd ./input/iso
-if [ ! -e Rocky-9.3-aarch64-minimal.iso ]; then
+ls -la
+if [[ ! -e Rocky-9.3-aarch64-minimal.iso ]]; then
     curl -OL https://download.rockylinux.org/pub/rocky/9/isos/aarch64/Rocky-9.3-aarch64-minimal.iso
 fi
+ls -la
