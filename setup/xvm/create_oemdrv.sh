a584e31636af5f79c3dc6f0d838d5a9ca18067fd
Joseph Long <github@joseph-long.com>
2024-04-08 12:08:16 -0400

    ci test: combine some steps in oemdrv generation

--- setup/xvm/create_oemdrv.sh
+++ setup/xvm/create_oemdrv.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+mkdir -p input
+# make ssh key pair
+if [[ ! -e ./input/xvm_key ]]; then
+    ssh-keygen -q -t ed25519 -f ./output/xvm_key -N ''
+fi
+cp ./output/xvm_key.pub ./input/kickstart/authorized_keys
+# copy kickstart files
+cp -R ./kickstart ./input/
 rm -f ./input/oemdrv.{dmg,qcow2}
 if [[ $(uname) == "Darwin" ]]; then
     hdiutil create -srcfolder ./input/kickstart -format UDRO -volname "OEMDRV" -fs "MS-DOS FAT32" ./input/oemdrv.dmg
@@ -8,8 +16,9 @@ else
     sudo parted oemdrv.img --script -- mklabel msdos
     sudo parted oemdrv.img --script -- mkpart primary fat32 1MiB 100%
     sudo mkfs.fat -n OEMDRV -F 32 oemdrv.img
+    sudo mkdir -p /mnt/oemdrv
     sudo mount -o loop oemdrv.img /mnt/oemdrv
-    cp -R ./input/kickstart/* /mnt/oemdrv/
+    sudo cp -R ./input/kickstart/* /mnt/oemdrv/
     sudo umount /mnt/oemdrv
     qemu-img convert -f raw -O qcow2 oemdrv.img ./input/oemdrv.qcow2
 fi
