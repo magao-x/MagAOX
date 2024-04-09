03353b0c74c76a12a4bfc0582164f0764756e5ce
Joseph Long <github@joseph-long.com>
2024-04-08 22:11:04 -0400

    ci test: try stage split caching

--- setup/xvm/download_rocky_iso.sh
+++ setup/xvm/download_rocky_iso.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 mkdir -p ./input/iso
 cd ./input/iso
-ls -la
 if [[ ! -e Rocky-9.3-aarch64-minimal.iso ]]; then
     curl -L https://download.rockylinux.org/pub/rocky/9/isos/aarch64/Rocky-9.3-aarch64-minimal.iso > Rocky-9.3-aarch64-minimal.iso.part
     mv Rocky-9.3-aarch64-minimal.iso.part Rocky-9.3-aarch64-minimal.iso
+else
+    echo "Rocky Linux 9.3 aarch64 minimal ISO already downloaded."
 fi
-ls -la
