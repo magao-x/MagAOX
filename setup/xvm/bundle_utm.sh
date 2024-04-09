ada2e2359e546c1868c3b43b85133dc2d1b76593
Joseph Long <github@joseph-long.com>
2024-04-09 11:02:55 -0400

    ci test: Move UTM bundle into a bundle folder, fix AAVMF_VARS path

--- setup/xvm/bundle_utm.sh
+++ setup/xvm/bundle_utm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cp -R ./utm ./output/MagAO-X.utm
-cd ./output
-mv ./xvm.qcow2 ./MagAO-X.utm/Data/xvm.qcow2
-tar -cJvf ./MagAO-X_VM_bundle.tar.xz ./MagAO-X.utm
+mkdir -p ./output/bundle/MagAO-X.utm
+cp -R ./utm ./output/bundle/MagAO-X.utm
+mv ./output/xvm.qcow2 ./output/bundle/MagAO-X.utm/Data/xvm.qcow2
+tar -cJvf ./MagAO-X_VM_bundle.tar.xz ./bundle
