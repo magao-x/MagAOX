72045c10dbff303fcf35ad3c87c1671169dd816c
Joseph Long <github@joseph-long.com>
2024-04-23 20:38:28 +0000

    Make: add python.mk and pythonApp.mk to standardize Python app builds

---
+++ Make/pythonApp.mk
@@ -0,0 +1,18 @@
+SELF_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+include $(SELF_DIR)/python.mk
+
+TARGET ?= $(t)
+
+all : install
+
+install:
+	sudo $(PYTHON) -c 'import purepyindi2' || (echo "Need purepyindi2 installed to $(shell which python)" && exit 1)
+	sudo $(PYTHON) -c 'import xconf' || (echo "Need xconf installed to $(shell which python)" && exit 1)
+	sudo $(PYTHON) -m pip install .
+	sudo ln -sfv $(PYTHON_SCRIPTS_PREFIX)/$(TARGET) /opt/MagAOX/bin/$(TARGET)
+	@echo "*** Install succeeded, app located in /opt/MagAOX/bin/$(TARGET) ***"
+
+.PHONY: all install
+
+clean:
+	@echo "Nothing to clean for $(TARGET)"
