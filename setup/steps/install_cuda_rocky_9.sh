1e76b666aa5a8e913910a121fa5ea778b1cf6de1
Joseph Long <github@joseph-long.com>
2024-05-13 16:39:20 -0400

    fix permissions on /opt/MagAOX/vendor/cuda_rpm

--- setup/steps/install_cuda_rocky_9.sh
+++ setup/steps/install_cuda_rocky_9.sh
@@ -4,6 +4,7 @@ source $DIR/../_common.sh
 set -exuo pipefail
 sudo mkdir -p /opt/MagAOX/vendor/cuda_rpm
 sudo chown :magaox-dev /opt/MagAOX/vendor/cuda_rpm
+sudo chmod g+ws /opt/MagAOX/vendor/cuda_rpm
 cd /opt/MagAOX/vendor/cuda_rpm
 wget https://developer.download.nvidia.com/compute/cuda/12.4.1/local_installers/cuda-repo-rhel9-12-4-local-12.4.1_550.54.15-1.x86_64.rpm
 
