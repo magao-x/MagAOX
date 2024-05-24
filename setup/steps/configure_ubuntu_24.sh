330be45a070902a3ce66e2e5890af4d0165384aa
Jared Males <jaredmales@pm.me>
2024-05-23 21:16:51 -0400

    updated provisioning for ubuntu 24

---
+++ setup/steps/configure_ubuntu_24.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../_common.sh
+set -euo pipefail
+
+log_info 'Making /etc/bash.bashrc source /etc/profile.d/*.sh, since graphical sessions appear not to for new Konsoles'
+if ! grep -q profile.d /etc/bash.bashrc; then
+cat <<'HERE' | sudo -H tee -a /etc/bash.bashrc
+if [ -d /etc/profile.d ]; then
+  for i in /etc/profile.d/*.sh; do
+    if [ -r $i ]; then
+      . $i
+    fi
+  done
+  unset i
+fi
+HERE
+fi
+
+if [[ $MAGAOX_ROLE == AOC ]]; then
+  sudo -H mkdir -p /etc/systemd/logind.conf.d/
+  cat <<'HERE' | sudo -H tee /etc/systemd/logind.conf.d/disable_power_keys.conf
+HandlePowerKey=ignore
+HandleSuspendKey=ignore
+HandleHibernateKey=ignore
+HandleRebootKey=ignore
+HERE
+fi
+
+if [[ $MAGAOX_ROLE == AOC || $MAGAOX_ROLE == ICC || $MAGAOX_ROLE == RTC || $MAGAOX_ROLE == TIC || $MAGAOX_ROLE == TOC ]]; then
+    log_info "Purging cloud-init"
+    sudo apt-get purge -y cloud-init || exit 1
+    sudo apt autoremove -y || true
+
+    log_info "Disable waiting for LAN config during boot"
+    sudo -H systemctl mask systemd-networkd-wait-online.service || true
+    
+    log_info "Ensure UFW firewall is enabled"
+    yes | sudo ufw enable || true
+    sudo ufw allow ssh || true
+    sudo ufw deny http || true
+    sudo ufw deny https || true
+    sudo ufw allow in from 192.168.0.0/24 || true
+
+    log_info "Use CentOS mountpoint for cpusets"
+    sudo mkdir -p /sys/fs/cgroup/cpuset
+    cat <<'HERE' | sudo -H tee /etc/cset.conf || true
+mountpoint = /sys/fs/cgroup/cpuset
+HERE
+fi
+
+log_info "Hush login banners"
+sudo touch /etc/skel/.hushlogin
+
+if [[ -d /usr/share/unattended-upgrades ]]; then
+  log_info "Disable automatic upgrades"
+  sudo -H cp -v /usr/share/unattended-upgrades/20auto-upgrades-disabled /etc/apt/apt.conf.d/
+fi
+
+log_info "Done with custom configuration for Ubuntu 22.04"
--- setup/steps/install_zeromq.sh
+++ setup/steps/install_zeromq.sh
@@ -3,7 +3,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../_common.sh
 set -euo pipefail
 cd /opt/MagAOX/vendor
-ZEROMQ_VERSION=4.3.4
+
+source /etc/os-release
+
+if [[ $VERSION_ID == "24.04" ]]; then
+    ZEROMQ_VERSION=4.3.5
+else
+    ZEROMQ_VERSION=4.3.4
+fi
+
 ZEROMQ_DIR=zeromq-${ZEROMQ_VERSION}
 ZEROMQ_TARFILE=$ZEROMQ_DIR.tar.gz
 if [[ ! -d $ZEROMQ_DIR ]]; then
