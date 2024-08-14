130c509636efc5834271747b70aa48531bb4b109
Joseph Long <github@joseph-long.com>
2024-08-14 15:11:49 -0400

    Update apt before installing qemu

--- setup/xvm/download_rocky_iso.sh
+++ setup/xvm/download_rocky_iso.sh
@@ -3,7 +3,7 @@ source ./_common.sh
 mkdir -p ./input/iso
 cd ./input/iso
 if [[ ! -e Rocky-${rockyVersion}-${vmArch}-minimal.iso ]]; then
-    curl -L https://download.rockylinux.org/pub/rocky/9/isos/${vmArch}/Rocky-${rockyVersion}-${vmArch}-minimal.iso > Rocky-${rockyVersion}-${vmArch}-minimal.iso.part
+    curl --no-progress-meter -L https://download.rockylinux.org/pub/rocky/9/isos/${vmArch}/Rocky-${rockyVersion}-${vmArch}-minimal.iso > Rocky-${rockyVersion}-${vmArch}-minimal.iso.part
     mv Rocky-${rockyVersion}-${vmArch}-minimal.iso.part Rocky-${rockyVersion}-${vmArch}-minimal.iso
 else
     echo "Rocky Linux ${vmArch} minimal ISO already downloaded."
