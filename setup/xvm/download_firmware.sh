8ceabae88ca4e8bf8382724da43332113ab18ef4
Joseph Long <github@joseph-long.com>
2024-08-19 19:53:32 -0400

    Update firmware download script

--- setup/xvm/download_firmware.sh
+++ setup/xvm/download_firmware.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 set -ex
-mkdir -p ./input/firmware
-cd ./input/firmware
+mkdir -p ./input/firmware || exit 1
+cd ./input/firmware || exit 1
 
 if [[ $vmArch == aarch64 ]]; then
     archInfix=aarch64
+    archFolder=aavmf
     archPrefix=AAVMF
 else
     archInfix=x86_64
+    archFolder=ovmf
     archPrefix=OVMF
 fi
 
 indexPage=https://dl.rockylinux.org/pub/rocky/9/AppStream/${archInfix}/os/Packages/e/
 if [[ ! -e ${archPrefix}_CODE.fd ]]; then
     rpmName=$(curl -q $indexPage | grep edk2 | sed -n 's/.*href="\(edk2-[^"]*\)".*/\1/p')
-    curl -OL ${indexPage}/${rpmName}
+    curl -OL ${indexPage}/${rpmName} || exit 1
     if [[ $(uname -o) == Darwin ]]; then
-        tar xvf $rpmName
+        tar xvf $rpmName || exit 1
     else
-        rpm2archive $rpmName
-        tar xvf $rpmName.tgz
+        rpm2archive $rpmName || exit 1
+        tar xvf $rpmName.tgz || exit 1
     fi
-    cp -v usr/share/${archPrefix}/${archPrefix}_*.fd ./
+    cp -v usr/share/${archFolder}/${archPrefix}_*.fd ./ || exit 1
 fi
 rm -rf usr *.rpm
