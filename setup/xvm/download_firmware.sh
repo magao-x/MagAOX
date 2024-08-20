ce45e2752314465622f847c9190b163efac6ab25
Joseph Long <github@joseph-long.com>
2024-08-19 20:14:16 -0400

    Use vmArch var in download_firmware.sh

--- setup/xvm/download_firmware.sh
+++ setup/xvm/download_firmware.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 mkdir -p ./input/firmware || exit 1
-cd ./input/firmware || exit 1
+cd ./input/firmware
 
-indexPage=https://dl.rockylinux.org/pub/rocky/9/AppStream/${archInfix}/os/Packages/e/
-if [[ ! -e ${archPrefix}_CODE.fd ]]; then
-    rpmName=$(curl -q $indexPage | grep edk2 | sed -n 's/.*href="\(edk2-[^"]*\)".*/\1/p')
-    curl -OL ${indexPage}/${rpmName} || exit 1
-    if [[ $(uname -o) == Darwin ]]; then
-        tar xvf $rpmName || exit 1
-    else
-        rpm2archive $rpmName || exit 1
-        tar xvf $rpmName.tgz || exit 1
-    fi
+indexPage=https://dl.rockylinux.org/pub/rocky/9/AppStream/${vmArch}/os/Packages/e/
+
+rpmName=$(curl -q $indexPage | grep edk2 | sed -n 's/.*href="\(edk2-[^"]*\)".*/\1/p')
+curl -OL ${indexPage}/${rpmName} || exit 1
+if [[ $(uname -o) == Darwin ]]; then
+    tar xvf $rpmName || exit 1
+else
+    rpm2archive $rpmName || exit 1
+    tar xvf $rpmName.tgz || exit 1
 fi
