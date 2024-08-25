633fbd4f96cc77b5552b522ec4b296fd46dd4259
Brian Carcich <briantcarcich@gmail.com>
2024-08-25 02:11:06 -0400

    Update mxlib install script from magaox/dev
    - Uses branch [magaox] instead of [dev] from
      github.com:jaredrmales/mxlib

--- setup/steps/install_mxlib.sh
+++ setup/steps/install_mxlib.sh
@@ -10,7 +10,7 @@ MXLIBROOT=/opt/MagAOX/source/mxlib
 # mxLib
 #
 
-MXLIB_COMMIT_ISH=dev
+MXLIB_COMMIT_ISH=magaox
 orgname=jaredmales
 reponame=mxlib
 parentdir=/opt/MagAOX/source
@@ -48,9 +48,7 @@ if [[ $MAGAOX_ROLE == RTC || $MAGAOX_ROLE == ICC || $MAGAOX_ROLE == AOC || $MAGA
   echo "INCLUDES += -I/usr/local/cuda/targets/x86_64-linux/include/" >> $mxlibCommonOverrides
 fi
 
-if [[ $ID == centos ]]; then
-  echo "CXXVERSION = -std=c++14" >> $mxlibCommonOverrides
-fi
+source /etc/os-release
 
 if [[ $ID == rocky && $(uname -p) == "aarch64" ]]; then
   echo "CXXFLAGS += -I/usr/include/lapacke" >> $mxlibCommonOverrides
