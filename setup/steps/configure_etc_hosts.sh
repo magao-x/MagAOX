15eb84b3eac810a877dcdd0c47c46420470557c4
Jared Males <jaredmales@gmail.com>
2024-10-14 14:29:04 -0700

    removed test exec

--- setup/steps/configure_etc_hosts.sh
+++ setup/steps/configure_etc_hosts.sh
@@ -2,9 +2,15 @@
 sudo tee /etc/hosts <<'HERE'
 127.0.0.1      localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1            localhost localhost.localdomain localhost6 localhost6.localdomain6
+
+
+############################
+# Instrument LAN
+############################
 192.168.0.10   exao1 aoc
-192.168.0.11   exao2 rtc
-192.168.0.12   exao3 icc
+192.168.0.191   exao2 rtc
+192.168.0.192   exao3 icc
+192.168.0.14   exao4 coc
 192.168.0.140  pdu0
 192.168.0.141  pdu1
 192.168.0.142  pdu2
@@ -14,5 +20,7 @@ sudo tee /etc/hosts <<'HERE'
 192.168.0.160  picomotorctl0
 192.168.0.170  moxadio1
 192.168.0.230  fxgenmodwfs
-192.168.0.242  dmsafety
+
+#note: 192.168.0.240--192.168.0.254 reserved for DHCP
+
 HERE
