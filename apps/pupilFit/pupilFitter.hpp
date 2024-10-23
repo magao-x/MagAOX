28facd75c8544be5710c87a1e851fc034d150c5f
Jared Males <jaredmales@gmail.com>
2024-10-22 20:22:46 -0700

    some formatting

--- apps/pupilFit/pupilFitter.hpp
+++ apps/pupilFit/pupilFitter.hpp
@@ -16,67 +16,67 @@ namespace MagAOX
 {
 namespace app
 {
-   
+
 /// Struct to perform centration and measure diameter of Pyramid pupils
 template<typename realT>
-struct pupilFitter 
+struct pupilFitter
 {
    mx::improc::eigenImage<realT> m_quad;
    mx::improc::eigenImage<realT> m_quadMag;
    mx::improc::eigenImage<realT> m_circ;
    mx::improc::eigenImage<realT> m_circMag;
-   
+
    unsigned m_rows {0}; ///< [in] the size of a quad, in rows
    unsigned m_cols {0}; ///< [in] the size of a quad, in cols
-   
+
    int m_numPupils {4}; ///< the number of pupils.  Default is 4, 3 is also supported.
-   float m_pupMedIndex {0.6866}; ///< the index of the illuminated pupil median in a sorted array.
-   
+   float m_pupMedIndex {0.6266}; ///< the index of the illuminated pupil median in a sorted array.
+
    float m_med[4] = {0,0,0,0};
    float m_avgx[4] = {0,0,0,0};
    float m_avgy[4] = {0,0,0,0};
    float m_avgr[4] = {0,0,0,0};
-   
+
    float m_thresh {0.5};
-   
+
    std::vector<realT> m_pixs;
-   
+
    pupilFitter() {}
-   
+
    int setSize( unsigned rows, ///< [in] the new size of a quad, in rows
                 unsigned cols  ///< [in] the new size of a quad, in cols
               );
-   
+
    /// Returns the quadrant starting coordinates
-   int quadCoords( size_t & i0, ///< [out] the i coordinate of the lower-left corner of quad 
+   int quadCoords( size_t & i0, ///< [out] the i coordinate of the lower-left corner of quad
                    size_t & j0, ///< [out] the j coordinate of the lower-left corner of auad
                    int quadNo   ///< [in] the quadrant number
                  );
-   
+
    int threshold( mx::improc::eigenImage<realT> & im );
-   
+
    int getQuad( mx::improc::eigenImage<realT> & quad,
                 mx::improc::eigenImage<realT> & im,
                 int quadNo
               );
-   
+
    int putQuad( mx::improc::eigenImage<realT> & im,
                 mx::improc::eigenImage<realT> & quad,
                 int quadNo
               );
-   
+
    int outerpix( float & avgx,
                  float & avgy,
                  float & avgr,
                  int quadNo
                );
-   
-   int fit( mx::improc::eigenImage<realT> & im, 
+
+   int fit( mx::improc::eigenImage<realT> & im,
             mx::improc::eigenImage<realT> & edged
           );
-   
+
    //int emitRegion( const std::string fname );
-   
+
 };
 
 template<typename realT>
@@ -86,31 +86,31 @@ int pupilFitter<realT>::setSize( unsigned rows,
 {
    m_rows = rows;
    m_cols = cols;
-   
+
    m_quad.resize(m_rows,m_cols);
    m_circ.resize(m_rows,m_cols);
-   
+
    //Set up for the magnified version
    m_quadMag.resize(m_quad.rows()*10, m_quad.cols()*10);
    m_circMag.resize(m_circ.rows()*10, m_circ.cols()*10);
 
    m_pixs.resize(m_quadMag.rows()*m_quadMag.cols());
-   
+
    return 0;
 }
-   
+
 template<typename realT>
 int pupilFitter<realT>::quadCoords( size_t & i0,
                                     size_t & j0,
-                                    int quadNo  
+                                    int quadNo
                                   )
 {
    if(m_numPupils == 3)
    {
       i0 = 0;
       j0 = 0;
-      
-      if(quadNo == 1) 
+
+      if(quadNo == 1)
       {
          i0 = m_rows;
       }
@@ -124,17 +124,17 @@ int pupilFitter<realT>::quadCoords( size_t & i0,
    {
       i0=0;
       j0=0;
-   
+
       if(quadNo == 1) i0 = m_rows;
       if(quadNo == 2) j0 = m_cols;
-      if(quadNo == 3) 
+      if(quadNo == 3)
       {
          i0 = m_rows;
          j0 = m_cols;
       }
-      
+
    }
-   
+
    return 0;
 }
 
@@ -149,7 +149,7 @@ int pupilFitter<realT>::threshold( mx::improc::eigenImage<realT> & im )
          else im(i,j) = 0;
       }
    }
-   
+
    return 0;
 }
 
@@ -159,15 +159,15 @@ int pupilFitter<realT>::getQuad( mx::improc::eigenImage<realT> & quad,
                                  int quadNo
                                )
 {
-   
+
    if(im.rows() != 2*m_rows || im.cols() != 2*m_cols)
    {
       return -1;
    }
-   
+
    size_t i0=0, j0=0;
    quadCoords(i0, j0, quadNo);
-   
+
    for(size_t i =i0; i< i0+m_rows; ++i)
    {
       for(size_t j=j0; j<j0+m_cols;++j)
@@ -175,7 +175,7 @@ int pupilFitter<realT>::getQuad( mx::improc::eigenImage<realT> & quad,
          quad(i-i0, j-j0) = im(i,j);
       }
    }
-   
+
    return 0;
 }
 
@@ -189,10 +189,10 @@ int pupilFitter<realT>::putQuad( mx::improc::eigenImage<realT> & im,
    {
       return -1;
    }
-   
+
    size_t i0=0, j0=0;
    quadCoords(i0, j0, quadNo);
-   
+
    for(size_t i =i0; i< i0+m_rows; ++i)
    {
       for(size_t j=j0; j<j0+m_cols;++j)
@@ -200,7 +200,7 @@ int pupilFitter<realT>::putQuad( mx::improc::eigenImage<realT> & im,
          im(i,j) = quad(i-i0, j-j0);;
       }
    }
-   
+
    return 0;
 }
 
@@ -211,29 +211,29 @@ int pupilFitter<realT>::outerpix( float & avgx,
                                   int quadNo
                                 )
 {
-   
+
    realT x0, y0, avgr0;
 
    mx::improc::circleOuterpix(x0, y0, avgr0, avgx, avgy, avgr, m_circMag, m_quadMag);
-   
+
    avgx/=10.0;
    avgy/=10.0;
    avgr/=10.0;
 
    size_t i0=0, j0=0;
    quadCoords(i0, j0, quadNo);
-   
+
    avgx += i0;
    avgy += j0;
-   
+
    return 0;
-   
+
 }
 
 
 
 template<typename realT>
-int pupilFitter<realT>::fit( mx::improc::eigenImage<realT> & im, 
+int pupilFitter<realT>::fit( mx::improc::eigenImage<realT> & im,
                              mx::improc::eigenImage<realT> & edged
                            )
 {
@@ -243,32 +243,35 @@ int pupilFitter<realT>::fit( mx::improc::eigenImage<realT> & im,
    {
       // 0) magnify the image
       getQuad(m_quad, imin, i);
-      
+
       mx::improc::imageMagnify(m_quadMag, m_quad, mx::improc::bilinearTransform<realT>());
 
       // 1) normalize by median of pupil
 
-      for(size_t j=0; j < m_pixs.size(); ++j) m_pixs[j] = m_quadMag.data()[j];
-      
+      for(size_t j=0; j < m_pixs.size(); ++j)
+      {
+        m_pixs[j] = m_quadMag.data()[j];
+      }
+
       std::sort(m_pixs.begin(), m_pixs.end());
 
       m_med[i] = m_pixs[m_pupMedIndex*m_pixs.size()]; //This should be the median of the pupils if the right size and contained in the quad
-      
-      m_quadMag/=m_med[i]; 
-      
+
+      m_quadMag/=m_med[i];
+
       // 2) Threshold the normalized quad
       threshold(m_quadMag);
 
       // 3) Find outer pixels and the radius
       outerpix(m_avgx[i], m_avgy[i], m_avgr[i], i);
-   
+
       // 4) De-magnify and prepare for putting on the streams
       mx::improc::imageRebinSum( m_quad, m_quadMag, true);
       mx::improc::imageRebinSum( m_circ, m_circMag);
       putQuad(im, m_quad, i);
       putQuad(edged, m_circ, i);
    }
-   
+
    return 0;
 }
 } //namespace app
