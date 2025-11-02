203bc46e948ff8a065974e4f957afa5a2d83f552
Jared Males <jaredmales@gmail.com>
2025-11-01 21:17:01 -0300

    updated xInstGraph tests

--- apps/xInstGraph/tests/xInstGraph_test.cpp
+++ apps/xInstGraph/tests/xInstGraph_test.cpp
@@ -28,7 +28,11 @@ class xInstGraph : public MagAOX::app::xInstGraph
         return MagAOX::app::xInstGraph::config;
     }
 
+<<<<<<< HEAD
 
+=======
+
+>>>>>>> e1ec92c0 (updated xInstGraph tests)
 
 };
 
