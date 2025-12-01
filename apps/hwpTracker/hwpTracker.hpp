684bbb6d92e5c51ac1149aac98340a5ab10ea989
Miles Lucas <mileslucas@arizona.edu>
2025-12-01 06:15:18 -0300

    update simple hwpTracker definitions

--- apps/hwpTracker/hwpTracker.hpp
+++ apps/hwpTracker/hwpTracker.hpp
@@ -265,9 +265,8 @@ void hwpTracker::loadConfig()
 int hwpTracker::appStartup()
 {
 
-    createStandardIndiToggleSw( m_indiP_tracking, "tracking" );
-    registerIndiPropertyNew( m_indiP_tracking, INDI_NEWCALLBACK( m_indiP_tracking ) );
-
+    CREATE_REG_INDI_NEW_TOGGLESWITCH( m_indiP_tracking, "tracking" );
+    
     REG_INDI_SETPROP( m_indiP_teldata, m_tcsDevName, "teldata" );
 
     REG_INDI_SETPROP( m_indiP_stagePolRot, m_devName, "position" );
@@ -276,7 +275,6 @@ int hwpTracker::appStartup()
 
     createStandardIndiNumber<float>(
         m_indiP_hwpSetPos, "hwp_position", -360.0, 360.0, 1e-3, "%.03f", "HWP Set Position", "HWP Status" );
-    registerIndiPropertyNew( m_indiP_hwpSetPos, INDI_NEWCALLBACK( m_indiP_hwpSetPos ) );
 
     REG_INDI_NEWPROP_NOCB( m_indiP_hwpTrackingOffset, "hwp_tracking_offset", pcf::IndiProperty::Number );
     m_indiP_hwpTrackingOffset.add( pcf::IndiElement( "value" ) );
@@ -318,10 +316,9 @@ int hwpTracker::appLogic()
 
         lastupdate = mx::sys::get_curr_time();
     }
-    else
+    else if( !m_tracking )
     {
-        if( !m_tracking )
-            lastupdate = 0;
+        lastupdate = 0;
     }
 
     TELEMETER_APP_LOGIC;
