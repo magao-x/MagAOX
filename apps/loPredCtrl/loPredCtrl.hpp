b7249409c8d275926fd79730654128ac23159af1
syhaffert <sebastiaanhaffert@gmail.com>
2026-03-22 15:16:00 +0000

    added framegrabber interface

--- apps/loPredCtrl/loPredCtrl.hpp
+++ apps/loPredCtrl/loPredCtrl.hpp
@@ -14,6 +14,7 @@
 #include <chrono>
 #include <thread>
 #include <random>
+#include <semaphore.h>
 
 #include <Eigen/Dense>
 #include <mx/improc/eigenCube.hpp>
@@ -32,7 +33,7 @@ using namespace mx::improc;
  namespace app
  {
 
- class loPredCtrl : public MagAOXApp<true>, public dev::shmimMonitor<loPredCtrl>
+ class loPredCtrl : public MagAOXApp<true>, public dev::shmimMonitor<loPredCtrl>, public dev::frameGrabber<loPredCtrl>, public dev::telemeter<loPredCtrl>
  {
      // Give the test harness access.
      friend class loPredCtrl_test;
@@ -42,6 +43,14 @@ using namespace mx::improc;
      // The base shmimMonitor type
      typedef dev::shmimMonitor<loPredCtrl> shmimMonitorT;
 
+     friend class dev::frameGrabber<loPredCtrl>;
+
+     typedef dev::frameGrabber<loPredCtrl> frameGrabberT;
+
+     friend class dev::telemeter<loPredCtrl>;
+
+     typedef dev::telemeter<loPredCtrl> telemeterT;
+
      /// Floating point type in which to do all calculations.
      typedef float realT;
 
@@ -50,24 +59,15 @@ using namespace mx::improc;
       *@{
       */
 
+     /// This framegrabber can't be flipped
+     static constexpr bool c_frameGrabber_flippable = false;
+
      ///@}
 
    protected:
      /** \name Configurable Parameters
       *@{
       */
-
-    // variables for sending the output to an output shmim.
-    std::string m_outputName;
-	IMAGE m_outputStream;
-	uint32_t m_outputWidth {0}; ///< The width of the image
-	uint32_t m_outputHeight {0}; ///< The height of the image.
-
-	uint8_t m_outputDataType{0}; ///< The ImageStreamIO type code.
-	size_t m_outputTypeSize {0}; ///< The size of the type, in bytes.
-
-	bool m_outputOpened {false};
-	bool m_outputRestart {false};
 
 
     // The incoming stream name
@@ -124,6 +124,20 @@ using namespace mx::improc;
 
     std::string m_filename {""};
 
+    std::string m_fpsSource{ "camwfs" }; /**< Device name for getting fps of the loop.
+                                              This device must have *.fps.current.  Default is camwfs*/
+
+    realT m_fps {0}; ///< The current fps
+
+    /** \name frameGrabber Interface
+     * @{
+     */
+
+    bool m_updated{ false }; ///< Flag indicating that the commands have been updated
+    sem_t m_smSemaphore{ 0 }; ///< Semaphore used to synchronize the fg thread and the process thread.
+
+    ///@}
+
     pcf::IndiProperty m_indiP_exploration;
     pcf::IndiProperty m_indiP_filename;
     pcf::IndiProperty m_indiP_learningToggle;
@@ -133,6 +147,9 @@ using namespace mx::improc;
 
     pcf::IndiProperty m_indiP_saveToggle;
     pcf::IndiProperty m_indiP_loadToggle;
+
+    pcf::IndiProperty m_indiP_fpsSource;
+    pcf::IndiProperty m_indiP_fps;
 
    public:
 
@@ -145,6 +162,8 @@ using namespace mx::improc;
 
     INDI_NEWCALLBACK_DECL( loPredCtrl, m_indiP_saveToggle );
     INDI_NEWCALLBACK_DECL( loPredCtrl, m_indiP_loadToggle );
+
+    INDI_SETCALLBACK_DECL( loPredCtrl, m_indiP_fpsSource );
 
      /// Default c'tor.
      loPredCtrl();
@@ -184,7 +203,49 @@ using namespace mx::improc;
      virtual int appShutdown();
 
      // Custom functions
-     int send_to_shmim();
+
+     /** \name frameGrabber Interface
+      * @{
+      */
+
+     /// Configure the output stream for acquistion.
+     /** Tests if stream exists and is expected size.  Creates it if needed.
+      *  will set m_width, m_height, and m_dataType.
+      */
+     int configureAcquisition();
+
+     /// Gets the frames-per-second readout rate
+     /** Used for the latency statistics
+       */
+     float fps();
+
+     /// Start acquisition.
+     /** A no-op in this class.
+      */
+     int startAcquisition();
+
+     /// Acquire data.
+     /** Here just waits on the semaphore.
+      */
+     int acquireAndCheckValid();
+
+     /// Loads the commands into the stream
+     int loadImageIntoStream(void * dest);
+
+     ///Take any actions needed to reconfigure the system.  Called if m_reconfig is set to true.
+     int reconfig();
+
+     ///@}
+
+     /** \name telemeter Interface
+      * @{
+      */
+
+     int checkRecordTimes();
+
+     int recordTelem( const telem_fgtimings * );
+
+     ///@}
 
    protected:
      int allocate( const dev::shmimT &dummy /**< [in] tag to differentiate shmimMonitor parents.*/ );
@@ -206,20 +267,6 @@ using namespace mx::improc;
      }
  };
 
- inline int loPredCtrl::send_to_shmim()
- {
-    // Check if processImage is running
-    // while(m_outputStream.md[0].write == 1);
-
-    m_outputStream.md[0].write = 1;
-    memcpy( m_outputStream.array.raw, full_command.data(), m_modevalWidth * m_modevalTypeSize );
-    m_outputStream.md[0].cnt0++;
-    m_outputStream.md[0].write = 0;
-
-    //ImageStreamIO_sempost( &m_outputStream, -1 );
-
-    return 0;
- }
 
  inline loPredCtrl::loPredCtrl() : MagAOXApp( MAGAOX_CURRENT_SHA1, MAGAOX_REPO_MODIFIED )
  {
@@ -229,7 +276,10 @@ using namespace mx::improc;
  inline void loPredCtrl::setupConfig()
  {
      shmimMonitorT::setupConfig( config );
-     config.add("outputShmim.shmimName", "", "outputShmim.shmimName", argType::Required, "outputShmim", "shmimName", false, "string", "The output shmim to write to.");
+     FRAMEGRABBER_SETUP_CONFIG( config );
+     TELEMETER_SETUP_CONFIG(config);
+
+     config.add("parameters.fpsSource", "", "parameters.fpsSource", argType::Required, "parameters", "fpsSource", false, "string", "The device name for getting fps of the loop.");
 
      config.add("parameters.gain", "", "parameters.gain", argType::Required, "parameters", "gain", false, "float", "The initial feedback gain.");
      config.add("parameters.regularization", "", "parameters.regularization", argType::Required, "parameters", "regularization", false, "float", "The regularization parameter.");
@@ -245,6 +295,11 @@ using namespace mx::improc;
  {
      shmimMonitorT::loadConfig( config );
 
+    frameGrabberT::m_ownShmim = false;
+    FRAMEGRABBER_LOAD_CONFIG(_config);
+
+    _config(m_fpsSource, "parameters.fpsSource");
+
     _config(m_gainCtrl, "parameters.gain");
     _config(m_regularizationCtrl, "parameters.regularization");
     _config(m_gammaCtrl, "parameters.gamma");
@@ -254,10 +309,7 @@ using namespace mx::improc;
     _config(m_history, "parameters.history");
     _config(m_future, "parameters.future");
 
-    _config(m_outputName, "outputShmim.shmimName");
-
-	std::cout << "Open output channel at " << m_outputName << std::endl;
-    std::cout << "Gain " << m_gainCtrl << std::endl;
+	std::cout << "Gain " << m_gainCtrl << std::endl;
     std::cout << "Regularization " << m_regularizationCtrl << std::endl;
     std::cout << "Gamma " << m_gammaCtrl << std::endl;
 
@@ -303,6 +355,26 @@ using namespace mx::improc;
 
      createStandardIndiRequestSw( m_indiP_loadToggle, "load_state", "Load the controller state", "Load State");
 	 registerIndiPropertyNew( m_indiP_loadToggle, INDI_NEWCALLBACK(m_indiP_loadToggle) );
+
+    REG_INDI_SETPROP( m_indiP_fpsSource, m_fpsSource, std::string( "fps" ) );
+
+    createROIndiNumber( m_indiP_fps, "fps" );
+    m_indiP_fps.add( pcf::IndiElement( "current" ) );
+    if( registerIndiPropertyReadOnly( m_indiP_fps ) < 0 )
+    {
+        log<software_error>( { "" } );
+        return -1;
+    }
+
+    if( sem_init( &m_smSemaphore, 0, 0 ) < 0 )
+    {
+        log<software_critical>( { errno, "Initializing S.M. semaphore" } );
+        return -1;
+    }
+
+    FRAMEGRABBER_APP_STARTUP;
+    TELEMETER_APP_STARTUP;
+
      state( stateCodes::OPERATING );
      return 0;
  }
@@ -314,12 +386,17 @@ using namespace mx::improc;
          return log<software_error, -1>( { __FILE__, __LINE__ } );
      }
 
+    FRAMEGRABBER_APP_LOGIC;
+    TELEMETER_APP_LOGIC;
+
      std::unique_lock<std::mutex> lock( m_indiMutex );
 
      if( shmimMonitorT::updateINDI() < 0 )
      {
          log<software_error>( { __FILE__, __LINE__ } );
      }
+
+    FRAMEGRABBER_UPDATE_INDI;
 
      updatesIfChanged<std::string>( m_indiP_exploration, { "current", "target" }, { m_exploration_sequence, m_exploration_sequence } );
 
@@ -349,6 +426,9 @@ using namespace mx::improc;
  inline int loPredCtrl::appShutdown()
  {
      shmimMonitorT::appShutdown();
+
+    FRAMEGRABBER_APP_SHUTDOWN;
+    TELEMETER_APP_SHUTDOWN;
 
      if(controller)
         delete controller;
@@ -384,36 +464,6 @@ using namespace mx::improc;
     generator = std::default_random_engine();
     distribution = std::normal_distribution<DDSPC::realT>(0.0, 1.0);
 
-
-    // Allocate the DM
-	if(m_outputOpened){
-		ImageStreamIO_closeIm(&m_outputStream);
-	}
-
-	m_outputOpened = false;
-	m_outputRestart = false; //Set this up front, since we're about to restart.
-
-	if( ImageStreamIO_openIm(&m_outputStream, m_outputName.c_str()) == 0){
-		if(m_outputStream.md[0].sem < 10){
-			ImageStreamIO_closeIm(&m_outputStream);
-		}else{
-			m_outputOpened = true;
-		}
-	}
-
-	if(!m_outputOpened){
-		log<text_log>( m_outputName + " not opened.", logPrio::LOG_NOTICE);
-		return -1;
-	}else{
-		m_outputWidth = m_outputStream.md->size[0];
-		m_outputHeight = m_outputStream.md->size[1];
-
-		m_outputDataType = m_outputStream.md->datatype;
-		m_outputTypeSize = sizeof(float);
-
-		log<text_log>( "Opened " + m_outputName + " " + std::to_string(m_outputWidth) + " x " + std::to_string(m_outputHeight) + " with data type: " + std::to_string(m_outputDataType), logPrio::LOG_NOTICE);
-	}
-
     controller = new DDSPC::PredictiveController(m_num_modes, m_history, m_future, m_gainCtrl, m_gammaCtrl, m_regularizationCtrl, m_covarianceCtrl);
 
     return 0;
@@ -421,6 +471,14 @@ using namespace mx::improc;
 
  inline int loPredCtrl::processImage( void *curr_src, const dev::shmimT &dummy )
  {
+    static_cast<void>( dummy ); // be unused
+    //record arrival time as the atime
+    if( clock_gettime( CLOCK_REALTIME, &(frameGrabberT::m_currImageTimestamp) ) < 0 )
+    {
+        m_shutdown = true;
+        return log<software_critical,-1>( { errno, "clock_gettime" } );
+    }
+
     auto start = std::chrono::high_resolution_clock::now();
     // static_cast<void>( dummy ); // be unused
     // This could be made more efficient by doing only a single copy statement.
@@ -506,8 +564,8 @@ using namespace mx::improc;
     }
     
     //
-    for(int i=0; i < m_modevalWidth; i++){
-        if(i < m_num_modes){
+    for(size_t i=0; i < m_modevalWidth; i++){
+        if(i < static_cast<size_t>(m_num_modes)){
             if(is_integrating){
                 full_command(i, 0) = full_command(i, 0) + new_command(i, 0);
             }else{
@@ -518,7 +576,15 @@ using namespace mx::improc;
         }
     }
 
-    send_to_shmim();
+    // Send modal coefficients to the correct stream
+    m_updated = true;
+
+    // trigger framegrabber
+    if( sem_post( &m_smSemaphore ) < 0 )
+    {
+        log<software_critical>( { errno, 0, "Error posting to semaphore" } );
+        return -1;
+    }
 
     if(is_learning){
         controller->update_system();
@@ -528,14 +594,105 @@ using namespace mx::improc;
     auto end = std::chrono::high_resolution_clock::now();
     loop_time_elapsed += std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(end - start).count();
     
-    if(frame_counter % 2000 == 0){
-        std::cout << "HOWDY elapsed us: " << loop_time_elapsed / 2000.0 << " us" << std::endl;
+    if(frame_counter % 20 == 0){
+        std::cout << "HOWDY elapsed us: " << loop_time_elapsed / 20.0 << " us" << std::endl;
         loop_time_elapsed = 0.0;
     }
     
     frame_counter++;
     return 0;
  }
+
+int loPredCtrl::configureAcquisition()
+{
+    static bool logged = false;
+
+    int rv = openShmim();
+    if(rv != 0)
+    {
+        return rv;
+    }
+
+    if( frameGrabberT::m_width != (uint32_t)(m_modevalWidth * m_modevalHeight) || frameGrabberT::m_height != 1 ||
+        frameGrabberT::m_dataType != _DATATYPE_FLOAT )
+    {
+        if( !logged )
+        {
+            log<text_log>( frameGrabberT::m_shmimName + " is wrong size (" + std::to_string(frameGrabberT::m_width) + "x" + std::to_string(frameGrabberT::m_height) + ") or type (" + std::to_string(frameGrabberT::m_dataType) + ")",
+                           logPrio::LOG_INFO );
+            logged = true;
+        }
+        return 1;
+    }
+
+    logged = false;
+
+    return 0;
+}
+
+float loPredCtrl::fps()
+{
+    return m_fps;
+}
+
+int loPredCtrl::startAcquisition()
+{
+    return 0;
+}
+
+int loPredCtrl::acquireAndCheckValid()
+{
+    timespec ts;
+
+    errno = 0;
+    if( clock_gettime( CLOCK_REALTIME, &ts ) < 0 )
+    {
+        log<software_critical>( { errno, "clock_gettime" } );
+        return -1;
+    }
+
+    ts.tv_sec += 1;
+
+    if( sem_timedwait( &m_smSemaphore, &ts ) == 0 )
+    {
+        if( m_updated )
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int loPredCtrl::loadImageIntoStream(void * dest)
+{
+    memcpy( dest, full_command.data(), full_command.rows() * frameGrabberT::m_typeSize );
+
+    m_updated = false;
+
+    return 0;
+}
+
+int loPredCtrl::reconfig()
+{
+    return 0;
+}
+
+int loPredCtrl::checkRecordTimes()
+{
+    return telemeterT::checkRecordTimes( telem_fgtimings() );
+}
+
+int loPredCtrl::recordTelem( const telem_fgtimings * )
+{
+    return recordFGTimings( true );
+}
 
  INDI_NEWCALLBACK_DEFN( loPredCtrl, m_indiP_exploration )( const pcf::IndiProperty &ipRecv )
 {
@@ -782,6 +939,28 @@ INDI_NEWCALLBACK_DEFN(loPredCtrl, m_indiP_loadToggle )(const pcf::IndiProperty &
 	}
 
    return 0;
+}
+
+INDI_SETCALLBACK_DEFN( loPredCtrl, m_indiP_fpsSource )( const pcf::IndiProperty &ipRecv )
+{
+    INDI_VALIDATE_CALLBACK_PROPS( m_indiP_fpsSource, ipRecv );
+
+    if( ipRecv.find( "current" ) != true ) // this isn't valid
+    {
+        return -1;
+    }
+
+    std::lock_guard<std::mutex> guard( m_indiMutex );
+
+    realT fps = ipRecv["current"].get<float>();
+
+    if( fps != m_fps )
+    {
+        m_fps = fps;
+        updateIfChanged( m_indiP_fps, "current", m_fps );
+    }
+
+    return 0;
 }
 
  } // namespace app
